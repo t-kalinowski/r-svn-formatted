c2be376a200a8b72f1ee33d3e493396d5538d5c7
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1997-10-04 22:28:16 +0000

    paste() will now return a character vector with length >= 1.
    git-svn-id: https://svn.r-project.org/R/trunk@122 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/paste.c
+++ src/main/paste.c
@@ -70,6 +70,8 @@ SEXP do_paste(SEXP call, SEXP op, SEXP args, SEXP env)
         if (length(CAR(px)) > maxlen)
             maxlen = length(CAR(px));
     }
+    if (maxlen == 0)
+        return mkString("");
 
     PROTECT(ans = allocVector(STRSXP, maxlen));
 
