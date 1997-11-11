9922d9b13c14bdae6701a0c96e39bd443acd0285
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1997-11-11 02:35:18 +0000

    Portable fixes.
    git-svn-id: https://svn.r-project.org/R/trunk@240 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/devX11.c
+++ src/unix/devX11.c
@@ -409,7 +409,7 @@ static void FreeColors()
 
 static void SetLinetype(int newlty, double nlwd)
 {
-    unsigned char dashlist[8];
+    char dashlist[8];
     int i, ndash, newlwd;
 
     newlwd = nlwd;
@@ -498,7 +498,7 @@ static int X11_Open(char *dsp, double w, double h)
                                 CWEventMask | CWBackPixel | CWBorderPixel | CWBackingStore, &attributes)) == 0)
         return 0;
 
-    XChangeProperty(display, window, XA_WM_NAME, XA_STRING, 8, PropModeReplace, "R Graphics", 13);
+    XChangeProperty(display, window, XA_WM_NAME, XA_STRING, 8, PropModeReplace, (unsigned char *)"R Graphics", 13);
 
     gcursor = XCreateFontCursor(display, CURSOR);
     XDefineCursor(display, window, gcursor);
