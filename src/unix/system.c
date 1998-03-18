b9b2d2562e2b8b9309eaf9b4f721a4515ae13836
bates <bates@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-18 03:17:00 +0000

    Fixing -Wall gripes
    git-svn-id: https://svn.r-project.org/R/trunk@913 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/system.c
+++ src/unix/system.c
@@ -338,10 +338,10 @@ FILE *R_OpenSiteFile(void)
 
     if (LoadSiteFile)
     {
-        if (fp = R_fopen(getenv("RPROFILE"), "r"))
+        if ((fp = R_fopen(getenv("RPROFILE"), "r")))
             return fp;
         sprintf(buf, "%s/etc/Rprofile", getenv("RHOME"));
-        if (fp = R_fopen(buf, "r"))
+        if ((fp = R_fopen(buf, "r")))
             return fp;
     }
 
@@ -357,10 +357,10 @@ FILE *R_OpenInitFile(void)
 
     if (LoadInitFile)
     {
-        if (fp = R_fopen(".Rprofile", "r"))
+        if ((fp = R_fopen(".Rprofile", "r")))
             return fp;
         sprintf(buf, "%s/.Rprofile", getenv("HOME"));
-        if (fp = R_fopen(buf, "r"))
+        if ((fp = R_fopen(buf, "r")))
             return fp;
     }
 
