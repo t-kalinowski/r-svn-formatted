ff4225494b1fc423a150e39820383d391983334e
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-04-08 09:29:23 +0000

    Levels could be non-character!  Fixed here.
    git-svn-id: https://svn.r-project.org/R/trunk@1043 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/split.c
+++ src/main/split.c
@@ -112,6 +112,8 @@ SEXP do_split(SEXP call, SEXP op, SEXP args, SEXP env)
     x = ans;
     if ((vec = getAttrib(f, R_LevelsSymbol)) != R_NilValue)
     {
+        if (!isString(vec))
+            errorcall(call, "non character factor levels!\n");
         for (i = 0; i < nlevs; i++)
         {
             TAG(x) = install(CHAR(STRING(vec)[i]));
