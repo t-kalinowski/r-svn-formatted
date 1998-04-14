a34de18312d5599b0efc0e05dad5fc7b7f038d01
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-04-14 16:19:40 +0000

    F2c configure problems subset() got botched by the 0-extent matrices
    git-svn-id: https://svn.r-project.org/R/trunk@1074 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gram.c
+++ src/main/gram.c
@@ -318,7 +318,7 @@ static const short yycheck[] = {
     -1,  -1, -1,  -1,  -1, 35, 36,  37,  38,  39, 40, 41, -1, -1, 44,  45, 46, 47, -1,  -1, -1, -1, 37, 38, 39,  40, 41,
     -1,  -1, 44,  45,  46, 47};
 /* -*-C-*-  Note some compilers choke on comments on `#line' lines.  */
-#line 3 "/usr/share/misc/bison.simple"
+#line 3 "/usr/lib/bison.simple"
 
 /* Skeleton output parser for bison,
    Copyright (C) 1984, 1989, 1990 Free Software Foundation, Inc.
@@ -513,7 +513,7 @@ static void __yy_memcpy(char *to, char *from, int count)
 #endif
 #endif
 
-#line 196 "/usr/share/misc/bison.simple"
+#line 196 "/usr/lib/bison.simple"
 
 /* The user can define YYPARSE_PARAM as the name of an argument to be passed
    into yyparse.  The argument should have type void *.
@@ -1324,7 +1324,7 @@ yyreduce:
     }
     }
     /* the action file gets copied in in place of this dollarsign */
-#line 498 "/usr/share/misc/bison.simple"
+#line 498 "/usr/lib/bison.simple"
 
     yyvsp -= yylen;
     yyssp -= yylen;
