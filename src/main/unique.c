f4b815062fdde58d55a303cd3314c3e565b47ef6
rgentlem <rgentlem@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-17 05:42:55 +0000

    fixed a problem with ...'s in match.arg.
    git-svn-id: https://svn.r-project.org/R/trunk@1466 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/unique.c
+++ src/main/unique.c
@@ -503,6 +503,10 @@ static SEXP StripUnmatched(SEXP s)
     {
         return StripUnmatched(CDR(s));
     }
+    else if (CAR(s) == R_DotsSymbol)
+    {
+        return StripUnmatched(CDR(s));
+    }
     else
     {
         CDR(s) = StripUnmatched(CDR(s));
@@ -607,13 +611,14 @@ SEXP do_matchcall(SEXP call, SEXP op, SEXP args, SEXP env)
         TAG(b) = TAG(f);
     }
 
-    /* Eliminate any unmatched formals */
-
-    rlist = StripUnmatched(rlist);
-
     /* Handle the dots */
 
     PROTECT(rlist = ExpandDots(rlist, expdots));
+
+    /* Eliminate any unmatched formals and any that match R_DotSymbol */
+    /* This needs to be after ExpandDots as the DOTSXP might match ... */
+
+    rlist = StripUnmatched(rlist);
 
     PROTECT(rval = allocSExp(LANGSXP));
     CAR(rval) = duplicate(CAR(CAR(CDR(args))));
