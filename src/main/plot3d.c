55d08ff7b2300b25f9336da2aa4b99a00a120551
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-22 00:49:44 +0000

    Persp added.
    git-svn-id: https://svn.r-project.org/R/trunk@1537 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plot3d.c
+++ src/main/plot3d.c
@@ -710,7 +710,6 @@ badxy:
     return R_NilValue; /* never used; to keep -Wall happy */
 }
 
-#ifdef PERSP
 /*  P e r s p e c t i v e   S u r f a c e   P l o t s  */
 
 /* Conversion of degrees to radians */
@@ -1096,4 +1095,3 @@ SEXP do_persp(SEXP call, SEXP op, SEXP args, SEXP env)
         recordGraphicOperation(op, originalArgs, dd);
     return x;
 }
-#endif
