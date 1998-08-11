30f4b258dfa0f8bb19ad55d949db04156e15ad6b
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-08-11 07:54:16 +0000

    as.vector now drops attributes again.
    git-svn-id: https://svn.r-project.org/R/trunk@1873 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/coerce.c
+++ src/main/coerce.c
@@ -1046,6 +1046,7 @@ SEXP do_asvector(SEXP call, SEXP op, SEXP args, SEXP rho)
         errorcall(call, "invalid mode\n");
     }
     ans = ascommon(call, CAR(args), type);
+    ATTRIB(ans) = R_NilValue;
     UNPROTECT(1);
     return ans;
 }
