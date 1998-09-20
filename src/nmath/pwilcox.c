1ac94ba1d2ae6ba808bc48b72b6394dd7b5e54d3
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-09-20 15:59:57 +0000

    Fix typo.
    git-svn-id: https://svn.r-project.org/R/trunk@2236 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pwilcox.c
+++ src/nmath/pwilcox.c
@@ -36,7 +36,7 @@ double pwilcox(double x, double m, double n)
 
 #ifdef IEEE_754
     if (ISNAN(x) || ISNAN(m) || ISNAN(n))
-        return x + n + p;
+        return x + m + n;
     if (!FINITE(m) || !FINITE(n))
     {
         ML_ERROR(ME_DOMAIN);
