5f411e088ced84f199f13ee55b083aec0f52d4de
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-10-25 21:46:47 +0000

    decided that isValidName check on symbols is more bug than feature
    git-svn-id: https://svn.r-project.org/R/trunk@2638 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -377,6 +377,13 @@ static void deparse2buff(SEXP s)
         print2buff("NULL");
         break;
     case SYMSXP:
+#if 1
+        print2buff(CHAR(PRINTNAME(s)));
+#else
+        /* I'm pretty sure this is WRONG:
+           Blindly putting special symbols in ""s causes more trouble than it solves
+            --pd
+        */
         if (isValidName(CHAR(PRINTNAME(s))))
             print2buff(CHAR(PRINTNAME(s)));
         else
@@ -394,6 +401,7 @@ static void deparse2buff(SEXP s)
                 print2buff(tpb);
             }
         }
+#endif
         break;
     case CHARSXP:
         print2buff(CHAR(s));
