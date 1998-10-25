7ba9a36e2c27bdebbf8e8ff1147751d0b63d5898
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-10-25 18:51:59 +0000

    PROTECT bug...
    git-svn-id: https://svn.r-project.org/R/trunk@2637 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/builtin.c
+++ src/main/builtin.c
@@ -605,7 +605,7 @@ SEXP do_get(SEXP call, SEXP op, SEXP args, SEXP rho)
 
     /* Now we can evaluate the arguments */
 
-    args = evalList(args, rho);
+    PROTECT(args = evalList(args, rho));
 
     /* The first arg is the object name */
     /* It must be present and a string */
@@ -655,6 +655,8 @@ SEXP do_get(SEXP call, SEXP op, SEXP args, SEXP rho)
 
     /* Search for the object */
     rval = findVar1(t1, genv, gmode, ginherits);
+
+    UNPROTECT(1);
 
     if (PRIMVAL(op))
     { /* have get(.) */
