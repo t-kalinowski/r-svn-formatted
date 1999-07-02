678c3c653dbe3126507e9e293d3731c2d8d76bad
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-07-02 20:38:23 +0000

    tidy up branch-update conflict
    git-svn-id: https://svn.r-project.org/R/trunk@5057 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/system.c
+++ src/gnuwin32/system.c
@@ -1273,50 +1273,48 @@ int R_ShowFiles(int nfile, char **file, char **headers, char *wtitle, int del, c
             pager = "internal";
         for (i = 0; i < nfile; i++)
         {
-#if 0
-	    if (FindFirstFile(file[i], &fd) != INVALID_HANDLE_VALUE) {
-		if (!strcmp(pager, "internal")) {
-		    newpager(wtitle, file[i], headers[i], del);
-		} else {
-		    sprintf(buf, "%s  %s", pager, file[i]);
-		    runcmd(buf, 0, 1, "");
-		}
-#else
-            if (!strcmp(pager, "internal"))
+            if (FindFirstFile(file[i], &fd) != INVALID_HANDLE_VALUE)
             {
-                newpager(wtitle, file[i], headers[i], del);
-            }
-            if (!strcmp(pager, "console"))
-            {
-                DWORD len = 1;
-                HANDLE f = CreateFile(file[i], GENERIC_READ, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-                if (f != INVALID_HANDLE_VALUE)
+                if (!strcmp(pager, "internal"))
                 {
-                    while (ReadFile(f, buf, 1023, &len, NULL) && len)
+                    newpager(wtitle, file[i], headers[i], del);
+                }
+                else if (!strcmp(pager, "console"))
+                {
+                    DWORD len = 1;
+                    HANDLE f = CreateFile(file[i], GENERIC_READ, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+                    if (f != INVALID_HANDLE_VALUE)
                     {
-                        buf[len] = '\0';
-                        R_WriteConsole(buf, strlen(buf));
+                        while (ReadFile(f, buf, 1023, &len, NULL) && len)
+                        {
+                            buf[len] = '\0';
+                            R_WriteConsole(buf, strlen(buf));
+                        }
+                        CloseHandle(f);
+                        if (del)
+                            DeleteFile(file[i]);
                     }
-                    CloseHandle(f);
-                    if (del)
-                        DeleteFile(file[i]);
+                    else
+                    {
+                        sprintf(buf, "Unable to open file '%s'", file[i]);
+                        warning(buf);
+                    }
                 }
                 else
                 {
-                    sprintf(buf, "Impossible to open file '%s'. Does it exist?\n", file[i]);
-                    warning(buf);
+                    sprintf(buf, "%s  %s", pager, file[i]);
+                    runcmd(buf, 0, 1, "");
                 }
-#endif
+            }
+            else
+            {
+                sprintf(buf, "file.show(): file %s does not exist\n", file[i]);
+                warning(buf);
+            }
         }
-        else
-        {
-            sprintf(buf, "file.show(): file %s does not exist\n", file[i]);
-            warning(buf);
-        }
+        return 0;
     }
-    return 0;
-}
-return 1;
+    return 1;
 }
 
 /* The location of the R system files */
