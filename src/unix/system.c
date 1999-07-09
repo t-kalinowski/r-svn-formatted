c9edcdbef26af71c6e78ee8658a20b1ff6e6f03c
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-07-09 08:16:04 +0000

    Improve dealing with __setfpucw.
    git-svn-id: https://svn.r-project.org/R/trunk@5114 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/system.c
+++ src/unix/system.c
@@ -662,10 +662,8 @@ int main(int ac, char **av)
     fpsetmask(0);
 #endif
 
-#ifdef linux
-#ifdef HAVE___SETFPUCW
+#ifdef NEED___SETFPUCW
     __setfpucw(_FPU_IEEE);
-#endif
 #endif
 
     if ((R_HistoryFile = getenv("R_HISTFILE")) == NULL)
@@ -762,10 +760,8 @@ void R_CleanUp(int ask)
     fpsetmask(~0);
 #endif
 
-#ifdef linux
-#ifdef HAVE___SETFPUCW
+#ifdef NEED___SETFPUCW
     __setfpucw(_FPU_DEFAULT);
-#endif
 #endif
 
     exit(0);
