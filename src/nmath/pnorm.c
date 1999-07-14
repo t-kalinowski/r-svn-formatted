accc63ca0064d0aff221e01660cd611a2108a0cb
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-07-14 07:28:07 +0000

    e.g. x=mu=Inf was wrong
    git-svn-id: https://svn.r-project.org/R/trunk@5158 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pnorm.c
+++ src/nmath/pnorm.c
@@ -71,10 +71,9 @@ double pnorm(double x, double mu, double sigma)
     double y;
     int i;
 
-    /* Note: The structure of these checks has been */
-    /* carefully thought through.  For example, if x == mu */
-    /* and sigma == 0, we still get the correct answer. */
-
+    /* Note: The structure of these checks has been carefully thought through.
+     * For example, if x == mu and sigma == 0, we still get the correct answer.
+     */
 #ifdef IEEE_754
     if (ISNAN(x) || ISNAN(mu) || ISNAN(sigma))
         return x + mu + sigma;
@@ -88,6 +87,8 @@ double pnorm(double x, double mu, double sigma)
 #ifdef IEEE_754
     if (!R_FINITE(x))
     {
+        if (ISNAN(x)) /* e.g. x=mu=Inf */
+            return (R_NaN);
         if (x < 0)
             return 0;
         else
