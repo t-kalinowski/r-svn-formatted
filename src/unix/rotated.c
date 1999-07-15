c3a810e848f7d94ffbe9219dbacb174a596ea524
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-07-15 16:13:49 +0000

    Rename Platform.h to Rconfig.h, Version.h to Rversion.h.
    git-svn-id: https://svn.r-project.org/R/trunk@5185 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/rotated.c
+++ src/unix/rotated.c
@@ -23,6 +23,10 @@
 
 /* ********************************************************************** */
 
+#ifdef HAVE_CONFIG_H
+#include <Rconfig.h>
+#endif
+
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
