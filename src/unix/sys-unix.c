65dec6ab822cd4b6a2a0f00a66467d469db8e6b2
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-07-18 10:08:36 +0000

    Be more careful about includes.
    git-svn-id: https://svn.r-project.org/R/trunk@5237 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/sys-unix.c
+++ src/unix/sys-unix.c
@@ -29,7 +29,9 @@
 #include "Fileio.h"
 
 #ifdef HAVE_LIBREADLINE
+#ifdef HAVE_READLINE_READLINE_H
 #include <readline/readline.h>
+#endif
 #ifdef HAVE_READLINE_HISTORY_H
 #include <readline/history.h>
 #endif
