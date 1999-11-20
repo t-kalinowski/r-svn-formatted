90b034d639f2f9ccaa1407eb9eb5ec20aae50915
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-11-20 00:49:08 +0000

    pixel doubling in rotated.c + NEWS entries
    git-svn-id: https://svn.r-project.org/R/trunk@6635 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/rotated.c
+++ src/unix/rotated.c
@@ -829,6 +829,7 @@ static RotatedTextItem *XRotCreateTextItem(Display *dpy, XFontStruct *font, doub
     int xp, yp;
     double sin_angle, cos_angle;
     int it, jt;
+    double itd, jtd;
     double di, dj;
     int ic = 0;
     double xl, xr, xinc;
@@ -1043,8 +1044,10 @@ static RotatedTextItem *XRotCreateTextItem(Display *dpy, XFontStruct *font, doub
         {
 
             /* rotate coordinates */
-            it = (double)item->cols_in / 2 + (di * cos_angle + dj * sin_angle);
-            jt = (double)item->rows_in / 2 - (-di * sin_angle + dj * cos_angle);
+            itd = (double)item->cols_in / 2 + (di * cos_angle + dj * sin_angle);
+            jtd = (double)item->rows_in / 2 - (-di * sin_angle + dj * cos_angle);
+            it = itd - (itd < 0); /* (int) -0.5 == 0 */
+            jt = jtd - (jtd < 0);
 
             /* set pixel if required */
             if (it >= 0 && it < item->cols_in && jt >= 0 && jt < item->rows_in)
