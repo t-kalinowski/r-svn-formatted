846f487eefc68cc1bdd50bacfac3562544b6fca6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-01-22 20:07:06 +0000

    long/int errors new API
    git-svn-id: https://svn.r-project.org/R/trunk@7726 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/splines/src/splines.c
+++ src/library/splines/src/splines.c
@@ -21,15 +21,12 @@
  * 1981) and the CMLIB library DBSPLINES.
  */
 
-#include "S.h"
-#ifndef R_S_H
-#define longint long int
-#endif /* R_S_H */
+#include "R.h"
 
 static double *ldel, *rdel;
-static long orderm1;
+static Sint orderm1;
 
-static void diff_table(double *ti, double x, longint ndiff)
+static void diff_table(double *ti, double x, Sint ndiff)
 {
     register double *r = rdel, *l = ldel, *dpt = ti;
 
@@ -42,7 +39,7 @@ static void diff_table(double *ti, double x, longint ndiff)
 
 static void basis_funcs(double *ti, double x, double *b)
 {
-    longint j, r;
+    Sint j, r;
     double saved, term;
 
     diff_table(ti, x, orderm1);
@@ -60,11 +57,11 @@ static void basis_funcs(double *ti, double x, double *b)
     }
 }
 
-static double evaluate(double *ti, double x, double *a, longint nder)
+static double evaluate(double *ti, double x, double *a, Sint nder)
 {
     register double *lpt, *rpt, *apt;
-    register longint inner;
-    longint outer = orderm1;
+    register Sint inner;
+    Sint outer = orderm1;
 
     while (nder--)
     {
@@ -72,17 +69,16 @@ static double evaluate(double *ti, double x, double *a, longint nder)
             *apt = outer * (*(apt + 1) - *apt) / (*(lpt + outer) - *lpt);
         outer--;
     }
-    diff_table(ti, x, (long)outer);
+    diff_table(ti, x, (Sint)outer);
     while (outer--)
         for (apt = a, lpt = ldel + outer, rpt = rdel, inner = outer + 1; inner--; lpt--, rpt++, apt++)
             *apt = (*(apt + 1) * *lpt + *apt * *rpt) / (*rpt + *lpt);
     return (*a);
 }
 
-void spline_value(double *knots, double *coeff, longint *ncoeff, longint *order, double *x, longint *nx, longint *deriv,
-                  double *y)
+void spline_value(double *knots, double *coeff, Sint *ncoeff, Sint *order, double *x, Sint *nx, Sint *deriv, double *y)
 {
-    long n = *nx;
+    Sint n = *nx;
     double *a, *last = knots + *ncoeff;
 
     a = Calloc(*order, double);
@@ -99,19 +95,19 @@ void spline_value(double *knots, double *coeff, longint *ncoeff, longint *order,
             coeff++;
         }
         Memcpy(a, coeff, *order);
-        *y++ = evaluate(knots, *x++, a, (longint)*deriv);
+        *y++ = evaluate(knots, *x++, a, (Sint)*deriv);
     }
     Free(ldel);
     Free(rdel);
     Free(a);
 }
 
-void spline_basis(double *knots, longint *ncoeff, longint *order, double *xvals, longint *derivs, longint *nx,
-                  double *basis, longint *offsets)
+void spline_basis(double *knots, Sint *ncoeff, Sint *order, double *xvals, Sint *derivs, Sint *nx, double *basis,
+                  Sint *offsets)
 {   /* evaluate the non-zero B-spline basis */
     /* functions (or their derivatives) at */
     /* xvals.  */
-    longint n = *nx, i, j;
+    Sint n = *nx, i, j;
     double *dpt, *coeff, *last = knots + *ncoeff;
 
     orderm1 = *order - 1L;
@@ -130,7 +126,7 @@ void spline_basis(double *knots, longint *ncoeff, longint *order, double *xvals,
                 for (j = 0; j < *order; j++)
                     coeff[j] = 0;
                 coeff[i] = 1;
-                *basis++ = evaluate(dpt, *xvals, coeff, (longint)*derivs);
+                *basis++ = evaluate(dpt, *xvals, coeff, (Sint)*derivs);
             }
         }
         else
@@ -138,16 +134,16 @@ void spline_basis(double *knots, longint *ncoeff, longint *order, double *xvals,
             basis_funcs(dpt, *xvals, basis); /* fast method for value */
             basis += *order;
         }
-        *offsets++ = (long)(dpt - knots);
+        *offsets++ = (Sint)(dpt - knots);
     }
     Free(ldel);
     Free(rdel);
     Free(coeff);
 }
 
-void lin_interp(double *x, double *y, double *x0, double *y0, longint *nvals)
+void lin_interp(double *x, double *y, double *x0, double *y0, Sint *nvals)
 {
-    longint n = *nvals;
+    Sint n = *nvals;
     double *firstx = x;
 
     while (n--)
