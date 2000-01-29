37497f53f29cd1cf6dca0562f2919db03dbcbc6a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-01-29 09:47:25 +0000

    .0
    git-svn-id: https://svn.r-project.org/R/trunk@7865 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pbinom.c
+++ src/nmath/pbinom.c
@@ -41,5 +41,5 @@ double pbinom(double x, double n, double p, int lower_tail, int log_p)
         return R_DT_0;
     if (n <= x)
         return R_DT_1;
-    return pbeta(1.0 - p, n - x, x + 1, lower_tail, log_p);
+    return pbeta(1 - p, n - x, x + 1, lower_tail, log_p);
 }
