b96ff4f007e98b532bb222212fe92ce3439b479d
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-03-15 01:38:04 +0000

    There was memory problem in GPolygon.c.  The arguments include C pointers copied from SEXPs.  Space was allocated internally with R_alloc. This occasionally tripped a gc and turned the pointers to mush.
    git-svn-id: https://svn.r-project.org/R/trunk@8519 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/graphics.c
+++ src/main/graphics.c
@@ -2942,14 +2942,25 @@ int GClipPolygon(double *x, double *y, int n, int coords, int store, double *xou
  */
 static void clipPolygon(int n, double *x, double *y, int coords, int bg, int fg, DevDesc *dd)
 {
-    char *vmax;
-    double *xc, *yc;
+    static double *xc = NULL, *yc = NULL;
+    double *tmp;
+    if (xc != NULL)
+    {
+        tmp = xc;
+        xc = NULL;
+        free(tmp);
+    }
+    if (yc != NULL)
+    {
+        tmp = yc;
+        yc = NULL;
+        free(tmp);
+    }
     if (bg == NA_INTEGER)
     {
         int i;
-        vmax = vmaxget();
-        xc = (double *)R_alloc(n + 1, sizeof(double));
-        yc = (double *)R_alloc(n + 1, sizeof(double));
+        xc = (double *)malloc((n + 1) * sizeof(double));
+        yc = (double *)malloc((n + 1) * sizeof(double));
         for (i = 0; i < n; i++)
         {
             xc[i] = x[i];
@@ -2959,22 +2970,30 @@ static void clipPolygon(int n, double *x, double *y, int coords, int bg, int fg,
         yc[n] = y[0];
         dd->gp.col = fg;
         GPolyline(n + 1, xc, yc, coords, dd);
-        vmaxset(vmax);
     }
     else
     {
         int npts;
-        xc = yc = 0; /* -Wall */
         npts = GClipPolygon(x, y, n, coords, 0, xc, yc, dd);
         if (npts > 1)
         {
-            vmax = vmaxget();
-            xc = (double *)R_alloc(npts, sizeof(double));
-            yc = (double *)R_alloc(npts, sizeof(double));
+            xc = (double *)malloc(npts * sizeof(double));
+            yc = (double *)malloc(npts * sizeof(double));
             npts = GClipPolygon(x, y, n, coords, 1, xc, yc, dd);
             dd->dp.polygon(npts, xc, yc, coords, bg, fg, dd);
-            vmaxset(vmax);
         }
+    }
+    if (xc != NULL)
+    {
+        tmp = xc;
+        xc = NULL;
+        free(tmp);
+    }
+    if (yc != NULL)
+    {
+        tmp = yc;
+        yc = NULL;
+        free(tmp);
     }
 }
 
