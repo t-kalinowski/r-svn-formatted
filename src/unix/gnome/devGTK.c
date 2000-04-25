2731818314c658fff061e542c586ac65f3bf44ac
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-04-25 06:45:40 +0000

    -Wall fixes.
    git-svn-id: https://svn.r-project.org/R/trunk@8940 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/gnome/devGTK.c
+++ src/unix/gnome/devGTK.c
@@ -818,7 +818,7 @@ static void GTK_Text(double x, double y, int coords, char *str, double rot, doub
     gtkDesc *gtkd = (gtkDesc *)dd->deviceSpecific;
     GdkColor gcol_fill;
     gint size;
-    double x1, y1, rrot = DEG2RAD * rot;
+    double rrot = DEG2RAD * rot;
 
     GConvert(&x, &y, coords, DEVICE, dd);
 
