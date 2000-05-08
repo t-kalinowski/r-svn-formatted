c8b001d5e6a4778d4cd799f6fdfc449e7e2629a7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-05-08 21:40:55 +0000

    add bitmap devices
    git-svn-id: https://svn.r-project.org/R/trunk@9115 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/X11/devX11.c
+++ src/unix/X11/devX11.c
@@ -43,6 +43,7 @@
 
 #include "Defn.h"
 #include "Graphics.h"
+#include "Fileio.h"  /* R_fopen */
 #include "rotated.h" /* 'Public' routines from here */
 
 #include "eventloop.h" /* For the input handlers of the event loop mechanism. */
@@ -56,6 +57,10 @@ int X11DeviceDriver(DevDesc *, char *, double, double, double, double, int, int)
 #define PSEUDOCOLOR1 2
 #define PSEUDOCOLOR2 3
 #define TRUECOLOR 4
+
+#define WINDOW 1
+#define PNG 2
+#define JPEG 3
 
 /********************************************************/
 /* This device driver has been documented so that it be	*/
@@ -115,6 +120,10 @@ typedef struct
     int usefixed;
     XFontStruct *fixedfont;
     XFontStruct *font;
+    int type;    /* Window or pixmap? */
+    int npages;  /* counter for a pixmap */
+    FILE *fp;    /* file for a bitmap device */
+    int quality; /* JPEG quality */
 
 } x11Desc;
 
@@ -1105,16 +1114,42 @@ static int X11_Open(DevDesc *dd, x11Desc *xd, char *dsp, double w, double h, dou
     /* free(dd) and free(xd) */
 
     XEvent event;
-    int iw, ih;
+    int iw, ih, type;
+    char *p = dsp;
     XGCValues gcv;
     int DisplayOpened = 0; /* Indicates whether the display is created within this particular call. */
+
+    if (!strncmp(dsp, "png::", 5))
+    {
+        FILE *fp;
+        if (!(fp = R_fopen(R_ExpandFileName(dsp + 5), "w")))
+            error("could not open PNG file `%s'", dsp + 6);
+        xd->fp = fp;
+        type = PNG;
+        p = "";
+    }
+    else if (!strncmp(dsp, "jpeg::", 6))
+    {
+        FILE *fp;
+        type = JPEG;
+        p = strchr(dsp + 6, ':');
+        *p = '\0';
+        xd->quality = atoi(dsp + 6);
+        if (!(fp = R_fopen(R_ExpandFileName(p + 1), "w")))
+            error("could not open JPEG file `%s'", p + 1);
+        xd->fp = fp;
+        p = "";
+    }
+    else
+        type = WINDOW;
+    xd->type = type;
 
     /* If there is no server connection, establish one and */
     /* initialize the X11 device driver data structures. */
 
     if (!displayOpen)
     {
-        if ((display = XOpenDisplay(dsp)) == NULL)
+        if ((display = XOpenDisplay(p)) == NULL)
             return 0;
 #define SETGAMMA
 #ifdef SETGAMMA
@@ -1162,41 +1197,55 @@ static int X11_Open(DevDesc *dd, x11Desc *xd, char *dsp, double w, double h, dou
     attributes.backing_store = Always;
     attributes.event_mask = ButtonPressMask | ExposureMask | StructureNotifyMask;
 
-    xd->windowWidth = iw = w / pixelWidth();
-    xd->windowHeight = ih = h / pixelHeight();
-
-    if ((xd->window = XCreateWindow(display, rootwin, DisplayWidth(display, screen) - iw - 10, 10, iw, ih, 1,
-                                    DefaultDepth(display, screen), InputOutput, DefaultVisual(display, screen),
-                                    CWEventMask | CWBackPixel | CWBorderPixel | CWBackingStore, &attributes)) == 0)
-        return 0;
-
-    XChangeProperty(display, xd->window, XA_WM_NAME, XA_STRING, 8, PropModeReplace, (unsigned char *)"R Graphics", 13);
-
-    xd->gcursor = XCreateFontCursor(display, CURSOR);
-    XDefineCursor(display, xd->window, xd->gcursor);
-
-    /* set up protocols so that window manager sends */
-    /* me an event when user "destroys" window */
-    _XA_WM_PROTOCOLS = XInternAtom(display, "WM_PROTOCOLS", 0);
-    protocol = XInternAtom(display, "WM_DELETE_WINDOW", 0);
-    XSetWMProtocols(display, xd->window, &protocol, 1);
-
-    /* Save the devDesc* with the window for event dispatching */
-    XSaveContext(display, xd->window, devPtrContext, (caddr_t)dd);
-
-    /* Map the window */
-
-    XSelectInput(display, xd->window, ExposureMask | ButtonPressMask | StructureNotifyMask);
-    XMapWindow(display, xd->window);
-    XSync(display, 0);
-
-    /* Gobble expose events */
-
-    XNextEvent(display, &event);
-    if (event.xany.type == Expose)
+    if (type == WINDOW)
     {
-        while (event.xexpose.count)
-            XNextEvent(display, &event);
+        xd->windowWidth = iw = w / pixelWidth();
+        xd->windowHeight = ih = h / pixelHeight();
+        if ((xd->window = XCreateWindow(display, rootwin, DisplayWidth(display, screen) - iw - 10, 10, iw, ih, 1,
+                                        DefaultDepth(display, screen), InputOutput, DefaultVisual(display, screen),
+                                        CWEventMask | CWBackPixel | CWBorderPixel | CWBackingStore, &attributes)) == 0)
+            return 0;
+
+        XChangeProperty(display, xd->window, XA_WM_NAME, XA_STRING, 8, PropModeReplace, (unsigned char *)"R Graphics",
+                        13);
+
+        xd->gcursor = XCreateFontCursor(display, CURSOR);
+        XDefineCursor(display, xd->window, xd->gcursor);
+
+        /* set up protocols so that window manager sends */
+        /* me an event when user "destroys" window */
+        _XA_WM_PROTOCOLS = XInternAtom(display, "WM_PROTOCOLS", 0);
+        protocol = XInternAtom(display, "WM_DELETE_WINDOW", 0);
+        XSetWMProtocols(display, xd->window, &protocol, 1);
+
+        /* Save the devDesc* with the window for event dispatching */
+        XSaveContext(display, xd->window, devPtrContext, (caddr_t)dd);
+
+        /* Map the window */
+
+        XSelectInput(display, xd->window, ExposureMask | ButtonPressMask | StructureNotifyMask);
+        XMapWindow(display, xd->window);
+        XSync(display, 0);
+
+        /* Gobble expose events */
+
+        XNextEvent(display, &event);
+        if (event.xany.type == Expose)
+        {
+            while (event.xexpose.count)
+                XNextEvent(display, &event);
+        }
+    }
+    else
+    { /* PIXMAP */
+        xd->windowWidth = iw = w;
+        xd->windowHeight = ih = h;
+        if ((xd->window = XCreatePixmap(display, rootwin, iw, ih, DefaultDepth(display, screen))) == 0)
+            return 0;
+        /* Save the devDesc* with the window for event dispatching */
+        /* Is this needed? */
+        XSaveContext(display, xd->window, devPtrContext, (caddr_t)dd);
+        xd->npages = 0;
     }
 
     /* Set the graphics context */
@@ -1314,7 +1363,8 @@ static void X11_Clip(double x0, double x1, double y0, double y1, DevDesc *dd)
 
     XSetClipRectangles(display, xd->wgc, 0, 0, &(xd->clip), 1, Unsorted);
 #ifdef XSYNC
-    XSync(display, 0);
+    if (xd->type == WINDOW)
+        XSync(display, 0);
 #endif
 }
 
@@ -1354,6 +1404,13 @@ static void X11_NewPage(DevDesc *dd)
 {
     x11Desc *xd = (x11Desc *)dd->deviceSpecific;
 
+    if (xd->type > WINDOW)
+    {
+        if (xd->npages++)
+            error("attempt to draw second page on pixmap device");
+        return;
+    }
+
     FreeX11Colors();
     if ((model == PSEUDOCOLOR2) || (xd->bg != dd->dp.bg))
     {
@@ -1365,6 +1422,55 @@ static void X11_NewPage(DevDesc *dd)
 #ifdef XSYNC
     XSync(display, 0);
 #endif
+}
+
+extern int R_SaveAsPng(void *d, int width, int height, unsigned long (*gp)(XImage *, int, int), int bgr, FILE *fp);
+
+extern int R_SaveAsJpeg(void *d, int width, int height, unsigned long (*gp)(XImage *, int, int), int bgr, int quality,
+                        FILE *outfile);
+
+static long knowncols[512];
+
+static unsigned long bitgp(XImage *xi, int x, int y)
+{
+    int i, r, g, b;
+    XColor xcol;
+    /*  returns the colour of the (x,y) pixel stored as RGB */
+    i = XGetPixel(xi, y, x);
+    switch (model)
+    {
+    case MONOCHROME:
+        return (i == 0) ? 0xFFFFFF : 0;
+    case GRAYSCALE:
+    case PSEUDOCOLOR1:
+    case PSEUDOCOLOR2:
+        if (i < 512)
+        {
+            if (knowncols[i] >= 0)
+                return knowncols[i];
+            else
+            {
+                xcol.pixel = i;
+                XQueryColor(display, colormap, &xcol);
+                knowncols[i] = ((xcol.red >> 8) << 16) | ((xcol.green >> 8) << 8) | (xcol.blue >> 8);
+                return knowncols[i];
+            }
+        }
+        else
+        {
+            xcol.pixel = i;
+            XQueryColor(display, colormap, &xcol);
+            return ((xcol.red >> 8) << 16) | ((xcol.green >> 8) << 8) | (xcol.blue >> 8);
+        }
+    case TRUECOLOR:
+        r = (i && RMask) >> RShift * 255 / RMask;
+        g = (i && GMask) >> GShift * 255 / GMask;
+        b = (i && BMask) >> BShift * 255 / BMask;
+        return r << 16 | g << 8 | b;
+    default:
+        return 0;
+    }
+    return 0;
 }
 
 /********************************************************/
@@ -1382,15 +1488,35 @@ static void X11_Close(DevDesc *dd)
 #endif
     x11Desc *xd = (x11Desc *)dd->deviceSpecific;
 
-    /* process pending events */
-    /* set block on destroy events */
-    inclose = 1;
-    R_ProcessEvents((void *)NULL);
+    if (xd->type == WINDOW)
+    {
+        /* process pending events */
+        /* set block on destroy events */
+        inclose = 1;
+        R_ProcessEvents((void *)NULL);
 
-    XFreeCursor(display, xd->gcursor);
-    XFreeGC(display, xd->wgc);
-    XDestroyWindow(display, xd->window);
-    XSync(display, 0);
+        XFreeCursor(display, xd->gcursor);
+        XDestroyWindow(display, xd->window);
+        XSync(display, 0);
+    }
+    else
+    {
+        if (xd->npages)
+        {
+            int i;
+            XImage *xi;
+            for (i = 0; i < 512; i++)
+                knowncols[i] = -1;
+            xi = XGetImage(display, xd->window, 0, 0, xd->windowWidth, xd->windowHeight, AllPlanes, ZPixmap);
+            if (xd->type == PNG)
+                R_SaveAsPng(xi, xd->windowWidth, xd->windowHeight, bitgp, 0, xd->fp);
+            else if (xd->type == JPEG)
+                R_SaveAsJpeg(xi, xd->windowWidth, xd->windowHeight, bitgp, 0, xd->quality, xd->fp);
+        }
+        XFreeGC(display, xd->wgc);
+        XFreePixmap(display, xd->window);
+        fclose(xd->fp);
+    }
 
     numX11Devices--;
     if (numX11Devices == 0)
@@ -1437,6 +1563,8 @@ static void X11_Activate(DevDesc *dd)
     char num[3];
     x11Desc *xd = (x11Desc *)dd->deviceSpecific;
 
+    if (xd->type > WINDOW)
+        return;
     strcpy(t, title);
     strcat(t, ": Device ");
     sprintf(num, "%i", deviceNumber(dd) + 1);
@@ -1460,6 +1588,8 @@ static void X11_Deactivate(DevDesc *dd)
     char num[3];
     x11Desc *xd = (x11Desc *)dd->deviceSpecific;
 
+    if (xd->type > WINDOW)
+        return;
     strcpy(t, title);
     strcat(t, ": Device ");
     sprintf(num, "%i", deviceNumber(dd) + 1);
@@ -1517,7 +1647,8 @@ static void X11_Rect(double x0, double y0, double x1, double y1, int coords, int
         XDrawRectangle(display, xd->window, xd->wgc, (int)x0, (int)y0, (int)x1 - (int)x0, (int)y1 - (int)y0);
     }
 #ifdef XSYNC
-    XSync(display, 0);
+    if (xd->type == WINDOW)
+        XSync(display, 0);
 #endif
 }
 
@@ -1591,7 +1722,8 @@ static void X11_Line(double x1, double y1, double x2, double y2, int coords, Dev
     SetLinetype(dd->gp.lty, dd->gp.lwd, dd);
     XDrawLine(display, xd->window, xd->wgc, xx1, yy1, xx2, yy2);
 #ifdef XSYNC
-    XSync(display, 0);
+    if (xd->type == WINDOW)
+        XSync(display, 0);
 #endif
 }
 
@@ -1626,7 +1758,8 @@ static void X11_Polyline(int n, double *x, double *y, int coords, DevDesc *dd)
     SetLinetype(dd->gp.lty, dd->gp.lwd, dd);
     XDrawLines(display, xd->window, xd->wgc, points, n, CoordModeOrigin);
 #ifdef XSYNC
-    XSync(display, 0);
+    if (xd->type == WINDOW)
+        XSync(display, 0);
 #endif
 
     C_free((char *)points);
@@ -1672,7 +1805,8 @@ static void X11_Polygon(int n, double *x, double *y, int coords, int bg, int fg,
         SetColor(bg, dd);
         XFillPolygon(display, xd->window, xd->wgc, points, n, Complex, CoordModeOrigin);
 #ifdef XSYNC
-        XSync(display, 0);
+        if (xd->type == WINDOW)
+            XSync(display, 0);
 #endif
     }
     if (fg != NA_INTEGER)
@@ -1681,7 +1815,8 @@ static void X11_Polygon(int n, double *x, double *y, int coords, int bg, int fg,
         SetLinetype(dd->gp.lty, dd->gp.lwd, dd);
         XDrawLines(display, xd->window, xd->wgc, points, n + 1, CoordModeOrigin);
 #ifdef XSYNC
-        XSync(display, 0);
+        if (xd->type == WINDOW)
+            XSync(display, 0);
 #endif
     }
 
@@ -1710,7 +1845,8 @@ static void X11_Text(double x, double y, int coords, char *str, double rot, doub
     GConvert(&x, &y, coords, DEVICE, dd);
     XRotDrawString(display, xd->font, rot, xd->window, xd->wgc, (int)x, (int)y, str);
 #ifdef XSYNC
-    XSync(display, 0);
+    if (xd->type == WINDOW)
+        XSync(display, 0);
 #endif
 }
 
@@ -1725,8 +1861,12 @@ static int X11_Locator(double *x, double *y, DevDesc *dd)
 {
     XEvent event;
     DevDesc *ddEvent;
+    x11Desc *xd = (x11Desc *)dd->deviceSpecific;
     caddr_t temp;
     int done = 0;
+
+    if (xd->type > WINDOW)
+        return 0;
     R_ProcessEvents((void *)NULL); /* discard pending events */
     XSync(display, 1);
     /* handle X events as normal until get a button */
@@ -1828,10 +1968,8 @@ static void X11_Hold(DevDesc *dd)
 /*    4)  base pointsize		*/
 /*    5)  gamma correction factor	*/
 /*    6)  colormodel                    */
-/*          0 = mono,                   */
-/*          1 = gray,                   */
-/*          2 = color,                  */
-/*          3 = old color.              */
+/*          see defines at top of file  */
+/*    7)  maxcube                    	*/
 
 int X11DeviceDriver(DevDesc *dd, char *display, double width, double height, double pointsize, double gamma,
                     int colormodel, int maxcube)
