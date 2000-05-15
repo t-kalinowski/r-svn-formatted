6b17d57d9bef6fe11b659c49e83bf61ff3e62041
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-05-15 16:44:26 +0000

    (is|as).(symbol|name) clean up
    git-svn-id: https://svn.r-project.org/R/trunk@9223 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/coerce.c
+++ src/main/coerce.c
@@ -1053,7 +1053,7 @@ SEXP do_asvector(SEXP call, SEXP op, SEXP args, SEXP rho)
     if (!isString(CADR(args)) || LENGTH(CADR(args)) < 1)
         errorcall_return(call, R_MSG_mode);
 
-    if (!strcmp("function", (CHAR(STRING(CADR(args))[0]))))
+    if (!strcmp("function", CHAR(STRING(CADR(args))[0])))
         type = CLOSXP;
     else
         type = str2type(CHAR(STRING(CADR(args))[0]));
@@ -1209,7 +1209,7 @@ SEXP do_is(SEXP call, SEXP op, SEXP args, SEXP rho)
     case STRSXP: /* is.character */
         LOGICAL(ans)[0] = (TYPEOF(CAR(args)) == STRSXP);
         break;
-    case SYMSXP: /* is.name */
+    case SYMSXP: /* is.symbol === is.name */
         LOGICAL(ans)[0] = (TYPEOF(CAR(args)) == SYMSXP);
         break;
     case ENVSXP: /* is.environment */
