a172fee99f41601cd2ec97482c8f3d863e31ba34
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-05-29 17:33:00 +0000

    standalone Mathlib
    git-svn-id: https://svn.r-project.org/R/trunk@9363 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qbinom.c
+++ src/nmath/qbinom.c
@@ -32,10 +32,6 @@
 #include "nmath.h"
 #include "dpq.h"
 
-#ifdef DEBUG_qbinom
-#include "PrtUtil.h"
-#endif
-
 double qbinom(double p, double n, double pr, int lower_tail, int log_p)
 {
     double q, mu, sigma, gamma, z, y;
