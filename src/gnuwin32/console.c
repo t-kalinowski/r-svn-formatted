90c40018c287269e2afcd256e41a1b39cc43bdd2
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-07-13 06:30:06 +0000

    gnuwin32 branch update add `Load history' item to Rgui File menu
    git-svn-id: https://svn.r-project.org/R/trunk@9837 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.c
+++ src/gnuwin32/console.c
@@ -1143,10 +1143,9 @@ for (;;)
                 strcpy(buf, cur_line);
                 p->r = -1;
                 cur_line[max_pos] = '\0';
-                if (max_pos && addtohistory && (!NHISTORY || (strcmp(cur_line, HISTORY(0)))))
+                if (max_pos && addtohistory)
                 {
-                    if (p->history->b[0])
-                        xbufadds(p->history, "\n", 0);
+                    xbufadds(p->history, "\n", 0);
                     xbufadds(p->history, cur_line, 0);
                 }
                 xbuffixl(p->lbuf);
