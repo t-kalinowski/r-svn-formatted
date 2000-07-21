a27cde8ca7a77a58d91b52f9db81766720af2405
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-07-21 18:46:44 +0000

    don't save history if workspace is not saved
    git-svn-id: https://svn.r-project.org/R/trunk@9948 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/system.c
+++ src/gnuwin32/system.c
@@ -367,6 +367,8 @@ void R_CleanUp(int saveact, int status, int runLast)
             R_dot_Last();
         if (R_DirtyImage)
             R_SaveGlobalEnv();
+        if (CharacterMode == RGui || (R_Interactive && CharacterMode == RTerm))
+            gl_savehistory(R_HistoryFile);
         break;
     case SA_NOSAVE:
         if (runLast)
@@ -380,8 +382,6 @@ void R_CleanUp(int saveact, int status, int runLast)
     closeAllHlpFiles();
     KillAllDevices();
     AllDevicesKilled = 1;
-    if (CharacterMode == RGui || (R_Interactive && CharacterMode == RTerm))
-        gl_savehistory(R_HistoryFile);
     if (R_Interactive && CharacterMode == RTerm)
         SetConsoleTitle("");
     UnLoad_Unzip_Dll();
