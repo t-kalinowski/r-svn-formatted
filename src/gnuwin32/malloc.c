c666113c496579499403ab064bc2b0ee108e60f4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-29 09:10:21 +0000

    remove (one) possibility of infinite loop in wsbrk increase initial reservation to 256Mb update docs on this
    git-svn-id: https://svn.r-project.org/R/trunk@11091 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/malloc.c
+++ src/gnuwin32/malloc.c
@@ -1038,14 +1038,12 @@ static void *findRegion(void *start_address, unsigned long size)
     while ((unsigned long)start_address < TOP_MEMORY)
     {
         VirtualQuery(start_address, &info, sizeof(info));
-        if (info.State != MEM_FREE)
-            start_address = (char *)info.BaseAddress + info.RegionSize;
-        else if (info.RegionSize >= size)
+        if (info.State == MEM_FREE && info.RegionSize >= size)
             return start_address;
         else
         {
-            /*	    Requested region is not available so see if the
-                    next region is available.  Set 'start_address'
+            /*	    Requested region is not available or not big enough
+                    so see if the next region is available.  Set 'start_address'
                     to the next region and call 'VirtualQuery()' again. */
 
             start_address = (char *)info.BaseAddress + info.RegionSize;
@@ -1068,9 +1066,11 @@ static void *findRegion(void *start_address, unsigned long size)
     return NULL;
 }
 
+/* BDR 2000-10-28: add loop count as this infinitely looped */
 void *wsbrk(long size)
 {
     void *tmp;
+    int count = 0;
     if (PageSize == 0)
         PageSize = getpagesize();
 
@@ -1084,17 +1084,20 @@ void *wsbrk(long size)
            try to reserve the address space (never unreserved) */
         if (gAddressBase == 0)
         {
-            gReservedSize = max(RESERVED_SIZE, AlignPage(size));
+            gReservedSize = max(RESERVED_SIZE, AlignPage64K(size));
             gNextAddress = gAddressBase = (unsigned int)VirtualAlloc(NULL, gReservedSize, MEM_RESERVE, PAGE_NOACCESS);
             if (!gAddressBase)
                 R_Suicide("unable to reserve initial space in wsbrk");
         }
         else if (AlignPage(gNextAddress + size) > (gAddressBase + gReservedSize))
         {
-            long new_size = max(NEXT_SIZE, AlignPage(size));
+            long new_size = max(NEXT_SIZE, AlignPage64K(size));
             void *new_address = (void *)(gAddressBase + gReservedSize);
             do
             {
+                /* if we failed twice, try somewhat desparately */
+                if (count >= 2)
+                    new_address = (void *)((unsigned int)new_address + new_size + NEXT_SIZE);
                 new_address = findRegion(new_address, new_size);
                 if (new_address == 0)
                     return (void *)-1;
@@ -1102,7 +1105,9 @@ void *wsbrk(long size)
                 /* repeat in case of race condition
                   The region that we found has been grabbed
                   by another thread */
-            } while (gNextAddress == 0);
+            } while (gNextAddress == 0 && count++ < 10);
+            if (gNextAddress == 0)
+                return (void *)-1;
 
             if (new_address != (void *)gNextAddress)
                 R_Suicide("internal error in wsbrk");
