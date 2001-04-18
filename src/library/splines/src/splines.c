a58b31ab3468835a3ad77fed872f7621a2119d24
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-18 10:21:25 +0000

    declarations added; drop never-used lin_interp()
    git-svn-id: https://svn.r-project.org/R/trunk@13604 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/splines/src/splines.c
+++ src/library/splines/src/splines.c
@@ -40,6 +40,10 @@ typedef struct spl_struct
         *a;       /* scratch array */
 } * splPTR;
 
+/* Exports */
+SEXP spline_basis(SEXP knots, SEXP order, SEXP xvals, SEXP derivs);
+SEXP spline_value(SEXP knots, SEXP coeff, SEXP order, SEXP x, SEXP deriv);
+
 static void /* free storage from a spl_struct */
     splFree(splPTR this)
 {
@@ -50,10 +54,9 @@ static void /* free storage from a spl_struct */
     Free(this);
 }
 
-static int /* set sp->curs to the index of the first
-          knot position > x.   Special handling
-          for x == sp->knots[sp->nknots - sp-order + 1] */
-set_cursor(splPTR sp, double x)
+/* set sp->curs to the index of the first knot position > x.
+   Special handling for x == sp->knots[sp->nknots - sp-order + 1] */
+static int set_cursor(splPTR sp, double x)
 {
     int i;
 
@@ -245,29 +248,4 @@ SEXP spline_basis(SEXP knots, SEXP order, SEXP xvals, SEXP derivs)
     setAttrib(val, install("Offsets"), offsets);
     UNPROTECT(6);
     return val;
-}
-
-void lin_interp(double *x, double *y, double *x0, double *y0, int *nvals)
-{
-    int n = *nvals;
-    double *firstx = x;
-
-    while (n--)
-    {
-        while (*x < *x0)
-        {
-            x++;
-            y++;
-        }
-        if (x > firstx)
-        {
-            x--;
-            y--;
-        }
-        if (*x > *x0)
-            *y0++ = *y + (*(y + 1) - *y) * (*x0 - *x) / (*(x + 1) - *x);
-        else
-            *y0++ = *y;
-        x0++;
-    }
 }
