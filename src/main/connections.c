42bca3c6c8b6e5ec32839f7fc1cda1ff882b10d0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-05-07 10:02:59 +0000

    tweak over-riding of defaults
    git-svn-id: https://svn.r-project.org/R/trunk@14013 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -562,6 +562,10 @@ static void gzfile_open(Rconnection con)
     con->isopen = TRUE;
     con->canwrite = (con->mode[0] == 'w' || con->mode[0] == 'a');
     con->canread = !con->canwrite;
+    if (strlen(con->mode) >= 2 && con->mode[1] == 'b')
+        con->text = FALSE;
+    else
+        con->text = TRUE;
     con->save = -1000;
 }
 
@@ -944,7 +948,7 @@ static Rconnection newtext(char *description, SEXP text)
     }
     init_con(new, description, "r");
     new->isopen = TRUE;
-    new->canread = TRUE;
+    new->canwrite = FALSE;
     new->open = &text_open;
     new->close = &text_close;
     new->destroy = &text_destroy;
@@ -1111,7 +1115,7 @@ static Rconnection newouttext(char *description, SEXP sfile, char *mode)
     }
     init_con(new, description, mode);
     new->isopen = TRUE;
-    new->canwrite = TRUE;
+    new->canread = FALSE;
     new->open = &text_open;
     new->close = &outtext_close;
     new->destroy = &outtext_destroy;
@@ -2679,7 +2683,6 @@ static Rconnection newurl(char *description, char *mode)
     }
     init_con(new, description, mode);
     new->canwrite = FALSE;
-    new->canseek = FALSE;
     new->open = &url_open;
     new->close = &url_close;
     new->fgetc = &url_fgetc;
