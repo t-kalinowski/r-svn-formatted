d78c8a6f03a33cd623e2441b33eaa5759287ba48
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-01 18:55:05 +0000

    Added running of onexit code to browser Q excape. Consolidated running of onexit code into  R_run_onexits.
    git-svn-id: https://svn.r-project.org/R/trunk@14478 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -573,6 +573,9 @@ static int ParseBrowser(SEXP CExpr, SEXP rho)
         }
         if (!strcmp(CHAR(PRINTNAME(CExpr)), "Q"))
         {
+            /* run onexit/cend code for everything on the stack (for now) */
+            R_run_onexits(NULL);
+            /* this is really dynamic state that should be managed as such */
             R_BrowseLevel = 0;
             LONGJMP(R_Toplevel.cjmpbuf, CTXT_TOPLEVEL);
         }
