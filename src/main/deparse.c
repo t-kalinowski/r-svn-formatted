3c9468011bea18bac8f59712b47b68caf2d23baf
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-11 07:05:25 +0000

    deparse(<empty df>)
    git-svn-id: https://svn.r-project.org/R/trunk@14613 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1997--2000  Robert Gentleman, Ross Ihaka and the
+ *  Copyright (C) 1997--2001  Robert Gentleman, Ross Ihaka and the
  *			      R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -497,16 +497,11 @@ static void deparse2buff(SEXP s)
         print2buff("<environment>");
         break;
     case VECSXP:
-        if (length(s) <= 0)
-            print2buff("list()");
-        else
-        {
-            attr1(s);
-            print2buff("list(");
-            vec2buff(s);
-            print2buff(")");
-            attr2(s);
-        }
+        attr1(s);
+        print2buff("list(");
+        vec2buff(s);
+        print2buff(")");
+        attr2(s);
         break;
     case EXPRSXP:
         if (length(s) <= 0)
