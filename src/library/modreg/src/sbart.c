06355c2f680ec4fd146bce2bbd2c86b6583467c1
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-10-06 21:51:40 +0000

    fix nan -- better tracing
    git-svn-id: https://svn.r-project.org/R/trunk@16245 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/modreg/src/sbart.c
+++ src/library/modreg/src/sbart.c
@@ -81,6 +81,11 @@ void F77_SUB(sbart)(double *penalt, double *dofoff, double *xs, double *ys, doub
                             where  L'L = [X'WX + lambda*SIGMA]  NOT REFERENCED
     */
 
+#define CRIT(FX) (*icrit == 3 ? FX - 3. : FX)
+    /* cancellation in (3 + eps) - 3, but still...informative */
+
+#define BIG_f (1e100)
+
     /* c_Gold is the squared inverse of the golden ratio */
     static const double c_Gold = 0.381966011250105151795413165634;
     /* == (3. - sqrt(5.)) / 2. */
@@ -93,10 +98,11 @@ void F77_SUB(sbart)(double *penalt, double *dofoff, double *xs, double *ys, doub
     double t1, t2, tol1, tol2;
 
     int i, maxit;
-    Rboolean tracing = (*ispar < 0);
+    Rboolean Fparabol = FALSE, tracing = (*ispar < 0);
 
     /* unnecessary initializations to keep  -Wall happy */
     d = 0.;
+    fu = 0.;
     u = 0.;
     ratio = 1.;
 
@@ -137,248 +143,248 @@ void F77_SUB(sbart)(double *penalt, double *dofoff, double *xs, double *ys, doub
         F77_CALL(sslvrg)
         (penalt, dofoff, xs, ys, ws, ssw, n, knot, nk, coef, sz, lev, crit, icrit, lspar, xwy, hs0, hs1, hs2, hs3, sg0,
          sg1, sg2, sg3, abd, p1ip, p2ip, ld4, ldnk, ier);
-
         /* got through check 2 */
+        return;
     }
-    else
-    {
-        /*  ---- spar not supplied --> compute it ! -------------------------
 
-               Use Forsythe Malcom and Moler routine to minimise criterion
-               f denotes the value of the criterion
+    /* ELSE ---- spar not supplied --> compute it ! ---------------------------
 
-               an approximation	x  to the point where	f  attains a minimum  on
-               the interval  (ax,bx)  is determined.
+           Use Forsythe Malcom and Moler routine to MINIMIZE criterion
+           f denotes the value of the criterion
+
+           an approximation	x  to the point where	f  attains a minimum  on
+           the interval  (ax,bx)  is determined.
         */
-        ax = *lspar;
-        bx = *uspar;
+    ax = *lspar;
+    bx = *uspar;
 
-        /* INPUT
+    /* INPUT
 
-           ax	 left endpoint of initial interval
-           bx	 right endpoint of initial interval
-           f	 function subprogram which evaluates  f(x)  for any  x
-                 in the interval  (ax,bx)
-           tol	 desired length of the interval of uncertainty of the final
-                 result ( >= 0 )
+       ax	 left endpoint of initial interval
+       bx	 right endpoint of initial interval
+       f	 function subprogram which evaluates  f(x)  for any  x
+             in the interval  (ax,bx)
+       tol	 desired length of the interval of uncertainty of the final
+             result ( >= 0 )
 
-           OUTPUT
+       OUTPUT
 
-           fmin	 abcissa approximating the point where	f  attains a minimum
-        */
+       fmin	 abcissa approximating the point where	f  attains a minimum
+    */
 
-        /*
-           The method used is a combination of  golden  section  search  and
-           successive parabolic interpolation.	convergence is never much slower
-           than	 that  for  a  fibonacci search.  if  f	 has a continuous second
-           derivative which is positive at the minimum (which is not  at  ax  or
-           bx),	 then  convergence  is	superlinear, and usually of the order of
-           about  1.324....
-            the function  f  is never evaluated at two points closer together
-           than	 eps*abs(fmin) + (tol/3), where eps is	approximately the square
-           root	 of  the  relative  machine  precision.	  if   f   is a unimodal
-           function and the computed values of	 f   are  always  unimodal  when
-           separated by at least  eps*abs(x) + (tol/3), then  fmin  approximates
-           the abcissa of the global minimum of	 f  on the interval  ax,bx  with
-           an error less than  3*eps*abs(fmin) + tol.  if   f	is not unimodal,
-           then fmin may approximate a local, but perhaps non-global, minimum to
-           the same accuracy.
-            this function subprogram is a slightly modified	version	 of  the
-           algol  60 procedure	localmin  given in richard brent, algorithms for
-           minimization without derivatives, prentice - hall, inc. (1973).
+    /*
+       The method used is a combination of  golden  section  search  and
+       successive parabolic interpolation.	convergence is never much slower
+       than	 that  for  a  fibonacci search.  if  f	 has a continuous second
+       derivative which is positive at the minimum (which is not  at  ax  or
+       bx),	 then  convergence  is	superlinear, and usually of the order of
+       about  1.324....
+        the function  f  is never evaluated at two points closer together
+       than	 eps*abs(fmin) + (tol/3), where eps is	approximately the square
+       root	 of  the  relative  machine  precision.	  if   f   is a unimodal
+       function and the computed values of	 f   are  always  unimodal  when
+       separated by at least  eps*abs(x) + (tol/3), then  fmin  approximates
+       the abcissa of the global minimum of	 f  on the interval  ax,bx  with
+       an error less than  3*eps*abs(fmin) + tol.  if   f	is not unimodal,
+       then fmin may approximate a local, but perhaps non-global, minimum to
+       the same accuracy.
+        this function subprogram is a slightly modified	version	 of  the
+       algol  60 procedure	localmin  given in richard brent, algorithms for
+       minimization without derivatives, prentice - hall, inc. (1973).
 
-           DOUBLE precision	 a,b,c,d,e,eps,xm,p,q,r,tol1,tol2,u,v,w
-           DOUBLE precision	 fu,fv,fw,fx,x
-        */
+       Double	 a,b,c,d,e,eps,xm,p,q,r,tol1,tol2,u,v,w
+       Double	 fu,fv,fw,fx,x
+    */
 
-        /*  eps is approximately the square root of the relative machine
-            precision.
+    /*  eps is approximately the square root of the relative machine
+        precision.
 
-            -	 eps = 1e0
-            - 10	 eps = eps/2e0
-            -	 tol1 = 1e0 + eps
-            -	 if (tol1 > 1e0) go to 10
-            -	 eps = sqrt(eps)
-            R Version <= 1.3.x had
-            eps = .000244     ( = sqrt(5.954 e-8) )
-             -- now eps is passed as argument
-        */
+        -	 eps = 1e0
+        - 10	 eps = eps/2e0
+        -	 tol1 = 1e0 + eps
+        -	 if (tol1 > 1e0) go to 10
+        -	 eps = sqrt(eps)
+        R Version <= 1.3.x had
+        eps = .000244     ( = sqrt(5.954 e-8) )
+         -- now eps is passed as argument
+    */
 
-        /* initialization */
+    /* initialization */
 
-        maxit = *iter;
-        *iter = 0;
-        a = ax;
-        b = bx;
-        v = a + c_Gold * (b - a);
-        w = v;
-        x = v;
-        e = 0.f;
-        *spar = x;
+    maxit = *iter;
+    *iter = 0;
+    a = ax;
+    b = bx;
+    v = a + c_Gold * (b - a);
+    w = v;
+    x = v;
+    e = 0.;
+    *spar = x;
+    *lspar = ratio * R_pow(16., *spar * 6. - 2.);
+    F77_CALL(sslvrg)
+    (penalt, dofoff, xs, ys, ws, ssw, n, knot, nk, coef, sz, lev, crit, icrit, lspar, xwy, hs0, hs1, hs2, hs3, sg0, sg1,
+     sg2, sg3, abd, p1ip, p2ip, ld4, ldnk, ier);
+    fx = *crit;
+    fv = fx;
+    fw = fx;
+
+    /* main loop
+       --------- */
+    while (*ier == 0)
+    { /* L20: */
+        xm = (a + b) * .5;
+        tol1 = *eps * fabs(x) + *tol / 3.;
+        tol2 = tol1 * 2.;
+        ++(*iter);
+
+        if (tracing)
+        {
+            if (*iter == 1)
+            { /* write header */
+                Rprintf("sbart (ratio = %15.8g) iterations;"
+                        " initial tol1 = %12.6e :\n"
+                        "%11s %14s  %9s %11s  Kind %11s %12s\n%s\n",
+                        ratio, tol1, "x  ",
+                        ((*icrit == 1) ? "GCV"
+                                       : (*icrit == 2) ? "CV"
+                                                       : (*icrit == 3) ? "(df0-df)^2" :
+                                                                       /*else (should not happen) */ "?f?"),
+                        "b - a", "e", "NEW lspar", "crit",
+                        " ---------------------------------------"
+                        "----------------------------------------");
+            }
+            Rprintf("%11.8f %14.9g %9.4e %11.5g", x, CRIT(fx), b - a, e);
+            Fparabol = FALSE;
+        }
+
+        /* Check the (somewhat peculiar) stopping criterion: note that
+           the RHS is negative as long as the interval [a,b] is not small:*/
+        if (fabs(x - xm) <= tol2 - (b - a) * .5 || *iter > maxit)
+            goto L_End;
+
+        /* is golden-section necessary */
+
+        if (fabs(e) <= tol1 ||
+            /*  if had Inf then go to golden-section */
+            fx >= BIG_f || fv >= BIG_f || fw >= BIG_f)
+            goto L_GoldenSect;
+
+        /* Fit Parabola */
+        if (tracing)
+        {
+            Rprintf(" FP");
+            Fparabol = TRUE;
+        }
+
+        r = (x - w) * (fx - fv);
+        q = (x - v) * (fx - fw);
+        p = (x - v) * q - (x - w) * r;
+        q = (q - r) * 2.;
+        if (q > 0.)
+            p = -p;
+        q = fabs(q);
+        r = e;
+        e = d;
+
+        /* is parabola acceptable?  Otherwise do golden-section */
+
+        if (fabs(p) >= fabs(.5 * q * r) || q == 0.)
+            /* above line added by BDR;
+             * [the abs(.) >= abs() = 0 should have branched..]
+             * in FTN: COMMON above ensures q is NOT a register variable */
+
+            goto L_GoldenSect;
+
+        if (p <= q * (a - x) || p >= q * (b - x))
+            goto L_GoldenSect;
+
+        /* Parabolic Interpolation step */
+
+        if (tracing)
+            Rprintf(" PI ");
+        d = p / q;
+        if (!R_FINITE(d))
+            REprintf(" !FIN(d:=p/q): ier=%d, (v,w, p,q)= %g, %g, %g, %g\n", *ier, v, w, p, q);
+        u = x + d;
+
+        /* f must not be evaluated too close to ax or bx */
+        if (u - a < tol2 || b - u < tol2)
+            d = fsign(tol1, xm - x);
+
+        goto L50;
+        /*------*/
+
+    L_GoldenSect: /* a golden-section step */
+
+        if (tracing)
+            Rprintf(" GS%s ", Fparabol ? "" : " --");
+
+        if (x >= xm)
+            e = a - x;
+        else /* x < xm*/
+            e = b - x;
+        d = c_Gold * e;
+
+    L50:
+        u = x + ((fabs(d) >= tol1) ? d : fsign(tol1, d));
+        /*  tol1 check : f must not be evaluated too close to x */
+
+        *spar = u;
         *lspar = ratio * R_pow(16., *spar * 6. - 2.);
         F77_CALL(sslvrg)
         (penalt, dofoff, xs, ys, ws, ssw, n, knot, nk, coef, sz, lev, crit, icrit, lspar, xwy, hs0, hs1, hs2, hs3, sg0,
          sg1, sg2, sg3, abd, p1ip, p2ip, ld4, ldnk, ier);
-        fx = *crit;
-        fv = fx;
-        fw = fx;
+        fu = *crit;
+        if (tracing)
+            Rprintf("%11g %12g\n", *lspar, CRIT(fu));
+        if (!R_FINITE(fu))
+        {
+            REprintf("spar-finding: non-finite value %g; using BIG value\n", fu);
+            fu = 2. * BIG_f;
+        }
 
-        /* main loop
-           --------- */
-        while (*ier == 0)
-        { /* L20: */
-            xm = (a + b) * .5f;
-            tol1 = *eps * fabs(x) + *tol / 3.f;
-            tol2 = tol1 * 2.f;
-            ++(*iter);
+        /*  update  a, b, v, w, and x */
 
-            if (tracing)
-            {
+        if (fu <= fx)
+        {
+            if (u >= x)
+                a = x;
+            else
+                b = x;
 
-                if (*iter == 1)
-                { /* write header */
-                    Rprintf("sbart (ratio = %15.8g) iterations:\n"
-                            "%11s %14s %10s %11s Kind %11s %12s\n%s\n",
-                            ratio, "x  ",
-                            ((*icrit == 1) ? "GCV"
-                                           : (*icrit == 2) ? "CV"
-                                                           : (*icrit == 3) ? "(df0-df)^2" :
-                                                                           /*else (should not happen) */ "?fx?"),
-                            "b - a ", "e", "lspar", "crit",
-                            " -----------------------------------------"
-                            "-------------------");
-                }
-                t1 = fx;
-                if (*icrit == 3)
-                    t1 -= 3.;
-                /* cancellation in (3 + eps) - 3, but still more informative */
-                Rprintf("%11.8f %14.9g %10.5g %11.5g", x, t1, b - a, e);
-            }
+            v = w;
+            fv = fw;
+            w = x;
+            fw = fx;
+            x = u;
+            fx = fu;
+        }
+        else
+        {
+            if (u < x)
+                a = u;
+            else
+                b = u;
 
-            /* Check the (somewhat peculiar) stopping criterion: note that
-               the RHS is negative as long as the interval [a,b] is not small:*/
-            if (fabs(x - xm) <= tol2 - (b - a) * .5 || *iter > maxit)
-                goto L_End;
-
-            /* is golden-section necessary */
-
-            if (fabs(e) <= tol1 ||
-                /*  if had Inf then go to golden-section */
-                fx >= 1e100 || fv >= 1e100 || fw >= 1e100)
-                goto L_GoldenSect;
-
-            /* Fit Parabola */
-            if (tracing)
-                Rprintf(" FP ");
-
-            r = (x - w) * (fx - fv);
-            q = (x - v) * (fx - fw);
-            p = (x - v) * q - (x - w) * r;
-            q = (q - r) * 2.f;
-            if (q > 0.)
-                p = -p;
-            q = fabs(q);
-            r = e;
-            e = d;
-
-            /* is parabola acceptable?  Otherwise do golden-section */
-
-            if (fabs(p) >= fabs(.5 * q * r) || q == 0.)
-                /* above line added by BDR;
-                 * [the abs(.) >= abs() = 0 should have branched..]
-                 * in FTN: COMMON above ensures q is NOT a register variable */
-
-                goto L_GoldenSect;
-
-            if (p <= q * (a - x) || p >= q * (b - x))
-                goto L_GoldenSect;
-
-            /* Parabolic Interpolation step */
-
-            if (tracing)
-                Rprintf(" PI ");
-            d = p / q;
-            if (!R_FINITE(d))
-                REprintf(" !FIN(d:=p/q): ier=%d, (v,w, p,q)= %g, %g, %g, %g\n", *ier, v, w, p, q);
-            u = x + d;
-
-            /* f must not be evaluated too close to ax or bx */
-            if (u - a < tol2 || b - u < tol2)
-                d = fsign(tol1, xm - x);
-
-            goto L50;
-            /*------*/
-
-            /*	a golden-section step */
-
-        L_GoldenSect:
-            if (tracing)
-                Rprintf(" GS ");
-
-            if (x >= xm)
-                e = a - x;
-            else /* x < xm*/
-                e = b - x;
-            d = c_Gold * e;
-
-        L50:
-            u = x + ((fabs(d) >= tol1) ? d : fsign(tol1, d));
-            /*  tol1 check : f must not be evaluated too close to x */
-
-            *spar = u;
-            *lspar = ratio * R_pow(16., *spar * 6. - 2.);
-            F77_CALL(sslvrg)
-            (penalt, dofoff, xs, ys, ws, ssw, n, knot, nk, coef, sz, lev, crit, icrit, lspar, xwy, hs0, hs1, hs2, hs3,
-             sg0, sg1, sg2, sg3, abd, p1ip, p2ip, ld4, ldnk, ier);
-            fu = *crit;
-            if (tracing)
-                Rprintf("%11g %12g\n", *lspar, fu);
-
-            /*  update  a, b, v, w, and x */
-
-            if (fu <= fx)
-            {
-                if (u >= x)
-                    a = x;
-                else
-                    b = x;
-
+            if (fu <= fw || w == x)
+            { /* L70: */
                 v = w;
                 fv = fw;
-                w = x;
-                fw = fx;
-                x = u;
-                fx = fu;
+                w = u;
+                fw = fu;
             }
-            else
-            {
-                if (u < x)
-                    a = u;
-                else
-                    b = u;
-
-                if (fu <= fw || w == x)
-                { /* L70: */
-                    v = w;
-                    fv = fw;
-                    w = u;
-                    fw = fu;
-                }
-                else if (fu <= fv || v == x || v == w)
-                { /* L80: */
-                    v = u;
-                    fv = fu;
-                }
+            else if (fu <= fv || v == x || v == w)
+            { /* L80: */
+                v = u;
+                fv = fu;
             }
-        } /* end main loop -- goto L20; */
+        }
+    } /* end main loop -- goto L20; */
 
-    L_End:
-        if (tracing)
-            Rprintf(" >>>> %11g => %11g\n", fu, fx);
-        *spar = x;
-        *crit = fx;
-
-    } /* end *spar computation */
-
+L_End:
+    if (tracing)
+        Rprintf(" >>> %11g => %11g\n", CRIT(fu), CRIT(fx));
+    *spar = x;
+    *crit = fx;
     return;
-} /* sbart_ */
+} /* sbart */
