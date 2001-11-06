1a0fdd2e538f0e7f5f09b74f1a0deb2e808c3ce6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-06 13:23:08 +0000

    comment out unused code
    git-svn-id: https://svn.r-project.org/R/trunk@16635 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/objects.c
+++ src/main/objects.c
@@ -961,6 +961,7 @@ static SEXP dispatchNonGeneric(SEXP name, SEXP env)
     return value;
 }
 
+#ifdef UNUSED
 static void load_methods_package()
 {
     SEXP e;
@@ -971,6 +972,7 @@ static void load_methods_package()
     eval(e, R_GlobalEnv);
     UNPROTECT(1);
 }
+#endif
 
 SEXP do_standardGeneric(SEXP call, SEXP op, SEXP args, SEXP env)
 {
