072d2cc76085067c020b1b436ed11860e845ada2
iacus <iacus@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-26 19:55:07 +0000

    added "break" in switch sequences where it was missing, otherwise MRC complains
    git-svn-id: https://svn.r-project.org/R/trunk@16896 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/engine.c
+++ src/main/engine.c
@@ -332,6 +332,7 @@ double toDeviceY(double value, GEUnit from, GEDevDesc *dd)
         result = dd->dev->bottom + result * (dd->dev->top - dd->dev->bottom);
     case GE_DEVICE:
         /* Do nothing */
+        break;
     }
     return result;
 }
@@ -371,6 +372,7 @@ double toDeviceWidth(double value, GEUnit from, GEDevDesc *dd)
         result = result * (dd->dev->right - dd->dev->left);
     case GE_DEVICE:
         /* Do nothing */
+        break;
     }
     return result;
 }
