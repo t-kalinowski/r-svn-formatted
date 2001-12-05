23900d8d014a501aa16359243613d8993932021a
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-12-05 00:05:35 +0000

    Allow par(gamma=)
    git-svn-id: https://svn.r-project.org/R/trunk@17173 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/devga.c
+++ src/gnuwin32/devga.c
@@ -2365,6 +2365,7 @@ Rboolean GADeviceDriver(NewDevDesc *dd, char *display, double width, double heig
     dd->canResizeText = TRUE;
     dd->canClip = TRUE;
     dd->canHAdj = 1; /* 0, 0.5, 1 */
+    dd->canChangeGamma = TRUE;
 
     /* initialise device description (most of the work */
     /* has been done in GA_Open) */
