46d077b1dcce3cedd75da2f1db62699500cc0e2a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-08 09:46:49 +0000

    unz_open now returns Rboolean
    git-svn-id: https://svn.r-project.org/R/trunk@18584 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/dounzip.c
+++ src/main/dounzip.c
@@ -254,26 +254,41 @@ SEXP do_int_unzip(SEXP call, SEXP op, SEXP args, SEXP env)
 
 #include <Rconnections.h>
 
-static void unz_open(Rconnection con)
+static Rboolean unz_open(Rconnection con)
 {
     unzFile uf;
     char path[2 * PATH_MAX], *p;
 
     if (con->mode[0] != 'r')
-        error("unz connections can only be opened for reading");
+    {
+        warning("unz connections can only be opened for reading");
+        return FALSE;
+    }
     p = R_ExpandFileName(con->description);
     if (strlen(p) > PATH_MAX - 1)
-        error("zip path is too long");
+    {
+        warning("zip path is too long");
+        return FALSE;
+    }
     strcpy(path, p);
     p = strrchr(path, ':');
     if (!p)
-        error("invalid description of unz connection");
+    {
+        warning("invalid description of unz connection");
+        return FALSE;
+    }
     *p = '\0';
     uf = unzOpen(path);
     if (!uf)
-        error("cannot open zip file `%s'", path);
+    {
+        warning("cannot open zip file `%s'", path);
+        return FALSE;
+    }
     if (unzLocateFile(uf, p + 1, 0) != UNZ_OK)
-        error("cannot locate file `%s' in zip file `%s'", p + 1, path);
+    {
+        warning("cannot locate file `%s' in zip file `%s'", p + 1, path);
+        return FALSE;
+    }
     unzOpenCurrentFile(uf);
     ((Runzconn)(con->private))->uf = uf;
     con->isopen = TRUE;
@@ -284,6 +299,7 @@ static void unz_open(Rconnection con)
     else
         con->text = TRUE;
     con->save = -1000;
+    return FALSE;
 }
 
 static void unz_close(Rconnection con)
