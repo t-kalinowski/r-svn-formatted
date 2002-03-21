37e9173be37baae4be1702fe585247f142a75050
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-21 13:22:43 +0000

    we have a test for getcwd, so use it
    git-svn-id: https://svn.r-project.org/R/trunk@18808 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/dynload.c
+++ src/unix/dynload.c
@@ -271,7 +271,7 @@ static void getFullDLLPath(SEXP call, char *buf, char *path)
         strcpy(buf, R_ExpandFileName(path));
     else if (path[0] != '/')
     {
-#ifdef HAVE_UNISTD_H
+#ifdef HAVE_GETCWD
         if (!getcwd(buf, PATH_MAX))
 #endif
             errorcall(call, "can't get working directory!");
