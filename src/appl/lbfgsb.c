0a66f71971cc988da2616d6d38612e9264e6c5f7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-05-15 14:49:58 +0000

    remove unused variable (and warning)
    git-svn-id: https://svn.r-project.org/R/trunk@19696 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/lbfgsb.c
+++ src/appl/lbfgsb.c
@@ -1487,7 +1487,7 @@ static void cauchy(int n, double *x, double *l, double *u, int *nbd, double *g, 
     */
 
     /* System generated locals */
-    int wy_dim1, wy_offset, ws_dim1, ws_offset, sy_dim1, sy_offset, wt_dim1, wt_offset, i__1, i__2;
+    int wy_dim1, wy_offset, ws_dim1, ws_offset, sy_dim1, sy_offset, wt_dim1, wt_offset, i__2;
     double d__1;
 
     /* Local variables */
