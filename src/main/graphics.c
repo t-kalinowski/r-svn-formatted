daf5576080a482749f001a2e77d5a109c5db8edf
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-10-18 22:56:46 +0000

    Fixed a memory leak in GPolygon.  Added vmemget and vmemset calls.
    git-svn-id: https://svn.r-project.org/R/trunk@21573 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/graphics.c
+++ src/main/graphics.c
@@ -3309,6 +3309,7 @@ void GPolygon(int n, double *x, double *y, int coords, int bg, int fg, DevDesc *
     int i;
     double *xx;
     double *yy;
+    char *vmaxsave = vmaxget();
 
     if (Rf_gpptr(dd)->lty == LTY_BLANK)
         fg = NA_INTEGER; /* transparent for the border */
@@ -3333,6 +3334,7 @@ void GPolygon(int n, double *x, double *y, int coords, int bg, int fg, DevDesc *
     }
     else
         clipPolygon(n, xx, yy, DEVICE, bg, fg, 0, dd);
+    vmaxset(vmaxsave);
 }
 
 #include <stdio.h>
