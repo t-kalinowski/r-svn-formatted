d8e02f365b5ce0495189eb5ad7e3f19963bf381c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-11-30 09:31:45 +0000

    separate out altered LAPACK files
    git-svn-id: https://svn.r-project.org/R/trunk@22132 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/Lapack.c
+++ src/modules/lapack/Lapack.c
@@ -264,10 +264,10 @@ static SEXP modLa_rg(SEXP x, SEXP only_values)
     wI = (double *)R_alloc(n, sizeof(double));
     /* ask for optimal size of work array */
     lwork = -1;
-    F77_CALL(dgeev)(jobVL, jobVR, &n, xvals, &n, wR, wI, left, &n, right, &n, &tmp, &lwork, &info);
+    F77_CALL(rgeev)(jobVL, jobVR, &n, xvals, &n, wR, wI, left, &n, right, &n, &tmp, &lwork, &info);
     lwork = (int)tmp;
     work = (double *)R_alloc(lwork, sizeof(double));
-    F77_CALL(dgeev)(jobVL, jobVR, &n, xvals, &n, wR, wI, left, &n, right, &n, work, &lwork, &info);
+    F77_CALL(rgeev)(jobVL, jobVR, &n, xvals, &n, wR, wI, left, &n, right, &n, work, &lwork, &info);
     if (info != 0)
         error("error code %d from Lapack routine dgeev", info);
 
