de379be8866e5f4f4ca0dd9e5f010c38cb891f8b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-12-30 10:04:27 +0000

    add `envir' argument to dump()
    git-svn-id: https://svn.r-project.org/R/trunk@22515 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -322,7 +322,7 @@ SEXP do_dput(SEXP call, SEXP op, SEXP args, SEXP rho)
 
 SEXP do_dump(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
-    SEXP file, names, o, objs, tval;
+    SEXP file, names, o, objs, tval, source;
     int i, j, nobjs, res;
     Rboolean wasopen, havewarned = FALSE;
     Rconnection con;
@@ -336,15 +336,19 @@ SEXP do_dump(SEXP call, SEXP op, SEXP args, SEXP rho)
     nobjs = length(names);
     if (nobjs < 1 || length(file) < 1)
         errorcall(call, "zero length argument");
+    source = CADDR(args);
+    if (source != R_NilValue && TYPEOF(source) != ENVSXP)
+        error("bad environment");
 
     PROTECT(o = objs = allocList(nobjs));
 
-    for (i = 0; i < nobjs; i++)
+    for (j = 0; j < nobjs; j++, o = CDR(o))
     {
-        SETCAR(o, eval(install(CHAR(STRING_ELT(names, i))), rho));
-        o = CDR(o);
+        SET_TAG(o, install(CHAR(STRING_ELT(names, j))));
+        SETCAR(o, findVar(TAG(o), source));
+        if (CAR(o) == R_UnboundValue)
+            error("Object \"%s\" not found", CHAR(PRINTNAME(TAG(o))));
     }
-
     o = objs;
     if (INTEGER(file)[0] == 1)
     {
