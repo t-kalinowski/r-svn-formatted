1be65a2d40c8af249396d943a31fd8ec1715e582
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-01-28 02:04:44 +0000

    Doc improvements & forward slash handling in choose.files()
    git-svn-id: https://svn.r-project.org/R/trunk@22890 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/extra.c
+++ src/gnuwin32/extra.c
@@ -1111,6 +1111,13 @@ SEXP do_chooseFiles(SEXP call, SEXP op, SEXP args, SEXP rho)
     filterindex = asInteger(CAD4R(args));
     if (length(def) != 1)
         errorcall(call, "default must be a character string");
+    strcpy(path, CHAR(STRING_ELT(def, 0)));
+    temp = strchr(path, '/');
+    while (temp)
+    {
+        *temp = '\\';
+        temp = strchr(temp, '/');
+    }
     if (length(caption) != 1)
         errorcall(call, "caption must be a character string");
     if (multi == NA_LOGICAL)
@@ -1131,7 +1138,7 @@ SEXP do_chooseFiles(SEXP call, SEXP op, SEXP args, SEXP rho)
     }
     *temp = 0;
 
-    askfilenames(CHAR(STRING_ELT(caption, 0)), CHAR(STRING_ELT(def, 0)), multi, cfilters, filterindex, list,
+    askfilenames(CHAR(STRING_ELT(caption, 0)), path, multi, cfilters, filterindex, list,
                  65500); /* list declared larger to protect against overwrites */
     Rwin_fpset();
     count = countFilenames(list);
