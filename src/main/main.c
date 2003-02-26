438ce438e7a3cc3ef841dbdc8cdd9e4665caac02
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-02-26 15:51:17 +0000

    RNG change + delayed greeting
    git-svn-id: https://svn.r-project.org/R/trunk@23377 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -413,14 +413,8 @@ void setup_Rmainloop(void)
 
     InitConnections(); /* needed to get any output at all */
 
-    /* Print a platform and version dependent */
-    /* greeting and a pointer to the copyleft. */
-
-    if (!R_Quiet)
-        PrintGreeting();
-
-        /* Initialize the interpreter's */
-        /* internal structures. */
+    /* Initialize the interpreter's */
+    /* internal structures. */
 
 #ifdef HAVE_LOCALE_H
 #ifdef Win32
@@ -530,6 +524,12 @@ void setup_Rmainloop(void)
     R_LoadProfile(R_OpenSysInitFile(), baseEnv);
     R_LoadProfile(R_OpenSiteFile(), baseEnv);
     R_LoadProfile(R_OpenInitFile(), R_GlobalEnv);
+
+    /* Print a platform and version dependent */
+    /* greeting and a pointer to the copyleft. */
+
+    if (!R_Quiet)
+        PrintGreeting();
 
     /* This is where we try to load a user's saved data.
        The right thing to do here is very platform dependent.
