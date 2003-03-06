9ff737a25dc93cdba9bc46a0995123516e5612e8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-03-06 13:14:28 +0000

    Linux seems to need TZ= set at all times, not just when tzset is called
    git-svn-id: https://svn.r-project.org/R/trunk@23492 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/datetime.c
+++ src/main/datetime.c
@@ -420,7 +420,7 @@ static int set_tz(char *tz, char *oldtz)
 #else
     char *p = NULL;
     int settz = 0;
-    char buff[20];
+    static char buff[200];
 
     strcpy(oldtz, "");
     p = getenv("TZ");
@@ -452,7 +452,7 @@ static void reset_tz(char *tz)
     if (strlen(tz))
     {
 #ifdef HAVE_PUTENV
-        char buff[20];
+        static char buff[200];
         strcpy(buff, "TZ=");
         strcat(buff, tz);
         putenv(buff);
