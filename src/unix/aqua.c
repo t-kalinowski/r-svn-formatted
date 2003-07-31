1ddab0d999e3580ce46512ca80501542589500a8
iacus <iacus@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-07-31 13:48:56 +0000

    *** empty log message ***
    git-svn-id: https://svn.r-project.org/R/trunk@25516 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/aqua.c
+++ src/unix/aqua.c
@@ -184,9 +184,6 @@ SEXP do_browsepkgs(SEXP call, SEXP op, SEXP args, SEXP env)
 
 void R_ProcessEvents(void)
 {
-    EventRef theEvent;
-    EventTargetRef theTarget = GetEventDispatcherTarget();
-
     if (CheckEventQueueForUserCancel())
         onintr();
 }
