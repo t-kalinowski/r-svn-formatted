5746c8b3fa0a73b43fb87901ce5ea48aeeb7b8d5
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-09-09 20:47:08 +0000

    Added some protects; reduced copying of right hand side in assignments.
    git-svn-id: https://svn.r-project.org/R/trunk@26137 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1229,7 +1229,14 @@ static SEXP applydefine(SEXP call, SEXP op, SEXP args, SEXP rho)
     expr = eval(expr, rho);
     UNPROTECT(5);
     unbindVar(R_TmpvalSymbol, rho);
+#ifdef CONSERVATIVE_COPYING
     return duplicate(saverhs);
+#else
+    /* we do not duplicate the value, so to be conservative mark the
+       value as NAMED = 2 */
+    SET_NAMED(saverhs, 2);
+    return saverhs;
+#endif
 }
 
 /* Defunct in in 1.5.0
@@ -1769,8 +1776,9 @@ int DispatchOrEval(SEXP call, SEXP op, char *generic, SEXP args, SEXP rho, SEXP 
             *ans = EvalArgs(args, rho, dropmissing);
         else
         {
-            *ans = CONS(x, EvalArgs(CDR(args), rho, dropmissing));
+            PROTECT(*ans = CONS(x, EvalArgs(CDR(args), rho, dropmissing)));
             SET_TAG(*ans, CreateTag(TAG(args)));
+            UNPROTECT(1);
         }
     }
     else
@@ -1808,8 +1816,9 @@ int DispatchOrEval(SEXP call, SEXP op, char *generic, SEXP args, SEXP rho, SEXP 
         }
     }
     /* else PROTECT(args); */
-    *ans = CONS(x, EvalArgs(CDR(args), rho, dropmissing));
+    PROTECT(*ans = CONS(x, EvalArgs(CDR(args), rho, dropmissing)));
     SET_TAG(*ans, CreateTag(TAG(args)));
+    UNPROTECT(1);
 #endif
     UNPROTECT(nprotect);
     return 0;
