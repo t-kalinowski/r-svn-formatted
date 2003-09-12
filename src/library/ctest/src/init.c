09b1520b3c67f31fc844fa97d8fbe813eb4a0ef8
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-09-12 13:53:00 +0000

    oops need new prho interface too
    git-svn-id: https://svn.r-project.org/R/trunk@26190 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ctest/src/init.c
+++ src/library/ctest/src/init.c
@@ -33,10 +33,9 @@ static R_NativePrimitiveArgType fexact_t[10] = {INTSXP,  INTSXP,  REALSXP, INTSX
                                                 REALSXP, REALSXP, REALSXP, REALSXP, INTSXP};
 
 static R_NativePrimitiveArgType pkendall_t[3] = {INTSXP, REALSXP, INTSXP};
-static R_NativePrimitiveArgType kendall_tau_t[4] = {INTSXP, REALSXP, REALSXP, REALSXP};
 
 static R_NativePrimitiveArgType pkstwo_t[3] = {INTSXP, REALSXP, REALSXP};
-static R_NativePrimitiveArgType prho_t[5] = {INTSXP, INTSXP, REALSXP, INTSXP, LGLSXP};
+static R_NativePrimitiveArgType prho_t[5] = {INTSXP, REALSXP, REALSXP, INTSXP, LGLSXP};
 static R_NativePrimitiveArgType psmirnov2x_t[3] = {REALSXP, INTSXP, INTSXP};
 
 static R_NativePrimitiveArgType swilk_t[9] = {LGLSXP,    SINGLESXP, INTSXP,  INTSXP, INTSXP,
@@ -46,7 +45,6 @@ static const R_CMethodDef cMethods[] = {{"chisqsim", (DL_FUNC)&chisqsim, 11, chi
                                         {"d2x2xk", (DL_FUNC)&d2x2xk, 5, d2_t},
                                         {"dansari", (DL_FUNC)&dansari, 4, dansari_t},
                                         {"fexact", (DL_FUNC)&fexact, 10, fexact_t},
-                                        {"kendall_tau", (DL_FUNC)&kendall_tau, 4, kendall_tau_t},
                                         {"pansari", (DL_FUNC)&pansari, 4, pansari_t},
                                         {"pkendall", (DL_FUNC)&pkendall, 3, pkendall_t},
                                         {"pkstwo", (DL_FUNC)&pkstwo, 3, pkstwo_t},
