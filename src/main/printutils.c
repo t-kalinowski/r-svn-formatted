f5b4e7263af17ae0c5a16ba43c20f1575715000e
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-10-14 03:17:55 +0000

    branch update
    git-svn-id: https://svn.r-project.org/R/trunk@26619 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/printutils.c
+++ src/main/printutils.c
@@ -519,6 +519,12 @@ void Rcons_vprintf(const char *format, va_list arg)
 void Rvprintf(const char *format, va_list arg)
 {
     Rconnection con = getConnection(R_OutputCon);
+    static int printcount = 0;
+    if (++printcount > 100)
+    {
+        R_CheckUserInterrupt();
+        printcount = 0;
+    }
 
     con->vfprintf(con, format, arg);
     con->fflush(con);
