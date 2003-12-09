bb23cd74c2c0b285ec17629b95bf7bc067fae8f0
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-09 02:00:59 +0000

    Signal error in args2buf if arglist isn't a list.
    git-svn-id: https://svn.r-project.org/R/trunk@27437 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -1185,6 +1185,8 @@ static void args2buff(SEXP arglist, int lineb, int formals, LocalParseData *d)
 
     while (arglist != R_NilValue)
     {
+        if (TYPEOF(arglist) != LISTSXP && TYPEOF(arglist) != LANGSXP)
+            error("badly formed function expression");
         if (TAG(arglist) != R_NilValue)
         {
 #if 0
