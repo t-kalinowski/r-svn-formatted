4c8bdb279aabb686c2815804e57422f1141e6e07
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-09 18:14:26 +0000

    merge modreg into stats
    git-svn-id: https://svn.r-project.org/R/trunk@27464 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/init.c
+++ src/library/stats/src/init.c
@@ -17,7 +17,9 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <R.h>
 #include "ctest.h"
+#include "modreg.h"
 #include <R_ext/Rdynload.h>
 
 #include <Rinternals.h>
@@ -41,6 +43,10 @@ static R_NativePrimitiveArgType psmirnov2x_t[3] = {REALSXP, INTSXP, INTSXP};
 static R_NativePrimitiveArgType swilk_t[9] = {LGLSXP,    SINGLESXP, INTSXP,  INTSXP, INTSXP,
                                               SINGLESXP, REALSXP,   REALSXP, INTSXP};
 
+static R_NativePrimitiveArgType Srunmed_t[6] = {REALSXP, REALSXP, INTSXP, INTSXP, INTSXP, LGLSXP};
+static R_NativePrimitiveArgType Trunmed_t[9] = {INTSXP, INTSXP,  REALSXP, REALSXP, INTSXP,
+                                                INTSXP, REALSXP, INTSXP,  INTSXP};
+
 static const R_CMethodDef cMethods[] = {{"chisqsim", (DL_FUNC)&chisqsim, 11, chisqsim_t},
                                         {"d2x2xk", (DL_FUNC)&d2x2xk, 5, d2_t},
                                         {"dansari", (DL_FUNC)&dansari, 4, dansari_t},
@@ -52,10 +58,30 @@ static const R_CMethodDef cMethods[] = {{"chisqsim", (DL_FUNC)&chisqsim, 11, chi
                                         {"psmirnov2x", (DL_FUNC)&psmirnov2x, 3, psmirnov2x_t},
                                         {"qansari", (DL_FUNC)&qansari, 4, qansari_t},
                                         {"swilk", (DL_FUNC)&swilk, 9, swilk_t},
+                                        {"BDRksmooth", (DL_FUNC)&BDRksmooth, 8},
+                                        {"loess_raw", (DL_FUNC)&loess_raw, 24},
+                                        {"loess_dfit", (DL_FUNC)&loess_dfit, 13},
+                                        {"loess_dfitse", (DL_FUNC)&loess_dfitse, 16},
+                                        {"loess_ifit", (DL_FUNC)&loess_ifit, 8},
+                                        {"loess_ise", (DL_FUNC)&loess_ise, 15},
+                                        {"Srunmed", (DL_FUNC)&Srunmed, 6, Srunmed_t},
+                                        {"Trunmed", (DL_FUNC)&Trunmed, 9, Trunmed_t},
                                         {NULL, NULL, 0}};
+
+static R_CallMethodDef CallEntries[] = {{"R_isoreg", (DL_FUNC)&R_isoreg, 1}, {NULL, NULL, 0}};
+
+static R_FortranMethodDef FortEntries[] = {{"lowesw", (DL_FUNC)&F77_SUB(lowesw), 4},
+                                           {"lowesp", (DL_FUNC)&F77_SUB(lowesp), 7},
+                                           {"setppr", (DL_FUNC)&F77_SUB(setppr), 6},
+                                           {"smart", (DL_FUNC)&F77_SUB(smart), 16},
+                                           {"pppred", (DL_FUNC)&F77_SUB(pppred), 5},
+                                           {"qsbart", (DL_FUNC)&F77_SUB(qsbart), 21},
+                                           {"bvalus", (DL_FUNC)&F77_SUB(bvalus), 7},
+                                           {"supsmu", (DL_FUNC)&F77_SUB(supsmu), 10},
+                                           {NULL, NULL, 0}};
 
 void R_init_stats(DllInfo *dll)
 {
     R_useDynamicSymbols(dll, FALSE);
-    R_registerRoutines(dll, cMethods, NULL, NULL, NULL);
+    R_registerRoutines(dll, cMethods, CallEntries, FortEntries, NULL);
 }
