2e33ddc7875cfc1f6ddfdfa090dcfb0cc1d386eb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-16 23:47:58 +0000

    suppress a warning on over-long strings
    git-svn-id: https://svn.r-project.org/R/trunk@27647 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/bzip2/bzlib.c
+++ src/extra/bzip2/bzlib.c
@@ -96,29 +96,30 @@ void BZ2_bz__AssertH__fail(int errcode)
 
     if (errcode == 1007)
     {
-        fprintf(stderr, "\n*** A special note about internal error number 1007 ***\n"
-                        "\n"
-                        "Experience suggests that a common cause of i.e. 1007\n"
-                        "is unreliable memory or other hardware.  The 1007 assertion\n"
-                        "just happens to cross-check the results of huge numbers of\n"
-                        "memory reads/writes, and so acts (unintendedly) as a stress\n"
-                        "test of your memory system.\n"
-                        "\n"
-                        "I suggest the following: try compressing the file again,\n"
-                        "possibly monitoring progress in detail with the -vv flag.\n"
-                        "\n"
-                        "* If the error cannot be reproduced, and/or happens at different\n"
-                        "  points in compression, you may have a flaky memory system.\n"
-                        "  Try a memory-test program.  I have used Memtest86\n"
-                        "  (www.memtest86.com).  At the time of writing it is free (GPLd).\n"
-                        "  Memtest86 tests memory much more thorougly than your BIOSs\n"
-                        "  power-on test, and may find failures that the BIOS doesn't.\n"
-                        "\n"
-                        "* If the error can be repeatably reproduced, this is a bug in\n"
-                        "  bzip2, and I would very much like to hear about it.  Please\n"
-                        "  let me know, and, ideally, save a copy of the file causing the\n"
-                        "  problem -- without which I will be unable to investigate it.\n"
-                        "\n");
+        fprintf(stderr, "%s%s%s",
+                "\n*** A special note about internal error number 1007 ***\n"
+                "\n"
+                "Experience suggests that a common cause of i.e. 1007\n"
+                "is unreliable memory or other hardware.  The 1007 assertion\n"
+                "just happens to cross-check the results of huge numbers of\n"
+                "memory reads/writes, and so acts (unintendedly) as a stress\n"
+                "test of your memory system.\n"
+                "\n",
+                "I suggest the following: try compressing the file again,\n"
+                "possibly monitoring progress in detail with the -vv flag.\n"
+                "\n"
+                "* If the error cannot be reproduced, and/or happens at different\n"
+                "  points in compression, you may have a flaky memory system.\n"
+                "  Try a memory-test program.  I have used Memtest86\n"
+                "  (www.memtest86.com).  At the time of writing it is free (GPLd).\n"
+                "  Memtest86 tests memory much more thorougly than your BIOSs\n"
+                "  power-on test, and may find failures that the BIOS doesn't.\n"
+                "\n",
+                "* If the error can be repeatably reproduced, this is a bug in\n"
+                "  bzip2, and I would very much like to hear about it.  Please\n"
+                "  let me know, and, ideally, save a copy of the file causing the\n"
+                "  problem -- without which I will be unable to investigate it.\n"
+                "\n");
     }
 
     exit(3);
