a20efe76fb71564c0ff023a455452df4c965bb98
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-06-20 18:36:48 +0000

    fix typo
    git-svn-id: https://svn.r-project.org/R/trunk@29937 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/bind.c
+++ src/main/bind.c
@@ -108,7 +108,7 @@ static void AnswerType(SEXP x, int recurse, int usenames, struct BindData *data)
         {
             int i, n;
             n = length(x);
-            if (usenames && !data->ans_names && !isNull(getAttrib(x, R_NamesSymbol)))
+            if (usenames && !data->ans_nnames && !isNull(getAttrib(x, R_NamesSymbol)))
                 data->ans_nnames = 1;
             for (i = 0; i < n; i++)
             {
