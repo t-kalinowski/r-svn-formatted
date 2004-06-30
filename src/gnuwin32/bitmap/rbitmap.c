282be92a53d8aa9f3083aedf9de96e883ab29f0f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-06-30 19:03:43 +0000

    remove Rprintf
    git-svn-id: https://svn.r-project.org/R/trunk@30104 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/bitmap/rbitmap.c
+++ src/gnuwin32/bitmap/rbitmap.c
@@ -512,7 +512,6 @@ int R_SaveAsBmp(void *d, int width, int height, unsigned long (*gp)(void *, int,
     BMPDW(0);         /* biCompression=BI_RGB */
     BMPDW(0);         /* biSizeImage (with BI_RGB not needed)*/
     lres = (long)(0.5 + res / 0.0254);
-    Rprintf("%d %d %x\n", res, lres, lres);
     BMPLONG(lres);    /* XPels/M <- used only by Windows?*/
     BMPLONG(lres);    /* XPels/M */
     BMPDW(biClrUsed); /* biClrUsed */
