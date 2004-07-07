3208ea0bd72894829849b2a58e3778f5c78b0a49
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-07-07 01:04:25 +0000

    Fixes for pdf support of alpha transparency
    git-svn-id: https://svn.r-project.org/R/trunk@30183 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/devPS.c
+++ src/main/devPS.c
@@ -4045,13 +4045,21 @@ static int alphaVersion(PDFDesc *pd)
     return (pd->versionMajor > 1 || (pd->versionMajor == 1 && pd->versionMinor >= 4));
 }
 
+/*
+ * Do we need to bother with semi-transparency?
+ */
+static int semiTransparent(int col)
+{
+    return !(R_OPAQUE(col) || R_TRANSPARENT(col));
+}
+
 static void PDF_SetLineColor(int color, NewDevDesc *dd)
 {
     PDFDesc *pd = (PDFDesc *)dd->deviceSpecific;
 
     if (color != pd->current.col)
     {
-        if (alphaVersion(pd))
+        if (alphaVersion(pd) && semiTransparent(color))
         {
             /*
              * Apply graphics state parameter dictionary
@@ -4069,7 +4077,7 @@ static void PDF_SetFill(int color, NewDevDesc *dd)
     PDFDesc *pd = (PDFDesc *)dd->deviceSpecific;
     if (color != pd->current.fill)
     {
-        if (alphaVersion(pd))
+        if (alphaVersion(pd) && semiTransparent(color))
         {
             /*
              * Apply graphics state parameter dictionary
@@ -4476,7 +4484,7 @@ static void PDF_Rect(double x0, double y0, double x1, double y1, R_GE_gcontext *
     /*
      * Only try to do real transparency if version at least 1.4
      */
-    if (alphaVersion(pd))
+    if ((semiTransparent(gc->col) || semiTransparent(gc->fill)) && alphaVersion(pd))
     {
         if (pd->inText)
             textoff(pd);
@@ -4526,16 +4534,17 @@ static void PDF_Circle(double x, double y, double r, R_GE_gcontext *gc, NewDevDe
     /*
      * Only try to do real transparency if version at least 1.4
      */
-    if (alphaVersion(pd))
+    if ((semiTransparent(gc->col) || semiTransparent(gc->fill)) && alphaVersion(pd))
     {
-        if (pd->inText)
-            textoff(pd);
         PDF_SetFill(gc->fill, dd);
         PDF_SetLineColor(gc->col, dd);
         PDF_SetLineStyle(gc->lty, gc->lwd, dd);
-        if (r > 10)
-        { /* somewhat arbitrary, use font up to 20pt */
-            /* Use four Bezier curves, hand-fitted to quadrants */
+        /*
+         * Due to possible bug in Acrobat Reader for rendering
+         * semi-transparent text, only every draw Bezier curves
+         * regardless of circle size.
+         */
+        {
             double s = 0.55 * r;
             if (pd->inText)
                 textoff(pd);
@@ -4545,22 +4554,6 @@ static void PDF_Circle(double x, double y, double r, R_GE_gcontext *gc, NewDevDe
             fprintf(pd->pdffp, "  %.2f %.2f %.2f %.2f %.2f %.2f c\n", x + r, y - s, x + s, y - r, x, y - r);
             fprintf(pd->pdffp, "  %.2f %.2f %.2f %.2f %.2f %.2f c\n", x - s, y - r, x - r, y - s, x - r, y);
             fprintf(pd->pdffp, "B\n");
-        }
-        else
-        {
-            /* Use char 108 in Dingbats, which is a solid disc
-               afm is C 108 ; WX 791 ; N a71 ; B 35 -14 757 708 ;
-               so diameter = 0.722 * size
-               centre = (0.396, 0.347) * size
-            */
-            a = 2. / 0.722 * r;
-            xx = x - 0.396 * a;
-            yy = y - 0.347 * a;
-            tr = 2;
-            if (!pd->inText)
-                texton(pd);
-            fprintf(pd->pdffp, "/F1 1 Tf %d Tr %.2f 0 0 %.2f %.2f %.2f Tm", tr, a, a, xx, yy);
-            fprintf(pd->pdffp, " (l) Tj 0 Tr\n");
         }
     }
     else
@@ -4626,7 +4619,7 @@ static void PDF_Line(double x1, double y1, double x2, double y2, R_GE_gcontext *
     /*
      * Only try to do real transparency if version at least 1.4
      */
-    if (alphaVersion(pd) || (R_ALPHA(gc->col) == 0))
+    if ((semiTransparent(gc->col) && alphaVersion(pd)) || (R_ALPHA(gc->col) == 0))
     {
         PDF_SetLineColor(gc->col, dd);
         PDF_SetLineStyle(gc->lty, gc->lwd, dd);
@@ -4645,7 +4638,7 @@ static void PDF_Polygon(int n, double *x, double *y, R_GE_gcontext *gc, NewDevDe
     /*
      * Only try to do real transparency if version at least 1.4
      */
-    if (alphaVersion(pd))
+    if ((semiTransparent(gc->col) || semiTransparent(gc->fill)) && alphaVersion(pd))
     {
         if (pd->inText)
             textoff(pd);
@@ -4711,7 +4704,7 @@ static void PDF_Polyline(int n, double *x, double *y, R_GE_gcontext *gc, NewDevD
     /*
      * Only try to do real transparency if version at least 1.4
      */
-    if (alphaVersion(pd))
+    if ((semiTransparent(gc->col) || semiTransparent(gc->fill)) && alphaVersion(pd))
     {
         if (pd->inText)
             textoff(pd);
