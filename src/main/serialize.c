ad57ee3f7a203d84a21fd50332bac2b6b5392ebd
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-07-24 16:43:38 +0000

    add configure test for working ftell (rather than just special-casing Windows)
    git-svn-id: https://svn.r-project.org/R/trunk@30369 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/serialize.c
+++ src/main/serialize.c
@@ -2087,14 +2087,14 @@ static SEXP appendStringToFile(SEXP file, SEXP string)
         error("not a proper file name");
     if (!IS_PROPER_STRING(string))
         error("not a proper string");
-#ifdef Win32
-    /* Windows returns position 0 with "ab" */
+#ifdef HAVE_WORKING_FTELL
+    /* Windows' ftell returns position 0 with "ab" */
+    if ((fp = fopen(CHAR(STRING_ELT(file, 0)), "ab")) == NULL)
+        error("file open failed");
+#else
     if ((fp = fopen(CHAR(STRING_ELT(file, 0)), "r+b")) == NULL)
         error("file open failed");
     fseek(fp, 0, SEEK_END);
-#else
-    if ((fp = fopen(CHAR(STRING_ELT(file, 0)), "ab")) == NULL)
-        error("file open failed");
 #endif
 
     len = LENGTH(STRING_ELT(string, 0));
