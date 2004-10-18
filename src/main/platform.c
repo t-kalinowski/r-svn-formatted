7c3302b2318a81f1df4975860dde54fcc7a5dee8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-10-18 12:21:41 +0000

    merge r31464 from R-patched
    git-svn-id: https://svn.r-project.org/R/trunk@31465 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/platform.c
+++ src/main/platform.c
@@ -1169,8 +1169,8 @@ SEXP do_capabilities(SEXP call, SEXP op, SEXP args, SEXP rho)
     SEXP ans, ansnames;
     int i = 0;
 #ifdef Unix
-    Rboolean X11 =
-        (strcmp(R_GUIType, "X11") == 0) || (strcmp(R_GUIType, "Tk") == 0) || (strcmp(R_GUIType, "GNOME") == 0);
+    Rboolean X11 = (strcmp(R_GUIType, "X11") == 0) || (strcmp(R_GUIType, "Tk") == 0) ||
+                   (strcmp(R_GUIType, "GNOME") == 0) || (strcmp(R_GUIType, "AQUA") == 0);
 #endif
 
     checkArity(op, args);
