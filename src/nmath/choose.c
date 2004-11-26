64b21a3f3edef908639c264937ecea54f042212a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-11-26 09:53:55 +0000

    choose(*, k) for small k
    git-svn-id: https://svn.r-project.org/R/trunk@32051 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/choose.c
+++ src/nmath/choose.c
@@ -31,12 +31,15 @@
  *
  *	These should work for the generalized binomial theorem,
  *	i.e., are also defined for non-integer n  (integer k).
+ *
+ *  We use the simple explicit product formula for  k <= k_small_max
+ *  and also have added statements to make sure that the symmetry
+ *    (n \\ k ) == (n \\ n-k)  is preserved for integer n.
  */
 
 #include "nmath.h"
 
-extern int signgam; /* set in each call to lgammafn() */
-static int s_choose;
+extern int signgam; /* set in lgammafn(.) */
 
 double lfastchoose(double n, double k)
 {
@@ -44,11 +47,11 @@ double lfastchoose(double n, double k)
 }
 /* mathematically the same:
    less stable typically, but useful if n-k+1 < 0 : */
-static double lfastchoose2(double n, double k)
+static double lfastchoose2(double n, double k, int *s_choose)
 {
     double r;
     r = lgammafn(n - k + 1.);
-    s_choose = signgam;
+    *s_choose = signgam;
     return lgammafn(n + 1.) - lgammafn(k + 1.) - r;
 }
 
@@ -70,11 +73,12 @@ double lchoose(double n, double k)
             return ML_NEGINF;
         if (k == 0)
             return 0.;
-        /*else: k == 1 */ return log(n);
+        /* else: k == 1 */
+        return log(n);
     }
     /* else: k >= 2 */
     if (n < 0)
-    { /* -n+ k-1 > 1 */
+    {
         if (ODD(k))
             return ML_NAN; /* log( <negative> ) */
         return lchoose(-n + k - 1, k);
@@ -83,18 +87,25 @@ double lchoose(double n, double k)
     {
         if (n < k)
             return ML_NEGINF;
+        if (n - k < 2)
+            return lchoose(n, n - k); /* <- Symmetry */
         return lfastchoose(n, k);
     }
     /* else non-integer n >= 0 : */
     if (n < k - 1)
     {
+        int s;
         if (fmod(floor(n - k + 1), 2.) == 0) /* choose() < 0 */
             return ML_NAN;
-        return lfastchoose2(n, k);
+        return lfastchoose2(n, k, &s);
     }
     return lfastchoose(n, k);
 }
 
+#define k_small_max 30
+/* 30 is somewhat arbitrary: it is on the *safe* side:
+ * both speed and precision are clearly improved for k < 30.
+ */
 double choose(double n, double k)
 {
     double r;
@@ -104,17 +115,24 @@ double choose(double n, double k)
     if (ISNAN(n) || ISNAN(k))
         return n + k;
 #endif
-    if (k < 2)
+    if (k < k_small_max)
     {
+        int j;
+        if (R_IS_INT(n) && n - k < k)
+            k = n - k; /* <- Symmetry */
         if (k < 0)
             return 0.;
         if (k == 0)
             return 1.;
-        /*else: k == 1 */ return n;
+        /* else: k >= 1 */
+        r = n;
+        for (j = 2; j <= k; j++)
+            r *= (n - j + 1) / j;
+        return r;
     }
-    /* else: k >= 2 */
+    /* else: k >= k_small_max */
     if (n < 0)
-    { /* -n+ k-1 > 1 */
+    {
         r = choose(-n + k - 1, k);
         if (ODD(k))
             r = -r;
@@ -124,12 +142,15 @@ double choose(double n, double k)
     {
         if (n < k)
             return 0.;
+        if (n - k < k_small_max)
+            return choose(n, n - k); /* <- Symmetry */
         return floor(exp(lfastchoose(n, k)) + 0.5);
     }
     /* else non-integer n >= 0 : */
     if (n < k - 1)
     {
-        r = lfastchoose2(n, k); /* -> s_choose */
+        int s_choose;
+        r = lfastchoose2(n, k, /* -> */ &s_choose);
         return s_choose * exp(r);
     }
     return exp(lfastchoose(n, k));
@@ -137,3 +158,4 @@ double choose(double n, double k)
 
 #undef ODD
 #undef R_IS_INT
+#undef k_small_max
