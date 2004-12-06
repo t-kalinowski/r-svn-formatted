148e3e798938107d6a18020da265f57af50b437d
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-12-06 02:55:20 +0000

    par(ask=TRUE) tweak
    git-svn-id: https://svn.r-project.org/R/trunk@32120 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/devga.c
+++ src/gnuwin32/devga.c
@@ -3065,6 +3065,7 @@ menu getGraphMenu(char *menuname)
 Rboolean winNewFrameConfirm()
 {
     char savetitle[50];
+    char msg[] = "Waiting to confirm page change...";
     gadesc *xd;
     GEDevDesc *dd = GEcurrentDevice();
 
@@ -3084,7 +3085,10 @@ Rboolean winNewFrameConfirm()
     addto(xd->gawin);
     gchangemenubar(xd->mbarconfirm);
     gchangepopup(xd->gawin, NULL);
-    setstatus("Waiting to confirm page change");
+    setstatus(msg);
+    R_WriteConsole(msg, strlen(msg));
+    R_WriteConsole("\n", 1);
+    R_FlushConsole();
     strncpy(savetitle, gettext(xd->gawin), sizeof(savetitle));
     settext(xd->gawin, "Click or hit ENTER for next page");
     while (!xd->clicked && !xd->enterkey)
