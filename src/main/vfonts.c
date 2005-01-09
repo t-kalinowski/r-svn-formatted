cfaf106f5c073a364a2412b21ec4ee1d1efb47ca
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-01-09 15:25:23 +0000

    Hershey fonts are encoded as Latin-1, so do appropriate conversion
    git-svn-id: https://svn.r-project.org/R/trunk@32552 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/vfonts.c
+++ src/main/vfonts.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2001 The R Development Core Team
+ *  Copyright (C) 2001-5 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,9 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* <UTF8> char here is either ASCII or handled as a whole */
+/* <UTF8> char here is either ASCII or handled as a whole.
+   However, the interpretation is as ASCII or in some cases Latin-1
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -26,6 +28,12 @@
 #include <Defn.h>
 #include <Rdynpriv.h>
 #include <Graphics.h>
+
+#ifdef HAVE_ALLOCA_H
+#include <alloca.h>
+#else
+extern char *alloca(size_t);
+#endif
 
 typedef struct
 {
@@ -73,10 +81,24 @@ static void vfonts_Init(void)
 double GVStrWidth(const unsigned char *s, int typeface, int fontindex, int unit, DevDesc *dd)
 {
     R_GE_gcontext gc;
+    char *str = (char *)s;
+#ifdef SUPPORT_UTF8
+    char *buff;
+#endif
     gcontextFromGP(&gc, dd);
     gc.fontface = typeface;
     gc.fontfamily[0] = fontindex;
-    return GConvertXUnits(R_GE_VStrWidth(s, &gc, (GEDevDesc *)dd), DEVICE, unit, dd);
+#ifdef SUPPORT_UTF8
+    if (utf8locale && !utf8strIsASCII(str))
+    {
+        buff = alloca(strlen(str) + 1); /* Output string cannot be longer */
+        if (!buff)
+            error("allocation failure in GVStrWidth");
+        utf8toLatin1(s, buff);
+        str = buff;
+    }
+#endif
+    return GConvertXUnits(R_GE_VStrWidth((unsigned char *)str, &gc, (GEDevDesc *)dd), DEVICE, unit, dd);
 }
 
 double R_GE_VStrWidth(const unsigned char *s, R_GE_gcontext *gc, GEDevDesc *dd)
@@ -95,10 +117,24 @@ double R_GE_VStrWidth(const unsigned char *s, R_GE_gcontext *gc, GEDevDesc *dd)
 double GVStrHeight(const unsigned char *s, int typeface, int fontindex, int unit, DevDesc *dd)
 {
     R_GE_gcontext gc;
+    char *str = (char *)s;
+#ifdef SUPPORT_UTF8
+    char *buff;
+#endif
     gcontextFromGP(&gc, dd);
     gc.fontface = typeface;
     gc.fontfamily[0] = fontindex;
-    return GConvertYUnits(R_GE_VStrHeight(s, &gc, (GEDevDesc *)dd), DEVICE, unit, dd);
+#ifdef SUPPORT_UTF8
+    if (utf8locale && !utf8strIsASCII(str))
+    {
+        buff = alloca(strlen(str) + 1); /* Output string cannot be longer */
+        if (!buff)
+            error("allocation failure in GVStrHeight");
+        utf8toLatin1(s, buff);
+        str = buff;
+    }
+#endif
+    return GConvertYUnits(R_GE_VStrHeight((unsigned char *)str, &gc, (GEDevDesc *)dd), DEVICE, unit, dd);
 }
 
 double R_GE_VStrHeight(const unsigned char *s, R_GE_gcontext *gc, GEDevDesc *dd)
@@ -118,6 +154,10 @@ void GVText(double x, double y, int unit, char *s, int typeface, int fontindex, 
             double rotation, DevDesc *dd)
 {
     R_GE_gcontext gc;
+    char *str = s;
+#ifdef SUPPORT_UTF8
+    char *buff;
+#endif
     gcontextFromGP(&gc, dd);
     /*
      * Ensure that the current par(xpd) settings are enforced.
@@ -126,7 +166,17 @@ void GVText(double x, double y, int unit, char *s, int typeface, int fontindex, 
     GConvert(&x, &y, unit, DEVICE, dd);
     gc.fontface = fontindex;
     gc.fontfamily[0] = typeface;
-    R_GE_VText(x, y, s, x_justify, y_justify, rotation, &gc, (GEDevDesc *)dd);
+#ifdef SUPPORT_UTF8
+    if (utf8locale && !utf8strIsASCII(str))
+    {
+        buff = alloca(strlen(str) + 1); /* Output string cannot be longer */
+        if (!buff)
+            error("allocation failure in GVText");
+        utf8toLatin1(s, buff);
+        str = buff;
+    }
+#endif
+    R_GE_VText(x, y, str, x_justify, y_justify, rotation, &gc, (GEDevDesc *)dd);
 }
 
 void R_GE_VText(double x, double y, char *s, double x_justify, double y_justify, double rotation, R_GE_gcontext *gc,
