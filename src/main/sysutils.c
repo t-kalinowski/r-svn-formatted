8e9baae2b8a03c5d5562a5ba646b4c912988d11d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-01-14 19:56:07 +0000

    add iconvlist() to glibc systems
    git-svn-id: https://svn.r-project.org/R/trunk@32645 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sysutils.c
+++ src/main/sysutils.c
@@ -388,8 +388,7 @@ SEXP do_iconv(SEXP call, SEXP op, SEXP args, SEXP env)
         cnt = 0;
         iconvlist(write_one, (void *)ans);
 #else
-        error("`iconvlist' is not available on this system");
-        ans = R_NilValue; /* -Wall */
+        PROTECT(ans = R_NilValue);
 #endif
     }
     else
