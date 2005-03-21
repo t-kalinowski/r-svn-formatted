99d255aaa09c8d0c622333f91eeee14c6da44a6f
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-03-21 23:47:46 +0000

    Fix PR#7314 on rhyper: bug in original algorithm
    git-svn-id: https://svn.r-project.org/R/trunk@33687 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/rhyper.c
+++ src/nmath/rhyper.c
@@ -271,6 +271,11 @@ double rhyper(double nn1in, double nn2in, double kkin)
         if (m < 100 || ix <= 50)
         {
             /* explicit evaluation */
+            /* The original algorithm (and TOMS 668) have
+                   f = f * i * (n2 - k + i) / (n1 - i) / (k - i);
+           in the (m > ix) case, but the definition of the
+               recurrence relation on p134 shows that the +1 is
+               needed. */
             f = 1.0;
             if (m < ix)
             {
@@ -280,7 +285,7 @@ double rhyper(double nn1in, double nn2in, double kkin)
             else if (m > ix)
             {
                 for (i = ix + 1; i <= m; i++)
-                    f = f * i * (n2 - k + i) / (n1 - i) / (k - i);
+                    f = f * i * (n2 - k + i) / (n1 - i + 1) / (k - i + 1);
             }
             if (v <= f)
             {
