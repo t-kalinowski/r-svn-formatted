aecced49ed3b3e08bbace9ecb58a656b75d14aeb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-03-22 14:44:50 +0000

    repair the damage from r33653
    git-svn-id: https://svn.r-project.org/R/trunk@33696 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devWindows.c
+++ src/library/grDevices/src/devWindows.c
@@ -53,8 +53,8 @@
 Rboolean PSDeviceDriver(NewDevDesc *, char *, char *, char *, char **, char *, char *, char *, double, double, Rboolean,
                         double, Rboolean, Rboolean, Rboolean, char *, char *, SEXP);
 
-Rboolean PDFDeviceDriver(NewDevDesc *dd, char *, char *, char *, char *, char *, double, double, double, int, char *,
-                         SEXP, int, int);
+Rboolean PDFDeviceDriver(NewDevDesc *dd, char *, char *, char *, char *, char *, char *, double, double, double, int,
+                         int, char *, SEXP, int, int);
 static Rboolean GADeviceDriver(NewDevDesc *dd, char *display, double width, double height, double pointsize,
                                Rboolean recording, int resize, int bg, int canvas, double gamma, int xpos, int ypos,
                                Rboolean buffered, SEXP psenv);
@@ -410,10 +410,10 @@ static void SaveAsPDF(NewDevDesc *dd, char *fn)
             }
         }
     }
-    if (PDFDeviceDriver(ndd, fn, family, encoding, bg, fg,
+    if (PDFDeviceDriver(ndd, fn, "special", family, encoding, bg, fg,
                         fromDeviceWidth(toDeviceWidth(1.0, GE_NDC, gdd), GE_INCHES, gdd),
                         fromDeviceHeight(toDeviceHeight(-1.0, GE_NDC, gdd), GE_INCHES, gdd),
-                        ((gadesc *)dd->deviceSpecific)->basefontsize, 1, "R Graphics Output", R_NilValue, 1, 4))
+                        ((gadesc *)dd->deviceSpecific)->basefontsize, 1, 0, "R Graphics Output", R_NilValue, 1, 4))
         PrivateCopyDevice(dd, ndd, "PDF");
 }
 
