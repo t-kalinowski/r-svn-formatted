94a8c837beaaaccb8c984372edf187ec601b7e41
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-04-04 15:14:01 +0000

    actually test a package in Rpackage
    git-svn-id: https://svn.r-project.org/R/trunk@33817 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/Embedding/Rpackage.c
+++ tests/Embedding/Rpackage.c
@@ -1,52 +1,33 @@
 #include <Rinternals.h>
 #include "embeddedRCall.h"
 
-static void doCtestExample();
-static void doMVAExample();
+static void doSplinesExample();
 extern int Rf_initEmbeddedR(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
     Rf_initEmbeddedR(argc, argv);
-    doCtestExample();
-    doMVAExample();
+    doSplinesExample();
     return (0);
 }
 
-static void doCtestExample()
+static void doSplinesExample()
 {
-    SEXP e, tmp;
+    SEXP e;
     int errorOccurred;
 
     PROTECT(e = allocVector(LANGSXP, 2));
+    SETCAR(e, Rf_install("library"));
+    SETCAR(CDR(e), mkString("splines"));
+
+    Test_tryEval(e, NULL);
+    UNPROTECT(1);
+
+    PROTECT(e = allocVector(LANGSXP, 2));
     SETCAR(e, Rf_install("example"));
-    SETCAR(CDR(e), tmp = allocVector(STRSXP, 1));
-    SET_STRING_ELT(tmp, 0, mkChar("t.test"));
+    SETCAR(CDR(e), mkString("ns"));
 
     Test_tryEval(e, &errorOccurred);
 
-    UNPROTECT(1);
-}
-
-static void doMVAExample()
-{
-    SEXP e, tmp;
-
-    PROTECT(e = allocVector(LANGSXP, 2));
-    SETCAR(e, Rf_install("library"));
-    SETCAR(CDR(e), tmp = allocVector(STRSXP, 1));
-    SET_STRING_ELT(tmp, 0, mkChar("mva"));
-
-    Test_tryEval(e, NULL);
-    UNPROTECT(1);
-
-    PROTECT(e = allocVector(LANGSXP, 2));
-    SETCAR(e, Rf_install("example"));
-    SETCAR(CDR(e), tmp = allocVector(STRSXP, 1));
-    SET_STRING_ELT(tmp, 0, mkChar("dist"));
-
-    Rf_PrintValue(e);
-
-    Test_tryEval(e, NULL);
     UNPROTECT(1);
 }
