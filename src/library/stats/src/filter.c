893b2b87b9f8463162f7ee70eb15e1b6469c8624
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-04-04 20:44:34 +0000

    acf() could cause crash PR#7771
    git-svn-id: https://svn.r-project.org/R/trunk@33824 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/filter.c
+++ src/library/stats/src/filter.c
@@ -123,7 +123,7 @@ void acf(double *x, int *n, int *nser, int *nlag, int *correlation, double *acf)
     int i, u, v, lag, nl = *nlag, nn = *n, ns = *nser, d1 = nl + 1, d2 = ns * d1, nu;
     double sum, *se;
 
-    se = (double *)R_alloc(nn, sizeof(double));
+    se = (double *)R_alloc(ns, sizeof(double));
     for (u = 0; u < ns; u++)
         for (v = 0; v < ns; v++)
             for (lag = 0; lag <= nl; lag++)
