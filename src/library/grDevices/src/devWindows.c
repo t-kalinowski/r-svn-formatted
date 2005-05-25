58e2b2ec48230c8bcc1a745cb3e040fc7a54ba78
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-05-25 20:24:30 +0000

    More fixes for PR#7873 (Windows locator() trouble)
    git-svn-id: https://svn.r-project.org/R/trunk@34487 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devWindows.c
+++ src/library/grDevices/src/devWindows.c
@@ -2571,6 +2571,8 @@ static void donelocator(void *data)
     xd->locator = FALSE;
 }
 
+static void GA_onExit(NewDevDesc *dd);
+
 static Rboolean GA_Locator(double *x, double *y, NewDevDesc *dd)
 {
     gadesc *xd = (gadesc *)dd->deviceSpecific;
@@ -2596,6 +2598,10 @@ static Rboolean GA_Locator(double *x, double *y, NewDevDesc *dd)
     begincontext(&cntxt, CTXT_CCODE, R_NilValue, R_NilValue, R_NilValue, R_NilValue, R_NilValue);
     cntxt.cend = &donelocator;
     cntxt.cenddata = xd;
+    xd->cntxt = &cntxt;
+
+    /* and an exit handler in case the window gets closed */
+    dd->onExit = GA_onExit;
 
     while (!xd->clicked)
     {
@@ -2604,6 +2610,9 @@ static Rboolean GA_Locator(double *x, double *y, NewDevDesc *dd)
         WaitMessage();
         R_ProcessEvents();
     }
+
+    dd->onExit = NULL;
+    xd->cntxt = NULL;
 
     endcontext(&cntxt);
     donelocator((void *)xd);
@@ -2734,6 +2743,7 @@ static Rboolean GADeviceDriver(NewDevDesc *dd, char *display, double width, doub
     dd->hold = GA_Hold;
     dd->metricInfo = GA_MetricInfo;
     xd->newFrameConfirm = GA_NewFrameConfirm;
+    xd->cntxt = NULL;
 
     /* set graphics parameters that must be set by device driver */
     /* Window Dimensions in Pixels */
@@ -3155,6 +3165,11 @@ static void GA_onExit(NewDevDesc *dd)
     xd->confirmation = FALSE;
     dd->gettingEvent = FALSE;
     xd->eventRho = NULL;
+
+    if (xd->cntxt)
+        endcontext(xd->cntxt);
+    if (xd->locator)
+        donelocator((void *)xd);
 
     addto(xd->gawin);
     gchangemenubar(xd->mbar);
