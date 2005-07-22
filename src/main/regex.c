c90831201fa10c96b0bb7d580e141842ddb59e95
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-07-22 15:26:25 +0000

    shut up warning
    git-svn-id: https://svn.r-project.org/R/trunk@35015 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/regex.c
+++ src/main/regex.c
@@ -9524,7 +9524,7 @@ const re_string_t *input;
         {
             /* match with range expression?  */
 #if __GNUC__ >= 2
-            wchar_t cmp_buf[] = {L'\0', L'\0', wc, L'\0', L'\0', L'\0'};
+            __extension__ wchar_t cmp_buf[] = {L'\0', L'\0', wc, L'\0', L'\0', L'\0'};
 #else
             wchar_t cmp_buf[] = {L'\0', L'\0', L'\0', L'\0', L'\0', L'\0'};
             cmp_buf[2] = wc;
