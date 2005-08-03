e0bcd377431c6db8e00e151248242bf3ed2b7fbe
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-08-03 08:44:17 +0000

    add back R modifications
    git-svn-id: https://svn.r-project.org/R/trunk@35125 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/zlib/gzio.c
+++ src/extra/zlib/gzio.c
@@ -11,6 +11,20 @@
 
 #include "zutil.h"
 
+/* R ADDITION */
+#if defined(HAVE_OFF_T) && defined(__USE_LARGEFILE)
+#define f_seek fseeko
+#define f_tell ftello
+#else
+#ifdef Win32
+#define f_seek fseeko64
+#define f_tell ftello64
+#else
+#define f_seek fseek
+#define f_tell ftell
+#endif
+#endif
+
 #ifdef NO_DEFLATE /* for compatibility with old definition */
 #define NO_GZCOMPRESS
 #endif
@@ -226,7 +240,7 @@ int fd;
     else
     {
         check_header(s); /* skip the .gz header */
-        s->start = ftell(s->file) - s->stream.avail_in;
+        s->start = f_tell(s->file) - s->stream.avail_in;
     }
 
     return (gzFile)s;
@@ -897,7 +911,7 @@ int whence;
         s->back = EOF;
         s->stream.avail_in = 0;
         s->stream.next_in = s->inbuf;
-        if (fseek(s->file, offset, SEEK_SET) < 0)
+        if (f_seek(s->file, offset, SEEK_SET) < 0)
             return -1L;
 
         s->in = s->out = offset;
@@ -963,7 +977,7 @@ int ZEXPORT gzrewind(file) gzFile file;
         (void)inflateReset(&s->stream);
     s->in = 0;
     s->out = 0;
-    return fseek(s->file, s->start, SEEK_SET);
+    return f_seek(s->file, s->start, SEEK_SET);
 }
 
 /* ===========================================================================
