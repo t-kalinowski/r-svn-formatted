7458a7e0131805313bed8eeec4694d83846d6811
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-08-20 07:20:06 +0000

    add error on USE_SYSTEM_REGEX
    git-svn-id: https://svn.r-project.org/R/trunk@35329 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/regex.c
+++ src/main/regex.c
@@ -9932,4 +9932,7 @@ int Rregexec(const regex_t *__restrict preg, const char *__restrict string, size
         err = re_search_internal(preg, string, length, offset, length - offset, length, nmatch, pmatch, eflags);
     return err != REG_NOERROR;
 }
-#endif /* not USE_SYSTEM_REGEX */
+#else /* not USE_SYSTEM_REGEX */
+/* for 2.1.0, this option is not functional */
+#error USE_SYSTEM_REGEX is no longer supported
+#endif
