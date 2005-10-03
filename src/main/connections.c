f68fde8eaa592d4cc0cdb2831653ccb24e9e852d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-10-03 15:48:04 +0000

    incorrect size calculation
    git-svn-id: https://svn.r-project.org/R/trunk@35721 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -2821,10 +2821,7 @@ static int rawRead(char *p, int size, int n, Rbyte *bytes, int nbytes, int *np)
 {
     int avail, m;
 
-    avail = (nbytes - n * size);
-    if (avail < 0)
-        avail = 0;
-    avail = avail / size;
+    avail = nbytes / size;
     m = n;
     if (m > avail)
         m = avail;
