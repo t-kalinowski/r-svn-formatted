db3e05ce3746be2b11fff52c78dc121b3c90dac8
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-04 02:14:47 +0000

    r36175 was too aggressive
    git-svn-id: https://svn.r-project.org/R/trunk@36177 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/builtin.c
+++ src/main/builtin.c
@@ -202,14 +202,17 @@ SEXP do_envirgets(SEXP call, SEXP op, SEXP args, SEXP rho)
     SEXP env;
     checkArity(op, args);
     env = CADR(args);
-    if (isNull(env))
+
+    if (TYPEOF(CAR(args)) == CLOSXP && (isEnvironment(env) || isNull(env)))
     {
-        warning(_("use of NULL environment is deprecated"));
-        env = R_BaseEnv;
-    }
-    if (TYPEOF(CAR(args)) == CLOSXP && isEnvironment(env))
+        if (isNull(env))
+        {
+            warning(_("use of NULL environment is deprecated"));
+            env = R_BaseEnv;
+        }
         SET_CLOENV(CAR(args), env);
-    else if (isEnvironment(env))
+    }
+    else if (isNull(env) || isEnvironment(env))
         setAttrib(CAR(args), R_DotEnvSymbol, env);
     else
         errorcall(call, _("replacement object is not an environment"));
