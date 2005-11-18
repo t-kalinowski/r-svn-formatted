ab0890de0387ef9d1e306616e74afb5b5c49ed25
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-18 07:45:11 +0000

    rwin.html -> index.html
    git-svn-id: https://svn.r-project.org/R/trunk@36382 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/rui.c
+++ src/gnuwin32/rui.c
@@ -1144,7 +1144,7 @@ int RguiCommonHelp(menu m, HelpMenuItems hmenu)
     MCHECK(newmenuitem("-", 0, NULL));
     MCHECK(hmenu->mhelp = newmenuitem(G_("R functions (text)..."), 0, menuhelp));
     MCHECK(hmenu->mhelpstart = newmenuitem(G_("Html help"), 0, menuhelpstart));
-    if (!check_doc_file("doc\\html\\rwin.html"))
+    if (!check_doc_file("doc\\html\\index.html"))
         disable(hmenu->mhelpstart);
     MCHECK(hmenu->mhelpsearch = newmenuitem(G_("Search help..."), 0, menuhelpsearch));
     MCHECK(hmenu->msearchRsite = newmenuitem(G_("search.r-project.org ..."), 0, menusearchRsite));
