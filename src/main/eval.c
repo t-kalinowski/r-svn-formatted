e2db7f28a0d72c213d4a52b4399a31910f18e29f
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-18 15:11:15 +0000

    No need for warning on eval(enclos = NULL); this is documented to be allowed.
    git-svn-id: https://svn.r-project.org/R/trunk@36387 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1581,10 +1581,7 @@ SEXP do_eval(SEXP call, SEXP op, SEXP args, SEXP rho)
     env = CADR(args);
     encl = CADDR(args);
     if (isNull(encl))
-    {
-        warning(_("use of NULL environment is deprecated"));
         encl = R_BaseEnv;
-    }
     else if (!isEnvironment(encl))
         errorcall(call, _("invalid '%s' argument"), "enclos");
     switch (TYPEOF(env))
