a58cd4dafb9af459b3a69aae78906e8ee17d3044
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-12-29 13:09:11 +0000

    use TextOutW so we can control the locale used to display the text.
    git-svn-id: https://svn.r-project.org/R/trunk@36909 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/graphapp/gdraw.c
+++ src/gnuwin32/graphapp/gdraw.c
@@ -1,6 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1998--2004  Guido Masarotto and Brian Ripley
+ *  Copyright (C) 2005        The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -27,6 +28,8 @@
 #include "internal.h"
 extern unsigned int TopmostDialogs; /* from dialogs.c */
 #include <winbase.h>
+#include <wchar.h>
+#define alloca(x) __builtin_alloca((x)) /* always GNUC */
 
 static HDC GETHDC(drawing d)
 {
@@ -495,7 +498,17 @@ int gdrawstr(drawing d, font f, rgb c, point p, char *s)
     SetBkMode(dc, TRANSPARENT);
     SetTextAlign(dc, TA_TOP | TA_LEFT | TA_UPDATECP);
 
+#ifdef SUPPORT_MBCS
+    { /* This allows us to change locales and output in the new locale */
+        wchar_t *wc;
+        int n = strlen(s), cnt;
+        wc = alloca((n + 1) * sizeof(wchar_t));
+        cnt = mbstowcs(wc, s, n);
+        TextOutW(dc, p.x, p.y, wc, cnt);
+    }
+#else
     TextOut(dc, p.x, p.y, s, strlen(s));
+#endif
 
     GetCurrentPositionEx(dc, &curr_pos);
     width = curr_pos.x - p.x;
@@ -528,16 +541,15 @@ void gdrawstr1(drawing d, font f, rgb c, point p, char *s, double hadj)
 
 /* This version interprets 's' as MBCS in the current locale */
 #ifdef SUPPORT_MBCS
-#include <wchar.h>
 void gwdrawstr1(drawing d, font f, rgb c, point p, char *s, double hadj)
 {
     HFONT old;
     HDC dc = GETHDC(d);
     UINT flags = TA_BASELINE | TA_UPDATECP;
-    wchar_t wc[1000];
-    int cnt;
+    wchar_t *wc;
+    int n = strlen(s), cnt;
+    wc = alloca((n + 1) * sizeof(wchar_t));
 
-    cnt = mbstowcs(wc, s, 1000);
     SetTextColor(dc, getwinrgb(d, c));
     old = SelectObject(dc, f->handle);
     MoveToEx(dc, p.x, p.y, NULL);
@@ -549,6 +561,7 @@ void gwdrawstr1(drawing d, font f, rgb c, point p, char *s, double hadj)
     else
         flags |= TA_RIGHT;
     SetTextAlign(dc, flags);
+    cnt = mbstowcs(wc, s, n); /* This is OK if we get an error */
     TextOutW(dc, p.x, p.y, wc, cnt);
     SelectObject(dc, old);
 }
