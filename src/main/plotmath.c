4996a4eabfee7a7bfbc3bd4b740229fe9aea095a
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-02-09 01:04:03 +0000

    Bug fix for get.gpar() (introduced with "[" method for gpar objects)
    Allow thin lines in plotmath 
    
    
    git-svn-id: https://svn.r-project.org/R/trunk@37301 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plotmath.c
+++ src/main/plotmath.c
@@ -591,7 +591,17 @@ static BBOX DrawBBox(BBOX bbox, double xoffset, double yoffset, mathContext *mc,
     y[3] = ConvertedY(mc, dd);
     gc->col = mc->BoxColor;
     gc->lty = LTY_SOLID;
-    gc->lwd = 1;
+    /*
+     * We used to just force lwd = 1, which is a reasonable sanity check
+     * because if the user had set lwd = 10 for something, your
+     * mathematical annotation would look REALLY bad.
+     * Unfortunately, this meant that the user could not set a nice
+     * small lwd to get a finer line;  we always bumped it back up to 1.
+     * NOW we just reduce lwd to 1 (as a sanity check) ONLY if
+     * lwd is greater than 1.
+     */
+    if (gc->lwd > 1)
+        gc->lwd = 1;
     GEPolyline(5, x, y, gc, dd);
     PMoveTo(xsaved, ysaved, mc);
     gc->col = savedcol;
@@ -1362,7 +1372,8 @@ static BBOX RenderSlash(int draw, mathContext *mc, R_GE_gcontext *gc, GEDevDesc 
         y[1] = ConvertedY(mc, dd);
         PMoveUp(-height, mc);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(2, x, y, gc, dd);
         PMoveAcross(0.5 * width, mc);
         gc->lty = savedlty;
@@ -1623,7 +1634,8 @@ static BBOX RenderWideTilde(SEXP expr, int draw, mathContext *mc, R_GE_gcontext 
         x[NTILDE + 2] = ConvertedX(mc, dd);
         y[NTILDE + 2] = ConvertedY(mc, dd);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(NTILDE + 3, x, y, gc, dd);
         PMoveTo(savedX + totalwidth, savedY, mc);
         gc->lty = savedlty;
@@ -1665,7 +1677,8 @@ static BBOX RenderWideHat(SEXP expr, int draw, mathContext *mc, R_GE_gcontext *g
         x[2] = ConvertedX(mc, dd);
         y[2] = ConvertedY(mc, dd);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(3, x, y, gc, dd);
         PMoveTo(savedX + width, savedY, mc);
         gc->lty = savedlty;
@@ -1702,7 +1715,8 @@ static BBOX RenderBar(SEXP expr, int draw, mathContext *mc, R_GE_gcontext *gc, G
         x[1] = ConvertedX(mc, dd);
         y[1] = ConvertedY(mc, dd);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(2, x, y, gc, dd);
         PMoveTo(savedX + width, savedY, mc);
         gc->lty = savedlty;
@@ -1883,7 +1897,8 @@ static BBOX RenderFraction(SEXP expr, int rule, int draw, mathContext *mc, R_GE_
             x[1] = ConvertedX(mc, dd);
             y[1] = ConvertedY(mc, dd);
             gc->lty = LTY_SOLID;
-            gc->lwd = 1;
+            if (gc->lwd > 1)
+                gc->lwd = 1;
             GEPolyline(2, x, y, gc, dd);
             PMoveUp(-AxisHeight(gc, dd), mc);
             gc->lty = savedlty;
@@ -1924,7 +1939,8 @@ static BBOX RenderUnderline(SEXP expr, int draw, mathContext *mc, R_GE_gcontext 
         x[1] = ConvertedX(mc, dd);
         y[1] = ConvertedY(mc, dd);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(2, x, y, gc, dd);
         PMoveUp(depth, mc);
         gc->lty = savedlty;
@@ -2548,7 +2564,8 @@ static BBOX RenderRadical(SEXP expr, int draw, mathContext *mc, R_GE_gcontext *g
         x[4] = ConvertedX(mc, dd);
         y[4] = ConvertedY(mc, dd);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(5, x, y, gc, dd);
         PMoveTo(savedX, savedY, mc);
         gc->lty = savedlty;
@@ -2593,7 +2610,8 @@ static BBOX RenderAbs(SEXP expr, int draw, mathContext *mc, R_GE_gcontext *gc, G
         x[1] = ConvertedX(mc, dd);
         y[1] = ConvertedY(mc, dd);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(2, x, y, gc, dd);
         PMoveUp(-height, mc);
         gc->lty = savedlty;
@@ -2614,7 +2632,8 @@ static BBOX RenderAbs(SEXP expr, int draw, mathContext *mc, R_GE_gcontext *gc, G
         x[1] = ConvertedX(mc, dd);
         y[1] = ConvertedY(mc, dd);
         gc->lty = LTY_SOLID;
-        gc->lwd = 1;
+        if (gc->lwd > 1)
+            gc->lwd = 1;
         GEPolyline(2, x, y, gc, dd);
         PMoveUp(-height, mc);
         gc->lty = savedlty;
