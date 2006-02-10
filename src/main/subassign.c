2f1332c90f80c8294a75df62d67676dd2962da41
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-02-10 09:47:45 +0000

    more informative error messages
    git-svn-id: https://svn.r-project.org/R/trunk@37315 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subassign.c
+++ src/main/subassign.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1997-2004   The R Development Core Team
+ *  Copyright (C) 1997-2006   The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -303,7 +303,8 @@ static int SubassignTypeFix(SEXP *x, SEXP *y, int stretch, int level, SEXP call)
         break;
 
     default:
-        errorcall(call, _("incompatible types (%d) in subassignment type fix"), which);
+        errorcall(call, _("incompatible types (from %s to %s) in subassignment type fix"), CHAR(type2str(which % 100)),
+                  CHAR(type2str(which / 100)));
     }
 
     if (stretch)
@@ -978,7 +979,8 @@ static SEXP MatrixAssign(SEXP call, SEXP x, SEXP s, SEXP y)
         break;
 
     default:
-        error(_("incompatible types (case %d) in matrix subset assignment"), which);
+        error(_("incompatible types (from %s to %s) in matrix subset assignment"), CHAR(type2str(which % 100)),
+              CHAR(type2str(which / 100)));
     }
     UNPROTECT(2);
     return x;
@@ -1165,7 +1167,8 @@ static SEXP ArrayAssign(SEXP call, SEXP x, SEXP s, SEXP y)
             break;
 
         default:
-            error(_("incompatible types (%d) in array subset assignment"), which);
+            error(_("incompatible types (from %s to %s) in array subset assignment"), CHAR(type2str(which % 100)),
+                  CHAR(type2str(which / 100)));
         }
     next_i:;
         if (n > 1)
@@ -1771,7 +1774,8 @@ SEXP attribute_hidden do_subassign2_dflt(SEXP call, SEXP op, SEXP args, SEXP rho
             break;
 
         default:
-            error(_("incompatible types (%d) in [[ assignment"), which);
+            error(_("incompatible types (from %s to %s) in [[ assignment"), CHAR(type2str(which % 100)),
+                  CHAR(type2str(which / 100)));
         }
         /* If we stretched, we may have a new name. */
         /* In this case we must create a names attribute */
