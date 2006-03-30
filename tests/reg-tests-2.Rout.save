t-kalinowski/r-svn-formatted

commit 7ade0f879d6661ab5f1541e1efe06941b122a491

Changes to substitute and findVar: In C, substitute( , R_NilValue) does no substs, just getting PREXPRs, while substitute( , R_GlobalEnv) does substitutions.
In R:  substitute( , .GlobalEnv) is just like
R_NilValue above, as is substitute( , NULL).

git-svn-id: https://svn.r-project.org/R/trunk@37269 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/coerce.c
@@ -2115,7 +2115,10 @@ SEXP attribute_hidden do_docall(SEXP call, SEXP op, SEXP args, SEXP rho)
 /* values as found in the environment.	There is no inheritance so only */
 /* the supplied environment is searched. If no environment is specified */
 /* the environment in which substitute was called is used.  If the */
-/* specified environment is R_NilValue then R_GlobalEnv is used. */
+/* specified environment is R_GlobalEnv it is converted to R_NilValue, for */
+/* historical reasons. */
+/* In substitute(), R_NilValue signals that no substitution should be done, only */
+/* extraction of promise expressions. */
 /* Arguments to do_substitute should not be evaluated. */
 
 SEXP substitute(SEXP lang, SEXP rho)
@@ -2126,23 +2129,26 @@ SEXP substitute(SEXP lang, SEXP rho)
     case PROMSXP:
         return substitute(PREXPR(lang), rho);
     case SYMSXP:
-        t = findVarInFrame3(rho, lang, TRUE);
-        if (t != R_UnboundValue)
+        if (rho != R_NilValue)
         {
-            if (TYPEOF(t) == PROMSXP)
+            t = findVarInFrame3(rho, lang, TRUE);
+            if (t != R_UnboundValue)
             {
-                do
+                if (TYPEOF(t) == PROMSXP)
                 {
-                    t = PREXPR(t);
-                } while (TYPEOF(t) == PROMSXP);
-                return t;
+                    do
+                    {
+                        t = PREXPR(t);
+                    } while (TYPEOF(t) == PROMSXP);
+                    return t;
+                }
+                else if (TYPEOF(t) == DOTSXP)
+                {
+                    error(_("... used in an incorrect context"));
+                }
+                if (rho != R_GlobalEnv)
+                    return t;
             }
-            else if (TYPEOF(t) == DOTSXP)
-            {
-                error(_("... used in an incorrect context"));
-            }
-            if (rho != R_GlobalEnv)
-                return t;
         }
         return (lang);
     case LANGSXP:
@@ -2162,6 +2168,8 @@ SEXP attribute_hidden substituteList(SEXP el, SEXP rho)
         return el;
     if (CAR(el) == R_DotsSymbol)
     {
+        if (rho == R_NilValue)
+            return substituteList(CDR(el), rho);
         h = findVarInFrame3(rho, CAR(el), TRUE);
         if (h == R_NilValue)
             return substituteList(CDR(el), rho);
@@ -2203,13 +2211,13 @@ SEXP attribute_hidden do_substitute(SEXP call, SEXP op, SEXP args, SEXP rho)
         env = rho;
     else
         env = eval(CADR(args), rho);
-    if (env == R_NilValue)
-        env = R_GlobalEnv;
+    if (env == R_GlobalEnv) /* For historical reasons, don't substitute in R_GlobalEnv */
+        env = R_NilValue;
     else if (TYPEOF(env) == VECSXP)
         env = NewEnvironment(R_NilValue, VectorToPairList(env), R_BaseEnv);
     else if (TYPEOF(env) == LISTSXP)
         env = NewEnvironment(R_NilValue, duplicate(env), R_BaseEnv);
-    if (TYPEOF(env) != ENVSXP)
+    if (env != R_NilValue && TYPEOF(env) != ENVSXP)
         errorcall(call, _("invalid environment specified"));
 
     PROTECT(env);

src/main/envir.c
@@ -880,6 +880,12 @@ SEXP findVarInFrame3(SEXP rho, SEXP symbol, Rboolean doGet)
     int hashcode;
     SEXP frame, c;
 
+    if (TYPEOF(rho) == NILSXP)
+    {
+        warning(_("use of NULL environment is deprecated"));
+        rho = R_BaseEnv;
+    }
+
     if (rho == R_BaseNamespace || rho == R_BaseEnv)
         return SYMBOL_BINDING_VALUE(symbol);
 
@@ -984,6 +990,16 @@ static SEXP findGlobalVar(SEXP symbol)
 SEXP findVar(SEXP symbol, SEXP rho)
 {
     SEXP vl;
+
+    if (TYPEOF(rho) == NILSXP)
+    {
+        warning(_("use of NULL environment is deprecated"));
+        rho = R_BaseEnv;
+    }
+
+    if (!isEnvironment(rho))
+        error(_("argument to %s is not an environment"), "findVar");
+
 #ifdef USE_GLOBAL_CACHE
     /* This first loop handles local frames, if there are any.  It
        will also handle all frames if rho is a global frame other than

commit 39cfd80975bbe5bc0e897ba818409bd16f77cb00

Fix for bug introduced in r37269.
git-svn-id: https://svn.r-project.org/R/trunk@37616 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/coerce.c
@@ -2173,6 +2173,10 @@ SEXP substitute(SEXP lang, SEXP rho)
 /* Work through a list doing substitute on the */
 /* elements taking particular care to handle ... */
 
+/* FIXME:  This version takes particular care, but still mis-handles 	*/
+/* cases like substitute(list(..., x), list(x=1)) (where it 		*/
+/* doesn't substitute for x).  -DJM 					*/
+
 SEXP attribute_hidden substituteList(SEXP el, SEXP rho)
 {
     SEXP h, t;
@@ -2181,7 +2185,7 @@ SEXP attribute_hidden substituteList(SEXP el, SEXP rho)
     if (CAR(el) == R_DotsSymbol)
     {
         if (rho == R_NilValue)
-            return substituteList(CDR(el), rho);
+            return el;
         h = findVarInFrame3(rho, CAR(el), TRUE);
         if (h == R_NilValue)
             return substituteList(CDR(el), rho);

tests/reg-tests-2.R
@@ -1776,3 +1776,7 @@ w <- c(rep(1,9), 0)
 summary(glm(y ~ x, weights = w))
 summary(glm(y ~ x, subset = w > 0))
 ## has NA dispersion in 2.2.1
+
+## substitute was losing "..." after r37269
+yaa <- function(...) substitute(list(...))
+yaa(foo(...))

tests/reg-tests-2.Rout.save
@@ -1,6 +1,6 @@
 
 R : Copyright 2006, The R Foundation for Statistical Computing
-Version 2.3.0 alpha (2006-03-27 r37590)
+Version 2.3.0 alpha (2006-03-30 r37614)
 ISBN 3-900051-07-0
 
 R is free software and comes with ABSOLUTELY NO WARRANTY.
@@ -5118,4 +5118,9 @@ AIC: 30.702
 Number of Fisher Scoring iterations: 2
 
 > ## has NA dispersion in 2.2.1
+> 
+> ## substitute was losing "..." after r37269
+> yaa <- function(...) substitute(list(...))
+> yaa(foo(...))
+list(foo(...))
 > 