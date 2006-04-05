f87c1166846009928f6f215eab243d3150b8c513
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-04-05 15:08:58 +0000

    BATCH changes
    git-svn-id: https://svn.r-project.org/R/trunk@37652 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/rcmdfn.c
+++ src/gnuwin32/front-ends/rcmdfn.c
@@ -104,7 +104,7 @@ int rcmdfn(int cmdarg, int argc, char **argv)
     if (cmdarg > 0 && argc > cmdarg && strcmp(argv[cmdarg], "BATCH") == 0)
     {
         /* handle Rcmd BATCH internally */
-        char infile[MAX_PATH], outfile[MAX_PATH];
+        char infile[MAX_PATH], outfile[MAX_PATH], *p;
         DWORD ret;
         SECURITY_ATTRIBUTES sa;
         PROCESS_INFORMATION pi;
@@ -113,6 +113,11 @@ int rcmdfn(int cmdarg, int argc, char **argv)
 
         /* process the command line */
         sprintf(cmd, "%s/bin/Rterm.exe --restore --save", getRHOME());
+        if ((p = getenv("R_BATCH_OPTIONS")) && strlen(p))
+        {
+            strcat(cmd, " ");
+            strcat(cmd, p);
+        }
 
         for (i = cmdarg + 1, iused = cmdarg; i < argc; i++)
         {
