611963128c5a725352e1ad86b89fa0514984096e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-05-15 07:28:50 +0000

    bindingIsLocked was returning incorrect logial on some systems
    git-svn-id: https://svn.r-project.org/R/trunk@38062 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/envir.c
+++ src/main/envir.c
@@ -2965,13 +2965,13 @@ Rboolean R_BindingIsLocked(SEXP sym, SEXP env)
     else if (TYPEOF(env) != ENVSXP)
         error(_("not an environment"));
     if (env == R_BaseEnv || env == R_BaseNamespace)
-        return BINDING_IS_LOCKED(sym);
+        return BINDING_IS_LOCKED(sym) != 0;
     else
     {
         SEXP binding = findVarLocInFrame(env, sym, NULL);
         if (binding == R_NilValue)
             error(_("no binding for \"%s\""), CHAR(PRINTNAME(sym)));
-        return BINDING_IS_LOCKED(binding);
+        return BINDING_IS_LOCKED(binding) != 0;
     }
 }
 
