352f9c8c638c68445f71289a0563159ec7ff2910
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-05-23 18:19:59 +0000

    workaround for LC_CTYPE=C under Windows
    git-svn-id: https://svn.r-project.org/R/trunk@38173 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/platform.c
+++ src/main/platform.c
@@ -1140,7 +1140,14 @@ SEXP attribute_hidden do_setlocale(SEXP call, SEXP op, SEXP args, SEXP rho)
         cat = LC_ALL;
         p = CHAR(STRING_ELT(locale, 0));
         setlocale(LC_COLLATE, p);
+#ifdef Win32
+        if (strcmp(p, "C") == 0)
+            setlocale(LC_CTYPE, "en");
+        else
+            setlocale(LC_CTYPE, p);
+#else
         setlocale(LC_CTYPE, p);
+#endif
         setlocale(LC_MONETARY, p);
         setlocale(LC_TIME, p);
         p = setlocale(cat, NULL);
@@ -1151,7 +1158,17 @@ SEXP attribute_hidden do_setlocale(SEXP call, SEXP op, SEXP args, SEXP rho)
         break;
     case 3:
         cat = LC_CTYPE;
+#ifdef Win32
+        /* LC_CTYPE=C bombs in mingwex */
+        p = CHAR(STRING_ELT(locale, 0));
+        /* LC_CTYPE=C bombs in mingwex */
+        if (strcmp(p, "C") == 0)
+            setlocale(LC_CTYPE, "en");
+        else
+            setlocale(LC_CTYPE, p);
+#else
         p = setlocale(cat, CHAR(STRING_ELT(locale, 0)));
+#endif
         break;
     case 4:
         cat = LC_MONETARY;
