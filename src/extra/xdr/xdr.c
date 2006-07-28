e522c8efe4d51850f932935386599be6e2f6a23f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-07-28 15:51:05 +0000

    quieten -Wall -pedeantic
    git-svn-id: https://svn.r-project.org/R/trunk@38709 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/xdr/xdr.c
+++ src/extra/xdr/xdr.c
@@ -61,6 +61,7 @@ static char sccsid[] = "@(#)xdr.c 1.35 87/08/12";
 #include <rpc/xdr.h>
 
 /* R-specific */
+void REprintf(char *, ...);
 #define fprintf(a, b) REprintf(b)
 
 /*
@@ -344,7 +345,7 @@ caddr_t cp;
 register u_int cnt;
 {
     register u_int rndup;
-    static crud[BYTES_PER_XDR_UNIT];
+    static int crud[BYTES_PER_XDR_UNIT];
 
     /*
      * if no data we are done
@@ -539,6 +540,9 @@ u_int maxsize;
         /* fall through... */
     case XDR_ENCODE:
         size = strlen(sp);
+        break;
+    case XDR_DECODE:
+        /* size not used: just keep compilers happy */
         break;
     }
     if (!xdr_u_int(xdrs, &size))
