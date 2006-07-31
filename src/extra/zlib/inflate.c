f6967817680ea29c50ce8c91c609b7230b8a910c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-07-31 19:55:15 +0000

    unfortunately .tar.gz sources are marked as gzip in some sites
    git-svn-id: https://svn.r-project.org/R/trunk@38745 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/zlib/inflate.c
+++ src/extra/zlib/inflate.c
@@ -85,12 +85,6 @@
 #include "inflate.h"
 #include "inffast.h"
 
-#ifdef HAVE_VISIBILITY_ATTRIBUTE
-#define attribute_visible __attribute__((visibility("default")))
-#else
-#define attribute_visible
-#endif
-
 #ifdef MAKEFIXED
 #ifndef BUILDFIXED
 #define BUILDFIXED
@@ -147,7 +141,7 @@ int value;
     return Z_OK;
 }
 
-int ZEXPORT attribute_visible inflateInit2_(strm, windowBits, version, stream_size) z_streamp strm;
+int ZEXPORT inflateInit2_(strm, windowBits, version, stream_size) z_streamp strm;
 int windowBits;
 const char *version;
 int stream_size;
@@ -583,7 +577,7 @@ unsigned out;
    will return Z_BUF_ERROR if it has not reached the end of the stream.
  */
 
-int ZEXPORT attribute_visible inflate(strm, flush) z_streamp strm;
+int ZEXPORT inflate(strm, flush) z_streamp strm;
 int flush;
 {
     struct inflate_state FAR *state;
@@ -1257,7 +1251,7 @@ inf_leave:
     return ret;
 }
 
-int ZEXPORT attribute_visible inflateEnd(strm) z_streamp strm;
+int ZEXPORT inflateEnd(strm) z_streamp strm;
 {
     struct inflate_state FAR *state;
     if (strm == Z_NULL || strm->state == Z_NULL || strm->zfree == (free_func)0)
