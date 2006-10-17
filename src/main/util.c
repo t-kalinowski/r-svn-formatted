810a4452779bf14fe2717e6c1c5e8e29837095ec
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-10-17 19:51:37 +0000

    some tidying up of abbreviate(), including removing the byte limit (because it was easy, not because it was needed).
    git-svn-id: https://svn.r-project.org/R/trunk@39653 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/util.c
+++ src/main/util.c
@@ -118,7 +118,7 @@ const static char *const falsenames[] = {
 SEXP asChar(SEXP x)
 {
     int w, d, e, wi, di, ei;
-    char buf[MAXELTSIZE];
+    char buf[MAXELTSIZE]; /* probably 100 would suffice */
 
     if (isVectorAtomic(x) && LENGTH(x) >= 1)
     {
