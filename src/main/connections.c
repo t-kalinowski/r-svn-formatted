62b1de410c9ae15090b64398b4d4d8e816f4699e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-01-05 09:21:20 +0000

    allow file("stdin") as distinct from stdin()
    git-svn-id: https://svn.r-project.org/R/trunk@40372 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -464,7 +464,18 @@ static Rboolean file_open(Rconnection con)
     else
         name = R_ExpandFileName(con->description);
     errno = 0; /* some systems require this */
-    fp = R_fopen(name, con->mode);
+    if (strcmp(name, "stdin"))
+    {
+        fp = R_fopen(name, con->mode);
+    }
+    else
+    { /* use file("stdin") to refer to the file and not the console */
+#ifdef HAVE_FDOPEN
+        fp = fdopen(0, con->mode);
+#else
+        warning(_("cannot open file '%s', reason '%s'"), name, "fdopen is not supported on this platform");
+#endif
+    }
     if (!fp)
     {
 #ifdef HAVE_STRERROR
@@ -517,7 +528,8 @@ static Rboolean file_open(Rconnection con)
 static void file_close(Rconnection con)
 {
     Rfileconn this = con->private;
-    fclose(this->fp);
+    if (strcmp(con->description, "stdin"))
+        fclose(this->fp);
     con->isopen = FALSE;
 #ifdef Win32
     if (this->anon_file)
