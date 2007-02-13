1ac1dcef1c85586d5b610aeb61b437be37bcf26d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-02-13 15:32:01 +0000

    tweaks
    git-svn-id: https://svn.r-project.org/R/trunk@40714 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/util.c
+++ src/main/util.c
@@ -913,6 +913,19 @@ SEXP attribute_hidden do_setencoding(SEXP call, SEXP op, SEXP args, SEXP rho)
     return x;
 }
 
+void attribute_hidden markKnown(SEXP x, SEXP ref)
+{
+    if (TYPEOF(x) != CHARSXP)
+        error("invalid use of 'markKnown'");
+    if (IS_LATIN1(ref) || IS_UTF8(ref))
+    {
+        if (known_to_be_latin1)
+            SET_LATIN1(x);
+        if (known_to_be_utf8)
+            SET_UTF8(x);
+    }
+}
+
 /* Note: this is designed to be fast and valid only for UTF-8 strings.
    It is also correct in EUC-* locales. */
 Rboolean utf8strIsASCII(char *str)
@@ -985,19 +998,6 @@ void mbcsToLatin1(char *in, char *out)
             out[i] = (char)wbuff[i];
     }
     out[res] = '\0';
-}
-
-void attribute_hidden markKnown(SEXP x, SEXP ref)
-{
-    if (TYPEOF(x) != CHARSXP)
-        error("invalid use of 'markKnown'");
-    if (IS_LATIN1(ref) || IS_UTF8(ref))
-    {
-        if (known_to_be_latin1)
-            SET_LATIN1(x);
-        if (known_to_be_utf8)
-            SET_UTF8(x);
-    }
 }
 
 /* MBCS-aware versions of common comparisons.  Only used for ASCII c */
