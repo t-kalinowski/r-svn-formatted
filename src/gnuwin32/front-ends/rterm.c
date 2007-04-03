88b0f1cccb905bb6b732255ae71ccd4bdcb3ade6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-04-03 14:33:48 +0000

    add completion for Rterm
    git-svn-id: https://svn.r-project.org/R/trunk@41020 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/rterm.c
+++ src/gnuwin32/front-ends/rterm.c
@@ -40,6 +40,7 @@ __declspec(dllimport) extern char *R_HistoryFile;
 
 extern char *getDLLVersion();
 extern void saveConsoleTitle();
+extern void R_gl_tab_set(void);
 
 static char Rversion[25];
 char *getRVersion()
@@ -73,6 +74,7 @@ int AppMain(int argc, char **argv)
     readconsolecfg();
     if (R_Interactive)
     {
+        R_gl_tab_set();
         gl_hist_init(R_HistorySize, 1);
         if (R_RestoreHistory)
             gl_loadhistory(R_HistoryFile);
