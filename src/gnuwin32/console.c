2739380b7687309f8466a336fd173b296abf79e7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-04-29 17:20:09 +0000

    alter the way completion on Rgui uses a scratchpad
    git-svn-id: https://svn.r-project.org/R/trunk@41381 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.c
+++ src/gnuwin32/console.c
@@ -390,6 +390,7 @@ ConsoleData newconsoledata(font f, int rows, int cols, int bufbytes, int bufline
     p->overwrite = 0;
     p->lazyupdate = 1;
     p->needredraw = 0;
+    p->wipe_completion = 0;
     p->my0 = p->my1 = -1;
     p->mx0 = 5;
     p->mx1 = 14;
@@ -741,7 +742,7 @@ static int writeline(ConsoleData p, int i, int j)
     return len;
 }
 
-void drawconsole(control c, rect r)
+void drawconsole(control c, rect r) /* r is unused here */
 {
     ConsoleData p = getdata(c);
 
@@ -975,10 +976,35 @@ void consoleflush(control c)
 
 /* free ^G ^Q ^R ^S, perhaps ^J */
 
+static void checkpointpos(xbuf p, int save)
+{
+    static int ns, av;
+    static char *free;
+    if (save)
+    {
+        ns = p->ns;
+        av = p->av;
+        free = p->free;
+    }
+    else
+    {
+        p->ns = ns;
+        p->av = av;
+        p->free = free;
+    }
+}
+
 static void storekey(control c, int k)
 {
     ConsoleData p = getdata(c);
 
+    if (p->wipe_completion)
+    {
+        p->wipe_completion = 0;
+        checkpointpos(p->lbuf, 0);
+        p->needredraw = 1;
+        REDRAW;
+    }
     if (p->kind == PAGER)
         return;
     if (k == BKSP)
@@ -1011,7 +1037,7 @@ void set_rcompgen_available(int x)
 static void performCompletion(control c)
 {
     ConsoleData p = getdata(c);
-    int i, alen, max_show = 10, cursor_position = p->c - prompt_wid;
+    int i, alen, alen2, max_show = 10, cursor_position = p->c - prompt_wid;
     char *partial_line = LINE(NUMLINES - 1) + prompt_wid;
     char *additional_text;
     char *pline, *cmd;
@@ -1091,10 +1117,13 @@ static void performCompletion(control c)
 #define POSSIBLE 1
 
     alen = length(VECTOR_ELT(ans, POSSIBLE));
+    additional_text = CHAR(STRING_ELT(VECTOR_ELT(ans, ADDITION), 0));
+    alen2 = strlen(additional_text);
     if (alen)
     {
         /* make a copy of the current string first */
         char *buf1, *p1 = LINE(NUMLINES - 1);
+        checkpointpos(p->lbuf, 1);
         buf1 = alloca(strlen(p1) + 1);
         sprintf(buf1, "%s\n", p1);
         consolewrites(c, buf1);
@@ -1106,11 +1135,11 @@ static void performCompletion(control c)
         }
         if (alen > max_show)
             consolewrites(c, "\n[...truncated]\n");
+        p->wipe_completion = 1;
     }
-    additional_text = CHAR(STRING_ELT(VECTOR_ELT(ans, ADDITION), 0));
-    alen = strlen(additional_text);
-    if (alen)
-        for (i = 0; i < alen; i++)
+
+    if (alen2)
+        for (i = 0; i < alen2; i++)
             storekey(c, additional_text[i]);
     return;
 }
