607476a6dd687ac1de5ca7520620970b7ccff53b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-05-10 18:38:30 +0000

    work around a libintl problem
    git-svn-id: https://svn.r-project.org/R/trunk@41525 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/errors.c
+++ src/main/errors.c
@@ -926,7 +926,8 @@ SEXP attribute_hidden do_ngettext(SEXP call, SEXP op, SEXP args, SEXP rho)
     else
         errorcall(call, _("invalid '%s' value"), "domain");
 
-    if (strlen(domain))
+    /* libintl seems to malfunction if given a message of "" */
+    if (strlen(domain) && length(STRING_ELT(msg1, 0)))
     {
         char *fmt = dngettext(domain, translateChar(STRING_ELT(msg1, 0)), translateChar(STRING_ELT(msg2, 0)), n);
         PROTECT(ans = allocVector(STRSXP, 1));
