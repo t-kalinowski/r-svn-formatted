710a0ae66daca9fc793e888bc1116a9cde3d56ee
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-06-02 14:42:30 +0000

    add const
    git-svn-id: https://svn.r-project.org/R/trunk@41785 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/dynload.c
+++ src/gnuwin32/dynload.c
@@ -54,7 +54,7 @@ static DL_FUNC getRoutine(DllInfo *info, char const *name);
 static void R_deleteCachedSymbols(DllInfo *dll);
 
 static void R_getDLLError(char *buf, int len);
-static void GetFullDLLPath(SEXP call, char *buf, char *path);
+static void GetFullDLLPath(SEXP call, char *buf, const char *path);
 
 static void closeLibrary(HINSTANCE handle)
 {
@@ -137,7 +137,7 @@ static void R_getDLLError(char *buf, int len)
     LocalFree(lpMsgBuf);
 }
 
-static void GetFullDLLPath(SEXP call, char *buf, char *path)
+static void GetFullDLLPath(SEXP call, char *buf, const char *path)
 {
     char *p;
 
