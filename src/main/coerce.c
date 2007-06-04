d658942b76d29f5281cf9643d6ff1f13919bbde1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-06-04 20:01:47 +0000

    typo in last change
    git-svn-id: https://svn.r-project.org/R/trunk@41816 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/coerce.c
+++ src/main/coerce.c
@@ -353,7 +353,7 @@ Rcomplex attribute_hidden ComplexFromString(SEXP x, int *warn)
     char *endp;
 
     z.r = z.i = NA_REAL;
-    if (x != R_NaString && !isBlankString(endp))
+    if (x != R_NaString && !isBlankString(xx))
     {
         xr = R_strtod(xx, &endp);
         if (isBlankString(endp))
@@ -363,7 +363,7 @@ Rcomplex attribute_hidden ComplexFromString(SEXP x, int *warn)
         }
         else if (*endp == '+' || *endp == '-')
         {
-            xi = R_strtod(xx, &endp);
+            xi = R_strtod(endp, &endp);
             if (*endp++ == 'i' && isBlankString(endp))
             {
                 z.r = xr;
