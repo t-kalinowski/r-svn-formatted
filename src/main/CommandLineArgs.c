4907babd559842c6493c95e99f273e407cb8687a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-08-20 11:44:44 +0000

    move Rbitmap.dll to grDevices where it is used clean up loadRconsole more notes on where entry points are used.
    git-svn-id: https://svn.r-project.org/R/trunk@42569 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/CommandLineArgs.c
+++ src/main/CommandLineArgs.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1997-2006   Robert Gentleman, Ross Ihaka
+ *  Copyright (C) 1997-2007   Robert Gentleman, Ross Ihaka
  *                            and the R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -82,7 +82,7 @@ SEXP attribute_hidden do_commandArgs(SEXP call, SEXP op, SEXP args, SEXP env)
 }
 
 #ifdef Win32
-extern int R_LoadRconsole;
+extern Rboolean R_LoadRconsole;
 #endif
 
 void R_common_command_line(int *pac, char **argv, Rstart Rp)
@@ -149,7 +149,7 @@ void R_common_command_line(int *pac, char **argv, Rstart Rp)
                 R_RestoreHistory = 0;             /* --no-restore-history */
                 Rp->NoRenviron = TRUE;
 #ifdef Win32
-                R_LoadRconsole = 0;
+                R_LoadRconsole = FALSE;
 #endif
             }
             else if (!strcmp(*av, "--no-environ"))
