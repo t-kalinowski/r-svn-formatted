86bd8c6fe13e649756a0e86a0ac0c5b9c383de95
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-12-22 10:28:01 +0000

    more on const char *
    git-svn-id: https://svn.r-project.org/R/trunk@43761 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/system.c
+++ src/unix/system.c
@@ -507,7 +507,7 @@ int Rf_initialize_R(int ac, char **av)
  *     file    = array of filenames
  *     editor  = editor to be used.
  */
-int R_EditFiles(int nfile, comst char **file, const char **title, const char *editor)
+int R_EditFiles(int nfile, const char **file, const char **title, const char *editor)
 {
     char buf[1024];
 #if defined(HAVE_AQUA)
