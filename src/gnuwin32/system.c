28f1134ebfc55b74a74fba158a984136bebc2cc7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-23 14:55:29 +0000

    report which encoding is giving trouble.
    git-svn-id: https://svn.r-project.org/R/trunk@44113 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/system.c
+++ src/gnuwin32/system.c
@@ -297,7 +297,7 @@ static int FileReadConsole(const char *prompt, char *buf, int len, int addhistor
         err = (res == (size_t)(-1));
         /* errors lead to part of the input line being ignored */
         if (err)
-            fputs(_("<ERROR: invalid input in encoding>\n"), stdout);
+            printf(_("<ERROR: invalid input in encoding '%s'>\n"), R_StdinEnc);
         strncpy(buf, obuf, len);
     }
 
