6da168f0c7006a8178da695fe36ce9e6f33fade1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-26 11:33:52 +0000

    another buffer-size issue
    git-svn-id: https://svn.r-project.org/R/trunk@44179 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/engine.c
+++ src/main/engine.c
@@ -2045,7 +2045,7 @@ void GESymbol(double x, double y, int pch, double size, R_GE_gcontext *gc, GEDev
     else if (pch < 0)
     {
         int res;
-        char str[7];
+        char str[16];
         if (gc->fontface == 5)
             error("use of negative pch with symbol font is invalid");
         res = ucstoutf8(str, -pch);
