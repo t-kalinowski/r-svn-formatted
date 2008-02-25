59b865d8865f6c421d4553804a1dd93468f8cafe
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-02-25 14:01:52 +0000

    add Fortran type declarations in two cases, mainly for didactical reasons
    git-svn-id: https://svn.r-project.org/R/trunk@44607 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/init.c
+++ src/library/stats/src/init.c
@@ -80,9 +80,17 @@ static R_NativePrimitiveArgType spline_coef_t[] = {INTSXP,  INTSXP,  REALSXP, RE
 static R_NativePrimitiveArgType spline_eval_t[] = {INTSXP,  INTSXP,  REALSXP, REALSXP, INTSXP,
                                                    REALSXP, REALSXP, REALSXP, REALSXP, REALSXP};
 
+static R_NativePrimitiveArgType lowesp_t[] = {REALSXP, INTSXP, REALSXP, INTSXP};
+static R_NativePrimitiveArgType lowesw_t[] = {INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, INTSXP, REALSXP};
+
 #define CDEF(name)                                                                                                     \
     {                                                                                                                  \
 #name, (DL_FUNC)&name, sizeof(name##_t) / sizeof(name##_t[0]), name##_t                                        \
+    }
+
+#define FDEF(name)                                                                                                     \
+    {                                                                                                                  \
+#name, (DL_FUNC)&F77_SUB(name), sizeof(name##_t) / sizeof(name##_t[0]), name##_t                               \
     }
 
 static const R_CMethodDef CEntries[] = {{"chisqsim", (DL_FUNC)&chisqsim, 11, chisqsim_t},
@@ -177,15 +185,22 @@ static const R_CallMethodDef CallEntries[] = {{"R_cutree", (DL_FUNC)&R_cutree, 2
                                               {"binomial_dev_resids", (DL_FUNC)&binomial_dev_resids, 3},
                                               {NULL, NULL, 0}};
 
-static const R_FortranMethodDef FortEntries[] = {
-    {"lowesw", (DL_FUNC)&F77_SUB(lowesw), 4},  {"lowesp", (DL_FUNC)&F77_SUB(lowesp), 7},
-    {"setppr", (DL_FUNC)&F77_SUB(setppr), 6},  {"smart", (DL_FUNC)&F77_SUB(smart), 16},
-    {"pppred", (DL_FUNC)&F77_SUB(pppred), 5},  {"setsmu", (DL_FUNC)&F77_SUB(setsmu), 0},
-    {"qsbart", (DL_FUNC)&F77_SUB(qsbart), 21}, {"bvalus", (DL_FUNC)&F77_SUB(bvalus), 7},
-    {"supsmu", (DL_FUNC)&F77_SUB(supsmu), 10}, {"hclust", (DL_FUNC)&F77_SUB(hclust), 11},
-    {"hcass2", (DL_FUNC)&F77_SUB(hcass2), 6},  {"kmns", (DL_FUNC)&F77_SUB(kmns), 17},
-    {"eureka", (DL_FUNC)&F77_SUB(eureka), 6},  {"stl", (DL_FUNC)&F77_SUB(stl), 18},
-    {"lminfl", (DL_FUNC)&F77_SUB(lminfl), 11}, {NULL, NULL, 0}};
+static const R_FortranMethodDef FortEntries[] = {FDEF(lowesw),
+                                                 FDEF(lowesp),
+                                                 {"setppr", (DL_FUNC)&F77_SUB(setppr), 6},
+                                                 {"smart", (DL_FUNC)&F77_SUB(smart), 16},
+                                                 {"pppred", (DL_FUNC)&F77_SUB(pppred), 5},
+                                                 {"setsmu", (DL_FUNC)&F77_SUB(setsmu), 0},
+                                                 {"qsbart", (DL_FUNC)&F77_SUB(qsbart), 21},
+                                                 {"bvalus", (DL_FUNC)&F77_SUB(bvalus), 7},
+                                                 {"supsmu", (DL_FUNC)&F77_SUB(supsmu), 10},
+                                                 {"hclust", (DL_FUNC)&F77_SUB(hclust), 11},
+                                                 {"hcass2", (DL_FUNC)&F77_SUB(hcass2), 6},
+                                                 {"kmns", (DL_FUNC)&F77_SUB(kmns), 17},
+                                                 {"eureka", (DL_FUNC)&F77_SUB(eureka), 6},
+                                                 {"stl", (DL_FUNC)&F77_SUB(stl), 18},
+                                                 {"lminfl", (DL_FUNC)&F77_SUB(lminfl), 11},
+                                                 {NULL, NULL, 0}};
 
 void
 #ifdef HAVE_VISIBILITY_ATTRIBUTE
