befaf8ec988ab4564775837470f6bebb287f68bc
urbaneks <urbaneks@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-03-02 21:53:05 +0000

    fix Cairo w/o Pango compilation in X11
    git-svn-id: https://svn.r-project.org/R/trunk@44661 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/devX11.c
+++ src/modules/X11/devX11.c
@@ -2440,11 +2440,11 @@ int Rf_setX11DeviceData(pDevDesc dd, double gamma_fac, pX11Desc xd)
             /* Pango's default resolution is 96 dpi */
             ps *= 1.0 / (96.0 * pixelWidth());
 #else
-            /* Cairo's default resolution is 96 dpi */
+            /* Cairo's default resolution is 72 dpi */
             ps *= 1.0 / (72.0 * pixelWidth());
 #endif
-#endif
         }
+#endif
     }
 
     /* Character Addressing Offsets */
@@ -2461,7 +2461,7 @@ int Rf_setX11DeviceData(pDevDesc dd, double gamma_fac, pX11Desc xd)
 #ifdef HAVE_WORKING_CAIRO
     dd->canHAdj = xd->useCairo ? 2 : 0;
 #else
-dd->canHAdj = 0;
+    dd->canHAdj = 0;
 #endif
     dd->canChangeGamma = FALSE;
 
