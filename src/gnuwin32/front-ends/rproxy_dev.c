8afc68e65f1deae55b7e25932ab4b303abf757a1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-03-05 09:40:43 +0000

    update
    git-svn-id: https://svn.r-project.org/R/trunk@44679 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/rproxy_dev.c
+++ src/gnuwin32/front-ends/rproxy_dev.c
@@ -379,7 +379,6 @@ int R_Proxy_Graphics_Driver_CB(R_Proxy_Graphics_CB *pDD, char *pDisplay, double 
     }
     /* Set up Data Structures  */
 
-    DEVDESC(pDD)->open = R_Proxy_Graphics_Open_CB;
     DEVDESC(pDD)->close = R_Proxy_Graphics_Close_CB;
     DEVDESC(pDD)->activate = R_Proxy_Graphics_Activate_CB;
     DEVDESC(pDD)->deactivate = R_Proxy_Graphics_Deactivate_CB;
@@ -761,7 +760,6 @@ int R_Proxy_Graphics_Driver_Recorder(R_Proxy_Graphics_Recorder *pDD, char *pDisp
     }
     /* Set up Data Structures  */
 
-    DEVDESC(pDD)->open = R_Proxy_Graphics_Open_Recorder;
     DEVDESC(pDD)->close = R_Proxy_Graphics_Close_Recorder;
     DEVDESC(pDD)->activate = R_Proxy_Graphics_Activate_Recorder;
     DEVDESC(pDD)->deactivate = R_Proxy_Graphics_Deactivate_Recorder;
