ffa1751a067385e3781c1542aee69e64a2ead38b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-03-11 16:58:30 +0000

    more r*(., scale=0) |--> return(location) changes
    git-svn-id: https://svn.r-project.org/R/trunk@44737 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/rt.c
+++ src/nmath/rt.c
@@ -1,7 +1,7 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-2001 The R Development Core Team
+ *  Copyright (C) 2000-2008 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -30,7 +30,6 @@
 
 double rt(double df)
 {
-    double num;
     if (ISNAN(df) || df <= 0.0)
         ML_ERR_return_NAN;
 
@@ -40,7 +39,7 @@ double rt(double df)
     {
         /* Some compilers (including MW6) evaluated this from right to left
             return norm_rand() / sqrt(rchisq(df) / df); */
-        num = norm_rand();
+        double num = norm_rand();
         return num / sqrt(rchisq(df) / df);
     }
 }
