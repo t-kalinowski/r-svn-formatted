8520826c87965aaba174149c79dcb3b3ffd48257
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-03-28 16:07:37 +0000

    tidy up encoding auxilliaries
    git-svn-id: https://svn.r-project.org/R/trunk@44967 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -3211,14 +3211,6 @@ void attribute_hidden(SET_UTF8)(SEXP x)
 {
     SET_UTF8(x);
 }
-void attribute_hidden(UNSET_LATIN1)(SEXP x)
-{
-    UNSET_LATIN1(x);
-}
-void attribute_hidden(UNSET_UTF8)(SEXP x)
-{
-    UNSET_UTF8(x);
-}
 int attribute_hidden(ENC_KNOWN)(SEXP x)
 {
     return ENC_KNOWN(x);
