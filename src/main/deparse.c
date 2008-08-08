565d48bec98f62bab05dccf2fcd554491d18e0af
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-08-08 04:54:32 +0000

    maintain stack order
    git-svn-id: https://svn.r-project.org/R/trunk@46256 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -305,12 +305,13 @@ SEXP attribute_hidden do_dput(SEXP call, SEXP op, SEXP args, SEXP rho)
     if (!isNull(CADDR(args)))
         opts = asInteger(CADDR(args));
 
-    PROTECT(tval = deparse1(tval, 0, opts));
+    tval = deparse1(tval, 0, opts);
     if (TYPEOF(CAR(args)) == CLOSXP)
     {
         SET_CLOENV(CAR(args), saveenv);
         UNPROTECT(1);
     }
+    PROTECT(tval); /* against Rconn_printf */
     ifile = asInteger(CADR(args));
 
     wasopen = 1;
