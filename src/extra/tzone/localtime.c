2cdd13b28f4c1da482e58ff106e15f19046c0cf1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-11-02 22:27:21 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@46815 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/tzone/localtime.c
+++ src/extra/tzone/localtime.c
@@ -357,12 +357,12 @@ register const int doextend;
         }
         if (doaccess && access(name, R_OK) != 0)
         {
-            Rf_warning("unknwon timezone '%s'", sname);
+            Rf_warning("unknown timezone '%s'", sname);
             return -1;
         }
         if ((fid = open(name, OPEN_MODE)) == -1)
         {
-            Rf_warning("unknwon timezone '%s'", sname);
+            Rf_warning("unknown timezone '%s'", sname);
             return -1;
         }
     }
