5a0c6802f067cb40c730844e8dd4dcdb705f1c98
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-01-11 17:41:24 +0000

    revert r47536
    git-svn-id: https://svn.r-project.org/R/trunk@47546 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -56,7 +56,9 @@ static void mbcsToSbcs(const char *in, char *out, const char *encoding, int enc)
 extern int errno;
 #endif
 
+#ifdef Win32
 #define USE_GZIO
+#endif
 
 #ifdef USE_GZIO
 #include <zlib.h>
