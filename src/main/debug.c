4c194b0518d441ca31d435b71e8bed724eb274bd
rgentlem <rgentlem@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-07-28 00:12:13 +0000

    also allowed one to debug SPECIALS and BUILTINS - the reason for this is that if they have methods that are implemented as closures then we will hit the debugger for them
    git-svn-id: https://svn.r-project.org/R/trunk@49006 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/debug.c
+++ src/main/debug.c
@@ -39,7 +39,8 @@ SEXP attribute_hidden do_debug(SEXP call, SEXP op, SEXP args, SEXP rho)
     }
     find_char_fun
 
-        if (TYPEOF(CAR(args)) != CLOSXP) errorcall(call, _("argument must be a closure"));
+        if (TYPEOF(CAR(args)) != CLOSXP && TYPEOF(CAR(args)) != SPECIALSXP && TYPEOF(CAR(args)) != BUILTINSXP)
+            errorcall(call, _("argument must be a closure"));
     switch (PRIMVAL(op))
     {
     case 0:
