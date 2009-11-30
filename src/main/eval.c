a3f2ea37ae8828ec6bb3f1036d9aa25b8fbb47ab
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-11-30 13:45:37 +0000

    comment out unused variable
    git-svn-id: https://svn.r-project.org/R/trunk@50617 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -2037,8 +2037,9 @@ static SEXP evalArgs(SEXP el, SEXP rho, SEXP op, int dropmissing)
         return evalListKeepMissing(el, rho);
 }
 
-/* A version of DispatchOrEval that checks for possible S4 methods for any argument, not just
- * the first.  Used in the code for `[` in do_subset.  differs in that all arguments are evaluated
+/* A version of DispatchOrEval that checks for possible S4 methods for
+ * any argument, not just the first.  Used in the code for `[` in
+ * do_subset.  Differs in that all arguments are evaluated
  * immediately, rather than after the call to R_possible_dispatch.
  */
 attribute_hidden int DispatchAnyOrEval(SEXP call, SEXP op, const char *generic, SEXP args, SEXP rho, SEXP *ans,
@@ -2047,7 +2048,7 @@ attribute_hidden int DispatchAnyOrEval(SEXP call, SEXP op, const char *generic, 
     if (R_has_methods(op))
     {
         SEXP argValue, el, value;
-        Rboolean hasS4 = FALSE;
+        /* Rboolean hasS4 = FALSE; */
         int nprotect = 0, dispatch;
         if (!argsevald)
         {
