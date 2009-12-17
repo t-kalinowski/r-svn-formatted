29517cff9e0573555095903482f80c3ae27a0c83
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-12-17 11:11:32 +0000

    warning -> error for embedded nuls and unrecognized escapes
    git-svn-id: https://svn.r-project.org/R/trunk@50768 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gram.c
+++ src/main/gram.c
@@ -151,8 +151,7 @@ enum yytokentype
 /*
  *  R : A Computer Langage for Statistical Data Analysis
  *  Copyright (C) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1997--2008  Robert Gentleman, Ross Ihaka and the
- *                            R Development Core Team
+ *  Copyright (C) 1997--2009  R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -4416,7 +4415,6 @@ static SEXP mkStringUTF8(const ucs_t *wcs, int cnt)
 static int StringValue(int c, Rboolean forSymbol)
 {
     int quote = c;
-    int have_warned = 0;
     char currtext[1010], *ct = currtext;
     char st0[MAXELTSIZE];
     unsigned int nstext = MAXELTSIZE;
@@ -4486,12 +4484,9 @@ static int StringValue(int c, Rboolean forSymbol)
                         CTEXT_POP();
                         if (i == 0)
                         { /* was just \x */
-                            if (GenerateCode && R_WarnEscapes)
-                            {
-                                have_warned++;
-                                warningcall(R_NilValue, _("'\\x' used without hex digits"));
-                            }
-                            val = 'x';
+                            *ct = '\0';
+                            errorcall(R_NilValue,
+                                      _("'\\x' used without hex digits in character string starting \"%s\""), currtext);
                         }
                         break;
                     }
@@ -4529,13 +4524,10 @@ static int StringValue(int c, Rboolean forSymbol)
                         xxungetc(c);
                         CTEXT_POP();
                         if (i == 0)
-                        { /* was just \x */
-                            if (GenerateCode && R_WarnEscapes)
-                            {
-                                have_warned++;
-                                warningcall(R_NilValue, _("\\u used without hex digits"));
-                            }
-                            val = 'u';
+                        { /* was just \u */
+                            *ct = '\0';
+                            errorcall(R_NilValue,
+                                      _("'\\u' used without hex digits in character string starting \"%s\""), currtext);
                         }
                         break;
                     }
@@ -4581,13 +4573,10 @@ static int StringValue(int c, Rboolean forSymbol)
                         xxungetc(c);
                         CTEXT_POP();
                         if (i == 0)
-                        { /* was just \x */
-                            if (GenerateCode && R_WarnEscapes)
-                            {
-                                have_warned++;
-                                warningcall(R_NilValue, _("\\U used without hex digits"));
-                            }
-                            val = 'U';
+                        { /* was just \U */
+                            *ct = '\0';
+                            errorcall(R_NilValue,
+                                      _("'\\U' used without hex digits in character string starting \"%s\""), currtext);
                         }
                         break;
                     }
@@ -4638,12 +4627,9 @@ static int StringValue(int c, Rboolean forSymbol)
                 case '\n':
                     break;
                 default:
-                    if (GenerateCode && R_WarnEscapes)
-                    {
-                        have_warned++;
-                        warningcall(R_NilValue, _("'\\%c' is an unrecognized escape in a character string"), c);
-                    }
-                    break;
+                    *ct = '\0';
+                    errorcall(R_NilValue, _("'\\%c' is an unrecognized escape in character string starting \"%s\""), c,
+                              currtext);
                 }
             }
         }
@@ -4715,18 +4701,6 @@ static int StringValue(int c, Rboolean forSymbol)
             PROTECT(yylval = mkString2(stext, bp - stext - 1));
         if (stext != st0)
             free(stext);
-        if (have_warned)
-        {
-            *ct = '\0';
-#ifdef ENABLE_NLS
-            warningcall(R_NilValue,
-                        ngettext("unrecognized escape removed from \"%s\"", "unrecognized escapes removed from \"%s\"",
-                                 have_warned),
-                        currtext);
-#else
-            warningcall(R_NilValue, "unrecognized escape(s) removed from \"%s\"", currtext);
-#endif
-        }
         return STR_CONST;
     }
 }
