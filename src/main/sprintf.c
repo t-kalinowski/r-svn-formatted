404023a3cc9c3f7f704e6bfd4709b7a3a49bd0f5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-01-05 19:57:02 +0000

    sprintf was not re-entrant
    git-svn-id: https://svn.r-project.org/R/trunk@50894 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sprintf.c
+++ src/main/sprintf.c
@@ -25,6 +25,7 @@
 
 #include <Defn.h>
 #include "RBufferUtils.h"
+#include <R_ext/RS.h> /* for Calloc/Free */
 
 #define MAXLINE MAXELTSIZE
 #define MAXNARGS 100
@@ -350,9 +351,15 @@ SEXP attribute_hidden do_sprintf(SEXP call, SEXP op, SEXP args, SEXP env)
                             case 's':
                                 if (TYPEOF(_this) != STRSXP)
                                 {
+                                    /* as.character method might call sprintf() */
+                                    int nc = strlen(outputString);
+                                    char *z = Calloc(nc + 1, char);
+                                    strcpy(z, outputString);
                                     PROTECT(tmp = lang2(install("as.character"), _this));
 
                                     COERCE_THIS_TO_A
+                                    strcpy(outputString, z);
+                                    Free(z);
                                 }
                                 break;
                             default:
