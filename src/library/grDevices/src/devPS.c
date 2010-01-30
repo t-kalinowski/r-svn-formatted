a5b905a3da7d77d39b02101c98739575290bf5ba
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-01-30 07:56:06 +0000

    avoid compiler warning
    git-svn-id: https://svn.r-project.org/R/trunk@51070 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1998--2008  Robert Gentleman, Ross Ihaka and the
+ *  Copyright (C) 1998--2010  Robert Gentleman, Ross Ihaka and the
  *                            R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -5926,7 +5926,7 @@ Rboolean PDFDeviceDriver(pDevDesc dd, const char *file, const char *paper, const
 
     if (strlen(file) > PATH_MAX - 1)
     {
-        PDFcleanup(0, pd);
+        /* not yet created PDFcleanup(0, pd); */
         free(dd);
         error(_("filename too long in pdf"));
     }
