843069ffb43711c6eb6f7496a52193621cedeeea
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-02-21 08:21:01 +0000

    use preferred capitalization of MinGW
    git-svn-id: https://svn.r-project.org/R/trunk@51157 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/graphappmain.c
+++ src/gnuwin32/front-ends/graphappmain.c
@@ -5,7 +5,7 @@
    symbols, as do the MS compilers.
 
    The mingw-w64-crt is different, but this file is not used on
-   mingw64
+   MinGW-w64
 */
 
 extern void GA_startgraphapp(HINSTANCE Instance, HINSTANCE PrevInstance, int CmdShow);
