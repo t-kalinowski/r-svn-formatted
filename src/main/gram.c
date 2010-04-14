4ae423522035a6e08304c28fc9553f8b528daea6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-04-14 20:33:42 +0000

    workaround for Solaris bug
    git-svn-id: https://svn.r-project.org/R/trunk@51733 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gram.c
+++ src/main/gram.c
@@ -286,6 +286,7 @@ static int mbcs_get_next(int c, wchar_t *wc)
             if (s[i] == R_EOF)
                 error(_("EOF whilst reading MBCS char at line %d"), ParseState.xxlineno);
         }
+        s[clen] = '\0'; /* x86 Solaris requires this */
         res = mbrtowc(wc, s, clen, NULL);
         if (res == -1)
             error(_("invalid multibyte character in parser at line %d"), ParseState.xxlineno);
@@ -4348,6 +4349,7 @@ static int mbcs_get_next2(int c, ucs_t *wc)
             if (s[i] == R_EOF)
                 error(_("EOF whilst reading MBCS char at line %d"), ParseState.xxlineno);
         }
+        s[clen] = '\0'; /* x86 Solaris requires this */
         res = mbtoucs(wc, s, clen);
         if (res == -1)
             error(_("invalid multibyte character in parser at line %d"), ParseState.xxlineno);
