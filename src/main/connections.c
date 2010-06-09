988f1f372b025d420629c833782191faaa53dd24
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-06-09 07:51:47 +0000

    give warning if gzfile(, "a") is attempted
    git-svn-id: https://svn.r-project.org/R/trunk@52237 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2000-9   The R Development Core Team.
+ *  Copyright (C) 2000-10   The R Development Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -1306,6 +1306,11 @@ static Rboolean gzfile_open(Rconnection con)
 
     strcpy(mode, con->mode);
     /* Must open as binary, only "r" and "w" are supported */
+    if (con->mode[0] == 'a')
+    {
+        warning(_("appending is not supported for gzfile connections"));
+        return FALSE;
+    }
     if (strchr(con->mode, 'w'))
         sprintf(mode, "wb%1d", gzcon->compress);
     else
@@ -1319,7 +1324,7 @@ static Rboolean gzfile_open(Rconnection con)
     }
     ((Rgzfileconn)(con->private))->fp = fp;
     con->isopen = TRUE;
-    con->canwrite = (con->mode[0] == 'w' || con->mode[0] == 'a');
+    con->canwrite = (con->mode[0] == 'w');
     con->canread = !con->canwrite;
     con->text = strchr(con->mode, 'b') ? FALSE : TRUE;
     set_iconv(con);
