bff1d336fe48d995dea46f1660ed4f32752215b0
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-06-15 00:44:50 +0000

    GERaster does nothing if width or height are zero
    git-svn-id: https://svn.r-project.org/R/trunk@52280 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/engine.c
+++ src/main/engine.c
@@ -1448,7 +1448,11 @@ void GERaster(unsigned int *raster, int w, int h, double x, double y, double wid
      * Maybe not too bad because it is just a matter of shaving off
      * some rows and columns from the image? (because R only does
      * rectangular clipping regions) */
-    dd->dev->raster(raster, w, h, x, y, width, height, angle, interpolate, gc, dd->dev);
+
+    if (width != 0 && height != 0)
+    {
+        dd->dev->raster(raster, w, h, x, y, width, height, angle, interpolate, gc, dd->dev);
+    }
 }
 
 /****************************************************************
