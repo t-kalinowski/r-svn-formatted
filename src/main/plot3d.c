d3a2adbfb2bb9ab66e5f3653eb93ed4a109c0b3f
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-08-06 12:21:33 +0000

    Fix contour(lty=2).
    git-svn-id: https://svn.r-project.org/R/trunk@52683 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plot3d.c
+++ src/main/plot3d.c
@@ -1294,11 +1294,10 @@ static void contour(SEXP x, int nx, SEXP y, int ny, SEXP z, double zc, SEXP labe
                         }
                         else
                         {
-                            for (iii = 0; iii < indx; iii++)
-                                GLine(xxx[iii], yyy[iii], xxx[iii + 1], yyy[iii + 1], USER, dd);
-                            for (iii = indx + range; iii < ns - 1; iii++)
-                                GLine(xxx[iii], yyy[iii], xxx[iii + 1], yyy[iii + 1], USER, dd);
-
+                            if (indx > 0)
+                                GPolyline(indx + 1, xxx, yyy, USER, dd);
+                            if (ns - 1 - indx - range > 0)
+                                GPolyline(ns - indx - range, xxx + indx + range, yyy + indx + range, USER, dd);
                             if (gotLabel)
                             {
                                 /* find which plot edge we are closest to */
