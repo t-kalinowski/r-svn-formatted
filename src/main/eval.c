8de5c46ebbb79fce11e10b44b7eec1f130c19c45
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-09-27 21:47:25 +0000

    Reset the ftype register after a call in bceval.
    git-svn-id: https://svn.r-project.org/R/trunk@53047 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -3768,6 +3768,7 @@ static SEXP bcEval(SEXP body, SEXP rho)
             }
             R_BCNodeStackTop -= 2;
             R_BCNodeStackTop[-1] = value;
+            ftype = 0;
             NEXT();
         }
         OP(CALLBUILTIN, 1) :
