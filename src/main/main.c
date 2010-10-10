34d42dfacebd1e5bc31a9614aeddc59e222d806a
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-10 02:07:28 +0000

    Added mechanism for enabling some forms of just in time compilation.
    git-svn-id: https://svn.r-project.org/R/trunk@53267 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -1018,6 +1018,11 @@ void setup_Rmainloop(void)
         REprintf(_("During startup - "));
         PrintWarnings();
     }
+
+#ifdef BYTECODE
+    /* trying to do this earlier seems to run into bootstrapping issues. */
+    R_init_jit_enabled();
+#endif
 }
 
 extern SA_TYPE SaveAction; /* from src/main/startup.c */
