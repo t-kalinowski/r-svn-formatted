a1482e7c9d0b76331f42bf81ce2e8f83824449eb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-15 15:57:34 +0000

    avoid compiler warning
    git-svn-id: https://svn.r-project.org/R/trunk@53324 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/dataentry.c
+++ src/modules/X11/dataentry.c
@@ -2552,7 +2552,7 @@ void popupmenu(DEstruct DE, int x_pos, int y_pos, int col, int row)
             button = event.xbutton.button;
             selected_pane = event.xbutton.window;
             for (i = 0; selected_pane != menupanes[i]; i++)
-                if (i >= 4)
+                if (i >= 3)
                     goto done;
             while (1)
             {
