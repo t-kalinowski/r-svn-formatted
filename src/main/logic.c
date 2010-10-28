9e16f5c3305d79abaa3b83b84d7607c5739ca65f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-28 23:46:40 +0000

    fix for PR#14244
    git-svn-id: https://svn.r-project.org/R/trunk@53467 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/logic.c
+++ src/main/logic.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1999--2009  The R Development Core Team.
+ *  Copyright (C) 1999--2010  The R Development Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -174,10 +174,13 @@ static SEXP lunary(SEXP call, SEXP op, SEXP arg)
     int i, len;
 
     len = LENGTH(arg);
-    if (len == 0)
-        return (allocVector(LGLSXP, 0));
     if (!isLogical(arg) && !isNumber(arg) && !isRaw(arg))
+    {
+        /* For back-compatibility */
+        if (!len)
+            return allocVector(LGLSXP, 0);
         errorcall(call, _("invalid argument type"));
+    }
     PROTECT(names = getAttrib(arg, R_NamesSymbol));
     PROTECT(dim = getAttrib(arg, R_DimSymbol));
     PROTECT(dimnames = getAttrib(arg, R_DimNamesSymbol));
