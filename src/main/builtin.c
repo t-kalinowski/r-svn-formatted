06d44bb5bf98f77e0674cb7d0016770e89ef0809
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-29 18:59:40 +0000

    environment<- now fully duplicates a compiled closure before replacing code in order to preserve attributes and such.
    git-svn-id: https://svn.r-project.org/R/trunk@53476 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/builtin.c
+++ src/main/builtin.c
@@ -308,8 +308,7 @@ SEXP attribute_hidden do_envirgets(SEXP call, SEXP op, SEXP args, SEXP rho)
         if (TYPEOF(BODY(s)) == BCODESXP)
         {
             /* switch to interpreted version if compiled */
-            s = allocSExp(CLOSXP);
-            SET_FORMALS(s, FORMALS(CAR(args)));
+            s = duplicate(s);
             SET_BODY(s, R_ClosureExpr(CAR(args)));
         }
         else
