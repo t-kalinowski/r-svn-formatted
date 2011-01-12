d40c851cb5e8d5e2b5f40c576b5cd8611b9c18e9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-01-12 17:38:45 +0000

    marry up R-exts.{c,r,texi}, including PR#14463
    git-svn-id: https://svn.r-project.org/R/trunk@53958 00db46b3-68df-0310-9c12-caf00c1e9a41

--- doc/manual/R-exts.c
+++ doc/manual/R-exts.c
@@ -159,7 +159,7 @@ SEXP showArgs(SEXP args)
     args = CDR(args); /* skip 'name' */
     for (i = 0; args != R_NilValue; i++, args = CDR(args))
     {
-        name = CHAR(PRINTNAME(TAG(args)));
+        name = isNull(TAG(args)) ? "" : CHAR(PRINTNAME(TAG(args)));
         el = CAR(args);
         switch (TYPEOF(el))
         {
