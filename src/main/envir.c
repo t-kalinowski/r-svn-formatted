9e7890992fc61334aa3ef5a0244c703d455f7848
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-01-16 00:59:24 +0000

    Added PROTECT calls for arguments of R_NewHashedEnv.
    git-svn-id: https://svn.r-project.org/R/trunk@53984 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/envir.c
+++ src/main/envir.c
@@ -336,9 +336,11 @@ SEXP R_NewHashedEnv(SEXP enclos, SEXP size)
 {
     SEXP s;
 
+    PROTECT(enclos);
+    PROTECT(size);
     PROTECT(s = NewEnvironment(R_NilValue, R_NilValue, enclos));
     SET_HASHTAB(s, R_NewHashTable(asInteger(size)));
-    UNPROTECT(1);
+    UNPROTECT(3);
     return s;
 }
 
