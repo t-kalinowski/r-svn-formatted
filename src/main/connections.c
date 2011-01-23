b772ea01b9f13efae8b97deb785a67819d8f416c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-01-23 08:11:07 +0000

    change isSeekable() for connections with active re-encoding
    git-svn-id: https://svn.r-project.org/R/trunk@54081 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -1930,6 +1930,10 @@ SEXP attribute_hidden do_gzfile(SEXP call, SEXP op, SEXP args, SEXP env)
     Connections[ncon] = con;
     strncpy(con->encname, CHAR(STRING_ELT(enc, 0)), 100); /* ASCII */
 
+    /* see the comment in do_url */
+    if (con->encname[0] && !streql(con->encname, "native.enc"))
+        con->canseek = 0;
+
     /* open it if desired */
     if (strlen(open))
     {
@@ -5344,6 +5348,11 @@ SEXP attribute_hidden do_url(SEXP call, SEXP op, SEXP args, SEXP env)
     con->blocking = block;
     strncpy(con->encname, CHAR(STRING_ELT(enc, 0)), 100); /* ASCII */
 
+    /* only text-mode connections are affected, but we can't tell that
+       until the connection is opened, and why set an encoding on a
+       connection intended to be used in binary mode? */
+    if (con->encname[0] && !streql(con->encname, "native.enc"))
+        con->canseek = 0;
     /* open it if desired */
     if (strlen(open))
     {
