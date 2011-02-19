011f261a0081bdb5e9b027fb072e5fd4fb7a15e4
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-02-19 21:56:43 +0000

    entry point R_curErrorBuf to retrieve error msg; use it to give a meaningful error message in method selection
    git-svn-id: https://svn.r-project.org/R/trunk@54493 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/errors.c
+++ src/main/errors.c
@@ -547,6 +547,11 @@ void PrintWarnings(void)
 
 static char errbuf[BUFSIZE];
 
+const char *R_curErrorBuf()
+{
+    return (const char *)errbuf;
+}
+
 /* temporary hook to allow experimenting with alternate error mechanisms */
 static void (*R_ErrorHook)(SEXP, char *) = NULL;
 
