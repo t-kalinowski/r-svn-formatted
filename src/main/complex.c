d80d34a9923b77f4736c486ed72367d6f1d8add5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-04-23 08:47:06 +0000

    fix up cpow problem on PPC mac
    git-svn-id: https://svn.r-project.org/R/trunk@55611 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/complex.c
+++ src/main/complex.c
@@ -149,13 +149,28 @@ static R_INLINE double complex R_cpow_n(double complex X, int k)
 
   2) On Mingw (but not Mingw-w64) the system cpow is explicitly linked
      against the (slow) MSVCRT pow, and gets (0+0i)^Y as 0+0i for all Y.
+
+  3) PPC Mac OS X crashes on powers of 0+0i (at least under Rosetta).
 */
 
 static double complex mycpow(double complex X, double complex Y)
 {
-    double complex Z, yr = creal(Y), yi = cimag(Y);
+    double complex Z;
+    double yr = creal(Y), yi = cimag(Y);
     int k;
-    if (yi == 0.0 && yr == (k = (int)yr) && abs(k) <= 65536)
+    if (X == 0.0)
+    {
+        if (yi == 0.0)
+        {
+            if (yr >= 0.0)
+                Z = R_pow(0.0, yr);
+            else
+                Z = R_pow(0.0, yr) + R_NaN * I;
+        }
+        else
+            Z = R_NaN + R_NaN * I;
+    }
+    else if (yi == 0.0 && yr == (k = (int)yr) && abs(k) <= 65536)
         Z = R_cpow_n(X, k);
     else
 #ifdef HAVE_CPOW
