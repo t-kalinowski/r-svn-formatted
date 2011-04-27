da9190d32afe594fb967be0f4af543580fa2c1c1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-04-27 05:58:22 +0000

    avoid possible round-up to 256
    git-svn-id: https://svn.r-project.org/R/trunk@55656 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devWindows.c
+++ src/library/grDevices/src/devWindows.c
@@ -3009,9 +3009,9 @@ static void doRaster(unsigned int *raster, int x, int y, int w, int h, double ro
         byte alpha = R_ALPHA(raster[i]);
         double fac = alpha / 255.0;
         imageData[i * 4 + 3] = alpha;
-        imageData[i * 4 + 2] = 0.5 + fac * R_RED(raster[i]);
-        imageData[i * 4 + 1] = 0.5 + fac * R_GREEN(raster[i]);
-        imageData[i * 4 + 0] = 0.5 + fac * R_BLUE(raster[i]);
+        imageData[i * 4 + 2] = 0.49 + fac * R_RED(raster[i]);
+        imageData[i * 4 + 1] = 0.49 + fac * R_GREEN(raster[i]);
+        imageData[i * 4 + 0] = 0.49 + fac * R_BLUE(raster[i]);
     }
     setpixels(img, imageData);
     gsetcliprect(xd->bm, xd->clip);
