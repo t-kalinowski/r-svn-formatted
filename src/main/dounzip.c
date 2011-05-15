92d2ed35271f91e80bd3109a61892e77ce70d68b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-05-15 05:42:33 +0000

    prefer utimes to utime, following POSIX 2008
    git-svn-id: https://svn.r-project.org/R/trunk@55910 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/dounzip.c
+++ src/main/dounzip.c
@@ -89,16 +89,16 @@ static void setFileTime(const char *fn, tm_unz tmu_date)
         dt.tm_year = tmu_date.tm_year;
     dt.tm_isdst = -1;
     time_t ftime = mktime(&dt);
-#elif defined(HAVE_UTIMES)
-struct timeval times[2];
+#if defined(HAVE_UTIMES)
+    struct timeval times[2];
 
-times[0].tv.sec = times[1].tv.sec = ftime;
-times[0].tv.usec = times[1].tv.usec = 0;
-utimes(fn, &times);
+    times[0].tv.sec = times[1].tv.sec = ftime;
+    times[0].tv.usec = times[1].tv.usec = 0;
+    utimes(fn, &times);
 #elif defined(HAVE_UTIME)
-struct utimbuf settime;
-settime.actime = settime.modtime = ftime;
-utime(fn, &settime);
+    struct utimbuf settime;
+    settime.actime = settime.modtime = ftime;
+    utime(fn, &settime);
 #endif
 }
 #endif
