5b552ed25eda4fa1c10bb92c922b538309e92400
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-05-17 07:08:45 +0000

    more allowance for spaces in file names on a Unix-alike
    git-svn-id: https://svn.r-project.org/R/trunk@55931 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/system.c
+++ src/unix/system.c
@@ -355,10 +355,23 @@ int Rf_initialize_R(int ac, char **av)
                 Rp->R_Interactive = FALSE;
                 if (strcmp(*av, "-"))
                 {
-                    ifp = R_fopen(*av, "r");
+                    /* Undo the escaping done in the front end */
+                    char path[PATH_MAX], *p = path, *q;
+                    for (q = *av; *q; q++)
+                    {
+                        if (*q == '~' && *(q + 1) == '+' && *(q + 2) == '~')
+                        {
+                            q += 2;
+                            *p++ = ' ';
+                        }
+                        else
+                            *p++ = *q;
+                    }
+                    *p = '\0';
+                    ifp = R_fopen(path, "r");
                     if (!ifp)
                     {
-                        snprintf(msg, 1024, _("cannot open file '%s': %s"), *av, strerror(errno));
+                        snprintf(msg, 1024, _("cannot open file '%s': %s"), path, strerror(errno));
                         R_Suicide(msg);
                     }
                 }
@@ -368,10 +381,23 @@ int Rf_initialize_R(int ac, char **av)
                 Rp->R_Interactive = FALSE;
                 if (strcmp((*av) + 7, "-"))
                 {
-                    ifp = R_fopen((*av) + 7, "r");
+                    /* Undo the escaping done in the front end */
+                    char path[PATH_MAX], *p = path, *q;
+                    for (q = (*av) + 7; *q; q++)
+                    {
+                        if (*q == '~' && *(q + 1) == '+' && *(q + 2) == '~')
+                        {
+                            q += 2;
+                            *p++ = ' ';
+                        }
+                        else
+                            *p++ = *q;
+                    }
+                    *p = '\0';
+                    ifp = R_fopen(path, "r");
                     if (!ifp)
                     {
-                        snprintf(msg, 1024, _("cannot open file '%s': %s"), (*av) + 7, strerror(errno));
+                        snprintf(msg, 1024, _("cannot open file '%s': %s"), path, strerror(errno));
                         R_Suicide(msg);
                     }
                 }
