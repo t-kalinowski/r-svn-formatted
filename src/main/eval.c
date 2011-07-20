4b89030eca22ad3e50bfab1681cfc2a0a5394b5b
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-07-20 13:14:19 +0000

    typo in name
    git-svn-id: https://svn.r-project.org/R/trunk@56445 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -5408,7 +5408,7 @@ SEXP attribute_hidden do_bcclose(SEXP call, SEXP op, SEXP args, SEXP rho)
     CheckFormals(forms);
 
     if (!isByteCode(body))
-        errorcall(call, _("invalid environment"));
+        errorcall(call, _("invalid body"));
 
     if (isNull(env))
     {
