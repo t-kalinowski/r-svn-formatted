dccb6a82065036173d181119c70966b957fa224a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-09-04 10:43:28 +0000

    update
    git-svn-id: https://svn.r-project.org/R/trunk@56925 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -4182,7 +4182,7 @@ static void PS_writeRaster(unsigned int *raster, int w, int h, double x, double 
        (b) add a mask with ImageType 3: see PLRM 3rd ed section 4.10.6.
 
        (c) interpolation (done, but at least ghostscript seems to
-       ignore the request (and Mac preview always interpolates.)
+       ignore the request, and Mac preview always interpolates.)
 
        (d) sRGB colorspace (done)
     */
