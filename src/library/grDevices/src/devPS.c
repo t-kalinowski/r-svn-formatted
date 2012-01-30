108cfcff03e4f1b46d4fb856cd28661ea54e0698
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-01-30 19:09:55 +0000

    suppress zero-radii circles
    git-svn-id: https://svn.r-project.org/R/trunk@58237 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -7703,7 +7703,8 @@ static void PDF_Circle(double x, double y, double r, const pGEcontext gc, pDevDe
     PDF_checkOffline();
 
     if (r <= 0.0)
-        return; /* since PR#14797 use 0-sized pch=0 */
+        return; /* since PR#14797 use 0-sized pch=1, but now
+       GECircle omits such circles */
 
     code = 2 * (R_VIS(gc->fill)) + (R_VIS(gc->col));
     if (code)
