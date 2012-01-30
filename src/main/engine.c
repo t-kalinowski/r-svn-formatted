fcc03c7c456daddd6b8c52bc99843688e18af38d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-01-30 18:54:20 +0000

    update comments
    git-svn-id: https://svn.r-project.org/R/trunk@58235 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/engine.c
+++ src/main/engine.c
@@ -2201,8 +2201,8 @@ void GESymbol(double x, double y, int pch, double size, const pGEcontext gc, pGE
             GERect(x - xc, y - yc, x + xc, y + yc, gc, dd);
             break;
 
-        case 1: /* S octahedron ( circle) */
-            xc = RADIUS * size;
+        case 1:                 /* S octahedron ( circle) */
+            xc = RADIUS * size; /* NB: could be zero */
             gc->fill = R_TRANWHITE;
             GECircle(x, y, xc, gc, dd);
             break;
