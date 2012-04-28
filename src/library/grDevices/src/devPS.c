148d303ce000bcebfb5a31b9a0d542a91ac5fcd9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-04-28 05:55:22 +0000

    check result of fwrite
    git-svn-id: https://svn.r-project.org/R/trunk@59235 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -7322,7 +7322,8 @@ static void PDF_endfile(PDFDesc *pd)
         pd->pdffp = R_fopen(pd->filename, "rb");
         while ((nc = fread(buf, 1, APPENDBUFSIZE, pd->pdffp)))
         {
-            fwrite(buf, 1, nc, pd->pipefp);
+            if (nc != fwrite(buf, 1, nc, pd->pipefp))
+                error("write error");
             if (nc < APPENDBUFSIZE)
                 break;
         }
