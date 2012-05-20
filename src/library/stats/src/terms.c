c8b995bd92eb66b0801b1a02bdc185feb567891c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-05-20 18:01:34 +0000

    visibility issue
    git-svn-id: https://svn.r-project.org/R/trunk@59372 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/terms.c
+++ src/library/stats/src/terms.c
@@ -1317,7 +1317,7 @@ SEXP updateform(SEXP args)
 
     SET_ATTRIB(_new, R_NilValue);
     SET_OBJECT(_new, 0);
-    static SEXP DotEnvSymbol = nstall(".Environment");
+    SEXP DotEnvSymbol = install(".Environment");
     setAttrib(_new, DotEnvSymbol, getAttrib(old, DotEnvSymbol));
 
     return _new;
