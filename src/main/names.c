3dec52c98ac08ae2ff4c8ce6d65fd2f95f21def7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-05-27 15:10:07 +0000

    improve messages
    git-svn-id: https://svn.r-project.org/R/trunk@59456 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/names.c
+++ src/main/names.c
@@ -1157,9 +1157,9 @@ SEXP attribute_hidden do_internal(SEXP call, SEXP op, SEXP args, SEXP env)
         errorcall(call, _("invalid .Internal() argument"));
     fun = CAR(s);
     if (!isSymbol(fun))
-        errorcall(call, _("invalid internal function"));
+        errorcall(call, _("invalid .Internal() argument"));
     if (INTERNAL(fun) == R_NilValue)
-        errorcall(call, _("no internal function \"%s\""), CHAR(PRINTNAME(fun)));
+        errorcall(call, _("there is no .Internal function '%s'"), CHAR(PRINTNAME(fun)));
     args = CDR(s);
     if (TYPEOF(INTERNAL(fun)) == BUILTINSXP)
         args = evalList(args, env, call, 0);
