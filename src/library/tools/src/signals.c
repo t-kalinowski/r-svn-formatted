a43289f59ede68a8d1d9abfe5eebc81fd71ae2c2
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-05-27 09:04:41 +0000

    fix some issues seen by examining no-segfaults.Rout
    git-svn-id: https://svn.r-project.org/R/trunk@59451 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/src/signals.c
+++ src/library/tools/src/signals.c
@@ -83,7 +83,10 @@ SEXP ps_priority(SEXP spid, SEXP svalue)
     for (int i = 0; i < ns; i++)
     {
         if (pid[i] <= 0)
+        {
+            res[i] = NA_INTEGER;
             continue;
+        }
         if (pid[i] != NA_INTEGER)
         {
             /* return value -1 is both an error value
