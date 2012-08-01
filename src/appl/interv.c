7cb9a0b5b4da1801090535a72cc97c6a307651d4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-01 12:54:39 +0000

    use .Call in findInterval()
    git-svn-id: https://svn.r-project.org/R/trunk@60071 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/interv.c
+++ src/appl/interv.c
@@ -35,7 +35,9 @@ int F77_SUB(interv)(double *xt, int *n, double *x, Rboolean *rightmost_closed, R
 }
 
 /* This one to be called from R {via .C(..)} :
- * FIXME: Replace by a .Call()able version!
+   FIXME: Replace by a .Call()able version!
+
+   Done for R 2.15.2, no longer used in R, but used in IBDsim and timeSeries
  */
 void find_interv_vec(double *xt, int *n, double *x, int *nx, int *rightmost_closed, int *all_inside, int *indx)
 {
