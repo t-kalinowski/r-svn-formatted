a3216e81e494d49c4c788063fa1572a762d6c2f8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-01 15:34:04 +0000

    more migration to .Call
    git-svn-id: https://svn.r-project.org/R/trunk@60074 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/init.c
+++ src/library/stats/src/init.c
@@ -207,6 +207,8 @@ static const R_CallMethodDef CallEntries[] = {{"R_cutree", (DL_FUNC)&R_cutree, 2
                                               {"mvfft", (DL_FUNC)&mvfft, 2},
                                               {"nextn", (DL_FUNC)&nextn, 2},
                                               {"r2dtable", (DL_FUNC)&r2dtable, 3},
+                                              CALLDEF(filter3, 4),
+                                              CALLDEF(filter4, 3),
                                               {NULL, NULL, 0}};
 
 static const R_FortranMethodDef FortEntries[] = {FDEF(lowesw),
