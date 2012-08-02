c225a86675071fe8139b0b0a59db581d2ba2df7d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-02 05:49:19 +0000

    some cleaning up
    git-svn-id: https://svn.r-project.org/R/trunk@60078 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/strsignif.c
+++ src/appl/strsignif.c
@@ -101,7 +101,7 @@ int trio_sprintf(char *buffer, const char *format, ...);
    casting in the code itself.  However, it does mean that we cannot
    use the argument type matching
  */
-attribute_hidden void str_signif(char *x, int *n, const char **type, int *width, int *digits, const char **format,
+attribute_hidden void str_signif(void *x, int *n, const char **type, int *width, int *digits, const char **format,
                                  const char **flag, char **result)
 {
     int wid = *width;
