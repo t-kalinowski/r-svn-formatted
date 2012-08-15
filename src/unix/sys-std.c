8177972ff0e02b1dfd95b61a49cc70700520bc23
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-15 10:37:55 +0000

    more details on available.packages, tweak to biplot.default
    git-svn-id: https://svn.r-project.org/R/trunk@60273 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/sys-std.c
+++ src/unix/sys-std.c
@@ -1021,6 +1021,9 @@ void attribute_hidden Rstd_Busy(int which)
    If ask = SA_SUICIDE, no save, no .Last, possibly other things.
  */
 
+/* in platform.c */
+void R_CleanTempDir2(void);
+
 void R_CleanTempDir(void)
 {
     char buf[1024];
@@ -1031,11 +1034,15 @@ void R_CleanTempDir(void)
         /* On Solaris the working directory must be outside this one */
         chdir(R_HomeDir());
 #endif
-        snprintf(buf, 1024, "rm -rf %s", Sys_TempDir);
-        buf[1023] = '\0';
         char *p = getenv("R_OSX_VALGRIND");
         if (!p)
+        {
+            snprintf(buf, 1024, "rm -rf %s", Sys_TempDir);
+            buf[1023] = '\0';
             R_system(buf);
+        }
+        else
+            R_CleanTempDir2();
     }
 }
 
