1ce8f19def5f5feb63b8e371676cf4de89ea6907
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-30 11:50:51 +0000

    more paranoid version
    git-svn-id: https://svn.r-project.org/R/trunk@60498 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/unique.c
+++ src/main/unique.c
@@ -1812,12 +1812,13 @@ SEXP attribute_hidden do_rowsum(SEXP call, SEXP op, SEXP args, SEXP env)
         ans = Rrowsum_matrix(CAR(args), CADR(args), CADDR(args), CADDDR(args), CAD4R(args));
         PROTECT(ans);
         SEXP nm = PROTECT(allocVector(VECSXP, 2));
-        SEXP ugroup = coerceVector(CADDDR(args), STRSXP);
+        SEXP ugroup = PROTECT(coerceVector(CADDDR(args), STRSXP));
         SET_VECTOR_ELT(nm, 0, ugroup);
         SEXP nm2 = getAttrib(CAR(args), R_DimNamesSymbol);
-        SET_VECTOR_ELT(nm, 1, VECTOR_ELT(nm2, 1));
+        if (length(nm2) >= 2)
+            SET_VECTOR_ELT(nm, 1, VECTOR_ELT(nm2, 1));
         setAttrib(ans, R_DimNamesSymbol, nm);
-        UNPROTECT(2);
+        UNPROTECT(3);
     }
     return ans;
 }
