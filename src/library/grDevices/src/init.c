a4115081c6f64802042cadd26d4d48fa8e5e897b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-09-01 17:11:57 +0000

    migrate grDevices::chull to .Call
    git-svn-id: https://svn.r-project.org/R/trunk@60532 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/init.c
+++ src/library/grDevices/src/init.c
@@ -53,22 +53,16 @@ static SEXP cairoProps(SEXP in)
 }
 #endif
 
-static R_NativePrimitiveArgType R_chull_t[] = {INTSXP, REALSXP, INTSXP, INTSXP, INTSXP, INTSXP, INTSXP, INTSXP, INTSXP};
-
 #ifndef WIN32
 void *getQuartzAPI();
 #endif
 
-#define CDEF(name)                                                                                                     \
-    {                                                                                                                  \
-#name, (DL_FUNC)&name, sizeof(name##_t) / sizeof(name##_t[0]), name##_t                                        \
-    }
-
-static R_CMethodDef CEntries[] = {CDEF(R_chull),
+static R_CMethodDef CEntries[] = {
 #ifndef WIN32
-                                  {"getQuartzAPI", (DL_FUNC)getQuartzAPI, 0},
+    // What is this for?  Not use in R nor R.app sources
+    {"getQuartzAPI", (DL_FUNC)getQuartzAPI, 0},
 #endif
-                                  {NULL, NULL, 0}};
+    {NULL, NULL, 0}};
 
 #define CALLDEF(name, n)                                                                                               \
     {                                                                                                                  \
@@ -81,6 +75,7 @@ static const R_CallMethodDef CallEntries[] = {
     CALLDEF(CIDFontInUse, 2),
     CALLDEF(R_CreateAtVector, 4),
     CALLDEF(R_GAxisPars, 3),
+    CALLDEF(C_chull, 1),
 #ifndef WIN32
     CALLDEF(makeQuartzDefault, 0),
     CALLDEF(cairoProps, 1),
