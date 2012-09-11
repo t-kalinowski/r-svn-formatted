1435ba382f8815f2525a11cb7bdf91eb1b092bd0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-09-11 16:01:36 +0000

    more tweaking
    git-svn-id: https://svn.r-project.org/R/trunk@60677 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/src/init.c
+++ src/library/utils/src/init.c
@@ -33,6 +33,7 @@
     }
 
 static const R_CallMethodDef CallEntries[] = {CALLDEF(crc64, 1),
+                                              CALLDEF(flushconsole, 0),
                                               CALLDEF(menu, 1),
                                               CALLDEF(nsl, 1),
                                               CALLDEF(objectSize, 1),
@@ -53,14 +54,14 @@ static const R_CallMethodDef CallEntries[] = {CALLDEF(crc64, 1),
     }
 
 static const R_ExternalMethodDef ExtEntries[] = {
-    EXTDEF(download, 5),     EXTDEF(unzip, 7),         EXTDEF(Rprof, 4),       EXTDEF(Rprofmem, 3),
+    EXTDEF(download, 5),    EXTDEF(unzip, 7),         EXTDEF(Rprof, 4),       EXTDEF(Rprofmem, 3),
 
-    EXTDEF(countfields, 6),  EXTDEF(readtablehead, 6), EXTDEF(typeconvert, 4), EXTDEF(writetable, 11),
+    EXTDEF(countfields, 6), EXTDEF(readtablehead, 6), EXTDEF(typeconvert, 4), EXTDEF(writetable, 11),
 
-    EXTDEF(addhistory, 1),   EXTDEF(loadhistory, 1),   EXTDEF(savehistory, 1),
+    EXTDEF(addhistory, 1),  EXTDEF(loadhistory, 1),   EXTDEF(savehistory, 1),
 
-    EXTDEF(dataentry, 2),    EXTDEF(dataviewer, 2),    EXTDEF(edit, 4),        EXTDEF(fileedit, 3),
-    EXTDEF(flushconsole, 0), EXTDEF(selectlist, 4),
+    EXTDEF(dataentry, 2),   EXTDEF(dataviewer, 2),    EXTDEF(edit, 4),        EXTDEF(fileedit, 3),
+    EXTDEF(selectlist, 4),
 
     {NULL, NULL, 0}};
 
