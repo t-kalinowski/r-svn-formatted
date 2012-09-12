5b6cbab6aab675ad6a21617d4c8c31f12bfb7e83
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-09-12 16:11:19 +0000

    Migrate most of the Windows .Internals to 'utils'
    git-svn-id: https://svn.r-project.org/R/trunk@60686 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/src/init.c
+++ src/library/utils/src/init.c
@@ -53,17 +53,41 @@ static const R_CallMethodDef CallEntries[] = {CALLDEF(crc64, 1),
 #name, (DL_FUNC)&name, n                                                                                       \
     }
 
-static const R_ExternalMethodDef ExtEntries[] = {
-    EXTDEF(download, 5),    EXTDEF(unzip, 7),         EXTDEF(Rprof, 4),       EXTDEF(Rprofmem, 3),
+static const R_ExternalMethodDef ExtEntries[] = {EXTDEF(download, 5),
+                                                 EXTDEF(unzip, 7),
+                                                 EXTDEF(Rprof, 4),
+                                                 EXTDEF(Rprofmem, 3),
 
-    EXTDEF(countfields, 6), EXTDEF(readtablehead, 6), EXTDEF(typeconvert, 4), EXTDEF(writetable, 11),
+                                                 EXTDEF(countfields, 6),
+                                                 EXTDEF(readtablehead, 6),
+                                                 EXTDEF(typeconvert, 4),
+                                                 EXTDEF(writetable, 11),
 
-    EXTDEF(addhistory, 1),  EXTDEF(loadhistory, 1),   EXTDEF(savehistory, 1),
+                                                 EXTDEF(addhistory, 1),
+                                                 EXTDEF(loadhistory, 1),
+                                                 EXTDEF(savehistory, 1),
 
-    EXTDEF(dataentry, 2),   EXTDEF(dataviewer, 2),    EXTDEF(edit, 4),        EXTDEF(fileedit, 3),
-    EXTDEF(selectlist, 4),
+                                                 EXTDEF(dataentry, 2),
+                                                 EXTDEF(dataviewer, 2),
+                                                 EXTDEF(edit, 4),
+                                                 EXTDEF(fileedit, 3),
+                                                 EXTDEF(selectlist, 4),
 
-    {NULL, NULL, 0}};
+#ifdef Win32
+                                                 EXTDEF(winProgressBar, 6),
+                                                 EXTDEF(closeWinProgressBar, 1),
+                                                 EXTDEF(setWinProgressBar, 4),
+                                                 EXTDEF(winDialog, 2),
+                                                 EXTDEF(winDialogString, 2),
+                                                 EXTDEF(winMenuNames, 0),
+                                                 EXTDEF(winMenuItems, 1),
+                                                 EXTDEF(winMenuAdd, 3),
+                                                 EXTDEF(winMenuDel, 2),
+
+                                                 EXTDEF(readRegistry, 4),
+#endif
+
+                                                 {NULL, NULL, 0}};
 
 void
 #ifdef HAVE_VISIBILITY_ATTRIBUTE
