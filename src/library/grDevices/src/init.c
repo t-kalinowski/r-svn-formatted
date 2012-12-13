6e7879938575dd8db2cb300a165ff1fc1d23aa56
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-12-13 12:18:11 +0000

    migrate col2rgb
    git-svn-id: https://svn.r-project.org/R/trunk@61304 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/init.c
+++ src/library/grDevices/src/init.c
@@ -69,27 +69,26 @@ static R_CMethodDef CEntries[] = {
 #name, (DL_FUNC)&name, n                                                                                       \
     }
 
-static const R_CallMethodDef CallEntries[] = {
-    // NB: each *also* needs an entry in useDynLib() in ../NAMESPACE !
-    CALLDEF(Type1FontInUse, 2),
-    CALLDEF(CIDFontInUse, 2),
-    CALLDEF(R_CreateAtVector, 4),
-    CALLDEF(R_GAxisPars, 3),
-    CALLDEF(chull, 1),
-    CALLDEF(gray, 2),
-    CALLDEF(RGB2hsv, 1),
-    CALLDEF(rgb, 6),
-    CALLDEF(hsv, 4),
-    CALLDEF(hcl, 5),
+static const R_CallMethodDef CallEntries[] = {CALLDEF(Type1FontInUse, 2),
+                                              CALLDEF(CIDFontInUse, 2),
+                                              CALLDEF(R_CreateAtVector, 4),
+                                              CALLDEF(R_GAxisPars, 3),
+                                              CALLDEF(chull, 1),
+                                              CALLDEF(gray, 2),
+                                              CALLDEF(RGB2hsv, 1),
+                                              CALLDEF(rgb, 6),
+                                              CALLDEF(hsv, 4),
+                                              CALLDEF(hcl, 5),
+                                              CALLDEF(col2rgb, 1),
 
 #ifndef WIN32
-    CALLDEF(makeQuartzDefault, 0),
-    CALLDEF(cairoProps, 1),
+                                              CALLDEF(makeQuartzDefault, 0),
+                                              CALLDEF(cairoProps, 1),
 #else
-    CALLDEF(bringToTop, 2),
-    CALLDEF(msgWindow, 2),
+                                              CALLDEF(bringToTop, 2),
+                                              CALLDEF(msgWindow, 2),
 #endif
-    {NULL, NULL, 0}};
+                                              {NULL, NULL, 0}};
 
 #define EXTDEF(name, n)                                                                                                \
     {                                                                                                                  \
@@ -121,7 +120,6 @@ static const R_ExternalMethodDef ExtEntries[] = {EXTDEF(PicTeX, 6),
                                                  EXTDEF(getGraphicsEventEnv, 1),
                                                  EXTDEF(setGraphicsEventEnv, 2),
                                                  EXTDEF(colors, 0),
-                                                 EXTDEF(col2rgb, 1),
                                                  EXTDEF(palette, 1),
                                                  EXTDEF(devAskNewPage, 1),
 
