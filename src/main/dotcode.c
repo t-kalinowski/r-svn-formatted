f077f2ffb93b757fa6340b910e403eac9fcb5f33
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-19 20:22:48 +0000

    check no ofargs for .External
    git-svn-id: https://svn.r-project.org/R/trunk@61692 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/dotcode.c
+++ src/main/dotcode.c
@@ -543,6 +543,14 @@ SEXP attribute_hidden do_External(SEXP call, SEXP op, SEXP args, SEXP env)
     check1arg2(args, call, ".NAME");
     args = resolveNativeRoutine(args, &ofun, &symbol, buf, NULL, NULL, NULL, call, env);
 
+    if (symbol.symbol.external && symbol.symbol.external->numArgs > -1)
+    {
+        int nargs = length(args) - 1;
+        if (symbol.symbol.external->numArgs != nargs)
+            warningcall(call, _("Incorrect number of arguments (%d), expecting %d for '%s'"), nargs,
+                        symbol.symbol.external->numArgs, buf);
+    }
+
     if (PRIMVAL(op) == 1)
     {
         R_ExternalRoutine2 fun = (R_ExternalRoutine2)ofun;
