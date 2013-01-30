bf7d1b1258e76898aa7eeed20b580a451c58d06c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-30 15:09:20 +0000

    more hiding
    git-svn-id: https://svn.r-project.org/R/trunk@61788 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/printvector.c
+++ src/main/printvector.c
@@ -337,7 +337,8 @@ static void printNamedLogicalVector(int *x, int n, SEXP *names)
                                             PRINT_N_VECTOR(formatRaw(x, n, &w),
                                                            Rprintf("%s%*s", EncodeRaw(x[k]), R_print.gap, ""))
 
-                                                void printNamedVector(SEXP x, SEXP names, int quote, const char *title)
+                                                attribute_hidden
+    void printNamedVector(SEXP x, SEXP names, int quote, const char *title)
 {
     int n;
 
