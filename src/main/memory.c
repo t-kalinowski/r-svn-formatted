016a02bd7c6de87a3dd84899245487c17e44c1c8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-30 17:26:36 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@61790 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -607,8 +607,8 @@ static R_size_t R_NodesInUse = 0;
    part of a hash chain.  Theoretically, for CHARSXPs the ATTRIB field
    should always be either R_NilValue or a CHARSXP. */
 #ifdef PROTECTCHECK
-#efine HAS_GENUINE_ATTRIB(x)(TYPEOF(x) != FREESXP && ATTRIB(x) != R_NilValue &&                                        \
-                             (TYPEOF(x) != CHARSXP || TYPEOF(ATTRIB(x)) != CHARSXP))
+#define HAS_GENUINE_ATTRIB(x)                                                                                          \
+    (TYPEOF(x) != FREESXP && ATTRIB(x) != R_NilValue && (TYPEOF(x) != CHARSXP || TYPEOF(ATTRIB(x)) != CHARSXP))
 #else
 #define HAS_GENUINE_ATTRIB(x) (ATTRIB(x) != R_NilValue && (TYPEOF(x) != CHARSXP || TYPEOF(ATTRIB(x)) != CHARSXP))
 #endif
