0c050cffbffffd92af330d5870a702ab14071aef
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-04-14 19:26:28 +0000

    simpler version of r62565
    git-svn-id: https://svn.r-project.org/R/trunk@62577 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/platform.c
+++ src/main/platform.c
@@ -2544,30 +2544,35 @@ SEXP attribute_hidden do_filecopy(SEXP call, SEXP op, SEXP args, SEXP rho)
             if (STRING_ELT(fn, i) != NA_STRING)
             {
                 wcsncpy(from, filenameToWchar(STRING_ELT(fn, i), TRUE), PATH_MAX);
-                /* If there was a trailing sep, this is a mistake */
-                p = from + (wcslen(from) - 1);
-                if (*p == L'\\')
-                    *p = L'\0';
-                p = wcsrchr(from, L'\\');
-                if (p)
+                if (wcslen(from))
                 {
-                    wcsncpy(name, p + 1, PATH_MAX);
-                    *(p + 1) = L'\0';
-                }
-                else
-                {
-                    if (wcslen(from) > 2 && from[1] == L':')
+                    /* If there was a trailing sep, this is a mistake */
+                    p = from + (wcslen(from) - 1);
+                    if (*p == L'\\')
+                        *p = L'\0';
+                    p = wcsrchr(from, L'\\');
+                    if (p)
                     {
-                        wcsncpy(name, from + 2, PATH_MAX);
-                        from[2] = L'\0';
+                        wcsncpy(name, p + 1, PATH_MAX);
+                        *(p + 1) = L'\0';
                     }
                     else
                     {
-                        wcsncpy(name, from, PATH_MAX);
-                        wcsncpy(from, L".\\", PATH_MAX);
+                        if (wcslen(from) > 2 && from[1] == L':')
+                        {
+                            wcsncpy(name, from + 2, PATH_MAX);
+                            from[2] = L'\0';
+                        }
+                        else
+                        {
+                            wcsncpy(name, from, PATH_MAX);
+                            wcsncpy(from, L".\\", PATH_MAX);
+                        }
                     }
+                    nfail = do_copy(from, name, dir, over, recursive, perms, 1);
                 }
-                nfail = do_copy(from, name, dir, over, recursive, perms, 1);
+                else
+                    nfail = 1;
             }
             else
                 nfail = 1;
