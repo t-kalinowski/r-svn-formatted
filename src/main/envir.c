0442652de1b7acc641d76d6451e8a2daf005e07f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-08-20 05:51:56 +0000

    ensure that the methods-package C code looks for its functions in its namespace and not on the search path
    git-svn-id: https://svn.r-project.org/R/trunk@63624 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/envir.c
+++ src/main/envir.c
@@ -654,7 +654,8 @@ void attribute_hidden InitBaseEnv()
 void attribute_hidden InitGlobalEnv()
 {
     R_GlobalEnv = R_NewHashedEnv(R_BaseEnv, ScalarInteger(0));
-#ifdef NEW_CODE /* Not used */
+    R_MethodsNamespace = R_GlobalEnv; // so it is initialized.
+#ifdef NEW_CODE                       /* Not used */
     HASHTAB(R_GlobalEnv) = R_NewHashTable(100);
 #endif
 #ifdef USE_GLOBAL_CACHE
