7728e8f459c3174700358551ecdfb23b26c443ab
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-08-22 22:55:19 +0000

    Fixed NA handling in scalar relop code.
    git-svn-id: https://svn.r-project.org/R/trunk@63659 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/relop.c
+++ src/main/relop.c
@@ -79,18 +79,18 @@ SEXP attribute_hidden do_relop(SEXP call, SEXP op, SEXP args, SEXP env)
     else if (IS_SCALAR(arg1, INTSXP))
     {
         int ix = INTEGER(arg1)[0];
-        if (ix == NA_INTEGER)
-            return ScalarLogical(NA_LOGICAL);
         if (IS_SCALAR(arg2, INTSXP))
         {
             int iy = INTEGER(arg2)[0];
-            if (iy == NA_INTEGER)
+            if (ix == NA_INTEGER || iy == NA_INTEGER)
                 return ScalarLogical(NA_LOGICAL);
             DO_SCALAR_RELOP(oper, ix, iy);
         }
         else if (IS_SCALAR(arg2, REALSXP))
         {
             double dy = REAL(arg2)[0];
+            if (ix == NA_INTEGER || ISNAN(dy))
+                return ScalarLogical(NA_LOGICAL);
             DO_SCALAR_RELOP(oper, ix, dy);
         }
     }
@@ -100,13 +100,15 @@ SEXP attribute_hidden do_relop(SEXP call, SEXP op, SEXP args, SEXP env)
         if (IS_SCALAR(arg2, INTSXP))
         {
             int iy = INTEGER(arg2)[0];
-            if (iy == NA_INTEGER)
+            if (ISNAN(dx) || iy == NA_INTEGER)
                 return ScalarLogical(NA_LOGICAL);
             DO_SCALAR_RELOP(oper, dx, iy);
         }
         else if (IS_SCALAR(arg2, REALSXP))
         {
             double dy = REAL(arg2)[0];
+            if (ISNAN(dx) || ISNAN(dy))
+                return ScalarLogical(NA_LOGICAL);
             DO_SCALAR_RELOP(oper, dx, dy);
         }
     }
