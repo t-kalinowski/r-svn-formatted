ccf635e92fc4ec85706eb091736c584b2348a134
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-09-07 06:01:02 +0000

    add comment
    git-svn-id: https://svn.r-project.org/R/trunk@63857 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/cairo/cairoFns.c
+++ src/library/grDevices/src/cairo/cairoFns.c
@@ -535,6 +535,8 @@ static void PG_text_extents(cairo_t *cc, PangoLayout *layout, gint *lbearing, gi
 {
     PangoRectangle rect, lrect;
 
+    // This could be pango_layout_get_line_readonly since 1.16
+    // Something like #if PANGO_VERSION_CHECK(1,16,0)
     pango_layout_line_get_pixel_extents(pango_layout_get_line(layout, 0), &rect, &lrect);
 
     if (width)
