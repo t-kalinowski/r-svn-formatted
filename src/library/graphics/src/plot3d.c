fd6e3f50e46bba5827551a0808cbdb8ab4240875
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-11-20 13:16:48 +0000

    take care of shade = NaN
    git-svn-id: https://svn.r-project.org/R/trunk@64273 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/src/plot3d.c
+++ src/library/graphics/src/plot3d.c
@@ -637,12 +637,18 @@ static void DrawFacets(double *z, double *x, double *y, int nx, int ny, int *ind
             newcol = col[icol];
             if (DoLighting)
             {
-                r = (int)(shade * R_RED(newcol));
-                g = (int)(shade * R_GREEN(newcol));
-                b = (int)(shade * R_BLUE(newcol));
-                newcol = R_RGB(r, g, b);
+                // shade can degenerate to NaN
+                if (R_FINITE(shade))
+                {
+                    r = (int)(shade * R_RED(newcol));
+                    g = (int)(shade * R_GREEN(newcol));
+                    b = (int)(shade * R_BLUE(newcol));
+                    newcol = R_RGB(r, g, b);
+                    GPolygon(nv, xx, yy, USER, newcol, border, dd);
+                }
             }
-            GPolygon(nv, xx, yy, USER, newcol, border, dd);
+            else
+                GPolygon(nv, xx, yy, USER, newcol, border, dd);
         }
     }
 }
