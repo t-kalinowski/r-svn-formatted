37bbd3ec1151932f16981beee03cf7d2c4282d02
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-01-05 14:32:00 +0000

    more use of R_D_forceint
    git-svn-id: https://svn.r-project.org/R/trunk@64655 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/fprec.c
+++ src/nmath/fprec.c
@@ -1,7 +1,7 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-12 The R Core Team
+ *  Copyright (C) 2000-14 The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -84,7 +84,7 @@ double fprec(double x, double digits)
     }
     if (x == 0)
         return x;
-    dig = (int)floor(digits + 0.5);
+    dig = (int)round(digits);
     if (dig > MAX_DIGITS)
     {
         return x;
