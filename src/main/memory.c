86ba7004df18a8b2e7d649273380a68bf3e41d73
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-01-31 07:21:22 +0000

    add bounds check to SET_VECTOR_ELT/SET_STRING_ELT
    git-svn-id: https://svn.r-project.org/R/trunk@64902 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -3472,6 +3472,8 @@ void(SET_STRING_ELT)(SEXP x, R_xlen_t i, SEXP v)
     if (TYPEOF(v) != CHARSXP)
         error("Value of SET_STRING_ELT() must be a 'CHARSXP' not a '%s'", type2char(TYPEOF(v)));
     CHECK_OLD_TO_NEW(x, v);
+    if (i < 0 || i >= XLENGTH(x))
+        error(_("attempt to set index %lu/%lu in SET_STRING_ELT"), i, XLENGTH(x));
     STRING_ELT(x, i) = v;
 }
 
@@ -3483,6 +3485,8 @@ SEXP(SET_VECTOR_ELT)(SEXP x, R_xlen_t i, SEXP v)
         error("%s() can only be applied to a '%s', not a '%s'", "SET_VECTOR_ELT", "list", type2char(TYPEOF(x)));
     }
     CHECK_OLD_TO_NEW(x, v);
+    if (i < 0 || i >= XLENGTH(x))
+        error(_("attempt to set index %lu/%lu in SET_VECTOR_ELT"), i, XLENGTH(x));
     return VECTOR_ELT(x, i) = v;
 }
 
