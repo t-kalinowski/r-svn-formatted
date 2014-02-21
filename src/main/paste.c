190f87d51df92a047a94baf72142eef42da42a6c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-02-21 11:51:43 +0000

    On Windows, remove trailing separators in file.path()
    git-svn-id: https://svn.r-project.org/R/trunk@65057 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/paste.c
+++ src/main/paste.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1997--2012  The R Core Team
+ *  Copyright (C) 1997--2014  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -412,6 +412,14 @@ SEXP attribute_hidden do_filepath(SEXP call, SEXP op, SEXP args, SEXP env)
                 buf += sepw;
             }
         }
+#ifdef Win32
+        // Trailing spaces are never valid for file paths
+        if (streql(csep, "/") || streql(csep, "\\"))
+        {
+            if (*(--buf) == csep[0])
+                *buf = '\0';
+        }
+#endif
         SET_STRING_ELT(ans, i, mkChar(cbuf));
     }
     R_FreeStringBufferL(&cbuff);
