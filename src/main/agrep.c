003e7deab48c58ba4104ec260d165a18f7e6159b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-03-02 12:21:36 +0000

    add agrepl
    git-svn-id: https://svn.r-project.org/R/trunk@65094 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/agrep.c
+++ src/main/agrep.c
@@ -294,6 +294,12 @@ SEXP attribute_hidden do_agrep(SEXP call, SEXP op, SEXP args, SEXP env)
     }
     tre_regfree(&reg);
 
+    if (PRIMVAL(op))
+    { /* agrepl case */
+        UNPROTECT(1);
+        return ind;
+    }
+
     if (opt_value)
     {
         PROTECT(ans = allocVector(STRSXP, nmatches));
