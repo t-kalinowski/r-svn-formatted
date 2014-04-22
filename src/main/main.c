12cc91fdaf918a778aea3db85803b569aa206369
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-04-22 09:42:37 +0000

    Fix for PR$15770, step into bug.
    git-svn-id: https://svn.r-project.org/R/trunk@65444 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -257,6 +257,10 @@ int Rf_ReplIteration(SEXP rho, int savestack, int browselevel, R_ReplState *stat
                 R_IoBufferWriteReset(&R_ConsoleIob);
                 return 0;
             }
+            /* PR#15770 We don't want to step into expressions entered at the debug prompt.
+               The 'S' will be changed back to 's' after the next eval. */
+            if (R_BrowserLastCommand == 's')
+                R_BrowserLastCommand = 'S';
         }
         R_Visible = FALSE;
         R_EvalDepth = 0;
@@ -273,6 +277,8 @@ int Rf_ReplIteration(SEXP rho, int savestack, int browselevel, R_ReplState *stat
         Rf_callToplevelHandlers(thisExpr, value, TRUE, wasDisplayed);
         R_CurrentExpr = value; /* Necessary? Doubt it. */
         UNPROTECT(1);
+        if (R_BrowserLastCommand == 'S')
+            R_BrowserLastCommand = 's';
         R_IoBufferWriteReset(&R_ConsoleIob);
         state->prompt_type = 1;
         return (1);
