6f87eceec2fac78629fda40f7a7c856d82eab17d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-05-05 12:37:06 +0000

    PR#15774: integer overflow accident (e.g. with clang)
    git-svn-id: https://svn.r-project.org/R/trunk@65526 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -369,22 +369,22 @@ static SEXP lcall;
         }                                                                                                              \
     } while (0)
 
+#define R_INT_MAX INT_MAX
+#define R_INT_MIN -INT_MAX
+// .. relying on fact that NA_INTEGER is outside of these
+
 static R_INLINE int R_integer_plus(int x, int y, Rboolean *pnaflag)
 {
     if (x == NA_INTEGER || y == NA_INTEGER)
         return NA_INTEGER;
-    else
+
+    if (((y > 0) && (x > (R_INT_MAX - y))) || ((y < 0) && (x < (R_INT_MIN - y))))
     {
-        int z = x + y;
-        if (GOODISUM(x, y, z) && z != NA_INTEGER)
-            return z;
-        else
-        {
-            if (pnaflag != NULL)
-                *pnaflag = TRUE;
-            return NA_INTEGER;
-        }
+        if (pnaflag != NULL)
+            *pnaflag = TRUE;
+        return NA_INTEGER;
     }
+    return x + y;
 }
 
 static R_INLINE int R_integer_minus(int x, int y, Rboolean *pnaflag)
