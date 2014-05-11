1dd7b80728fb43cda82ce01079e6e938aa95889d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-05-11 12:02:26 +0000

    abs(-2147483648) is undefined, so special-case
    git-svn-id: https://svn.r-project.org/R/trunk@65563 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -1437,7 +1437,10 @@ SEXP attribute_hidden do_abs(SEXP call, SEXP op, SEXP args, SEXP env)
         PROTECT(s);
         /* Note: relying on INTEGER(.) === LOGICAL(.) : */
         for (i = 0; i < n; i++)
-            INTEGER(s)[i] = abs(INTEGER(x)[i]);
+        {
+            int xi = INTEGER(x)[i];
+            INTEGER(s)[i] = (xi == NA_INTEGER) ? xi : abs(xi);
+        }
     }
     else if (TYPEOF(x) == REALSXP)
     {
