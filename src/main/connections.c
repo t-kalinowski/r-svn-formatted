3547149632bb3cdf2da6867d5d79734b5a945f85
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-05-20 15:56:13 +0000

    revert part of r65117 which was not confined to Windows
    git-svn-id: https://svn.r-project.org/R/trunk@65696 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -1035,10 +1035,8 @@ static size_t fifo_write(const void *ptr, size_t size, size_t nitems, Rconnectio
     Rfifoconn this = con->private;
 
     /* uses 'size_t' for len */
-    if ((size * sizeof(wchar_t) * nitems) > 50000)
-    {
+    if ((double)size * (double)nitems > SSIZE_MAX)
         error(_("too large a block specified"));
-    }
     return write(this->fd, ptr, size * nitems) / size;
 }
 
