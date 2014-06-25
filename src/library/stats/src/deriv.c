670ebc9f76f5c01148a175f7d2ea35e108307676
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-06-25 05:37:04 +0000

    Bugs found by Radford Neal:  "Fixed lack of protection bugs in the D function in deriv.c."
    git-svn-id: https://svn.r-project.org/R/trunk@66021 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/deriv.c
+++ src/library/stats/src/deriv.c
@@ -463,27 +463,28 @@ static SEXP D(SEXP expr, SEXP var)
         }
         else if (CAR(expr) == AsinSymbol)
         {
-            ans =
-                simplify(DivideSymbol, PP(D(CADR(expr), var)),
-                         PP_S(SqrtSymbol, PP_S(MinusSymbol, Constant(1.), PP_S(PowerSymbol, CADR(expr), Constant(2.))),
-                              R_MissingArg));
-            UNPROTECT(4);
+            ans = simplify(DivideSymbol, PP(D(CADR(expr), var)),
+                           PP_S(SqrtSymbol,
+                                PP_S(MinusSymbol, PP(Constant(1.)), PP_S(PowerSymbol, CADR(expr), PP(Constant(2.)))),
+                                R_MissingArg));
+            UNPROTECT(6);
         }
         else if (CAR(expr) == AcosSymbol)
         {
-            ans = simplify(
-                MinusSymbol,
-                PP_S(DivideSymbol, PP(D(CADR(expr), var)),
-                     PP_S(SqrtSymbol, PP_S(MinusSymbol, Constant(1.), PP_S(PowerSymbol, CADR(expr), Constant(2.))),
-                          R_MissingArg)),
-                R_MissingArg);
-            UNPROTECT(5);
+            ans =
+                simplify(MinusSymbol,
+                         PP_S(DivideSymbol, PP(D(CADR(expr), var)),
+                              PP_S(SqrtSymbol,
+                                   PP_S(MinusSymbol, PP(Constant(1.)), PP_S(PowerSymbol, CADR(expr), PP(Constant(2.)))),
+                                   R_MissingArg)),
+                         R_MissingArg);
+            UNPROTECT(7);
         }
         else if (CAR(expr) == AtanSymbol)
         {
             ans = simplify(DivideSymbol, PP(D(CADR(expr), var)),
-                           PP_S(PlusSymbol, Constant(1.), PP_S(PowerSymbol, CADR(expr), Constant(2.))));
-            UNPROTECT(3);
+                           PP_S(PlusSymbol, PP(Constant(1.)), PP_S(PowerSymbol, CADR(expr), PP(Constant(2.)))));
+            UNPROTECT(5);
         }
         else if (CAR(expr) == LGammaSymbol)
         {
@@ -503,27 +504,27 @@ static SEXP D(SEXP expr, SEXP var)
         }
         else if (CAR(expr) == TriGammaSymbol)
         {
-            ans = simplify(TimesSymbol, PP(D(CADR(expr), var)), PP_S(PsiSymbol, CADR(expr), ScalarInteger(2)));
-            UNPROTECT(2);
+            ans = simplify(TimesSymbol, PP(D(CADR(expr), var)), PP_S(PsiSymbol, CADR(expr), PP(ScalarInteger(2))));
+            UNPROTECT(3);
         }
         else if (CAR(expr) == PsiSymbol)
         {
             if (length(expr) == 2)
             {
-                ans = simplify(TimesSymbol, PP(D(CADR(expr), var)), PP_S(PsiSymbol, CADR(expr), ScalarInteger(1)));
-                UNPROTECT(2);
+                ans = simplify(TimesSymbol, PP(D(CADR(expr), var)), PP_S(PsiSymbol, CADR(expr), PP(ScalarInteger(1))));
+                UNPROTECT(3);
             }
             else if (TYPEOF(CADDR(expr)) == INTSXP || TYPEOF(CADDR(expr)) == REALSXP)
             {
                 ans = simplify(TimesSymbol, PP(D(CADR(expr), var)),
-                               PP_S(PsiSymbol, CADR(expr), ScalarInteger(asInteger(CADDR(expr)) + 1)));
-                UNPROTECT(2);
+                               PP_S(PsiSymbol, CADR(expr), PP(ScalarInteger(asInteger(CADDR(expr)) + 1))));
+                UNPROTECT(3);
             }
             else
             {
                 ans = simplify(TimesSymbol, PP(D(CADR(expr), var)),
-                               PP_S(PsiSymbol, CADR(expr), simplify(PlusSymbol, CADDR(expr), ScalarInteger(1))));
-                UNPROTECT(2);
+                               PP_S(PsiSymbol, CADR(expr), simplify(PlusSymbol, CADDR(expr), PP(ScalarInteger(1)))));
+                UNPROTECT(3);
             }
         }
 
