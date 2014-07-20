0e63c4353c7c2bb1fbcdb441e98efc85af664c69
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-07-20 08:11:09 +0000

    non-deprecated version
    git-svn-id: https://svn.r-project.org/R/trunk@66204 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/util.c
+++ src/main/util.c
@@ -2090,7 +2090,7 @@ typedef enum
     ULOC_DATA_LOCALE_TYPE_LIMIT = 3
 } ULocDataLocaleType;
 
-const char *ucol_getLocale(const UCollator *coll, ULocDataLocaleType type, UErrorCode *status);
+const char *ucol_getLocaleByType(const UCollator *coll, ULocDataLocaleType type, UErrorCode *status);
 
 #define U_ZERO_ERROR 0
 #define U_FAILURE(x) ((x) > U_ZERO_ERROR)
@@ -2254,7 +2254,8 @@ SEXP attribute_hidden do_ICUget(SEXP call, SEXP op, SEXP args, SEXP rho)
         int type = asInteger(CAR(args));
         if (type < 1 || type > 2)
             error(_("invalid '%s' value"), "type");
-        res = ucol_getLocale(collator, type == 1 ? ULOC_ACTUAL_LOCALE : ULOC_VALID_LOCALE, &status);
+
+        res = ucol_getLocaleByType(collator, type == 1 ? ULOC_ACTUAL_LOCALE : ULOC_VALID_LOCALE, &status);
         if (!U_FAILURE(status) && res)
             ans = res;
     }
