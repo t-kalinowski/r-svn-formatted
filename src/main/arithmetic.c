927348036357b78a9fdd849f049f7241325d17b3
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-08-09 18:40:05 +0000

    Pre-allocate formal argument lists for matching in primitives; from Tomas Kalibera.
    git-svn-id: https://svn.r-project.org/R/trunk@66326 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -1714,8 +1714,9 @@ SEXP attribute_hidden do_math2(SEXP call, SEXP op, SEXP args, SEXP env)
 /* This is a primitive SPECIALSXP with internal argument matching */
 SEXP attribute_hidden do_Math2(SEXP call, SEXP op, SEXP args, SEXP env)
 {
-    SEXP res, ap, call2;
+    SEXP res, call2;
     int n, nprotect = 2;
+    static SEXP do_Math2_formals = NULL;
 
     if (length(args) >= 2 && isSymbol(CADR(args)) && R_isMissing(CADR(args), env))
     {
@@ -1750,11 +1751,10 @@ SEXP attribute_hidden do_Math2(SEXP call, SEXP op, SEXP args, SEXP env)
             /* If named, do argument matching by name */
             if (TAG(args) != R_NilValue || TAG(CDR(args)) != R_NilValue)
             {
-                PROTECT(ap = CONS(R_NilValue, list1(R_NilValue)));
-                SET_TAG(ap, install("x"));
-                SET_TAG(CDR(ap), install("digits"));
-                PROTECT(args = matchArgs(ap, args, call));
-                nprotect += 2;
+                if (do_Math2_formals == NULL)
+                    do_Math2_formals = allocFormalsList2(install("x"), install("digits"));
+                PROTECT(args = matchArgs(do_Math2_formals, args, call));
+                nprotect++;
             }
             if (length(CADR(args)) == 0)
                 errorcall(call, _("invalid second argument of length 0"));
@@ -1799,6 +1799,7 @@ SEXP attribute_hidden do_log(SEXP call, SEXP op, SEXP args, SEXP env)
 {
     PROTECT(args = evalListKeepMissing(args, env));
     int n = length(args);
+    static SEXP do_log_formals = NULL;
 
     if (n == 1 && TAG(args) == R_NilValue)
     {
@@ -1831,7 +1832,7 @@ SEXP attribute_hidden do_log(SEXP call, SEXP op, SEXP args, SEXP env)
         }
     }
 
-    SEXP res, ap = args, call2;
+    SEXP res, call2;
     int nprotect = 2;
 
     if (n >= 2 && CADR(args) == R_MissingArg)
@@ -1858,11 +1859,10 @@ SEXP attribute_hidden do_log(SEXP call, SEXP op, SEXP args, SEXP env)
             break;
         case 2: {
             /* match argument names if supplied */
-            PROTECT(ap = list2(R_NilValue, R_NilValue));
-            SET_TAG(ap, install("x"));
-            SET_TAG(CDR(ap), R_baseSymbol);
-            PROTECT(args = matchArgs(ap, args, call));
-            nprotect += 2;
+            if (do_log_formals == NULL)
+                do_log_formals = allocFormalsList2(install("x"), R_baseSymbol);
+            PROTECT(args = matchArgs(do_log_formals, args, call));
+            nprotect++;
             if (length(CADR(args)) == 0)
                 errorcall(call, _("invalid argument 'base' of length 0"));
             if (isComplex(CAR(args)) || isComplex(CADR(args)))
