t-kalinowski/r-svn-formatted

commit 7753944649e28362659abc8f5f8f2f3c3348e864

ls() also gets a "sorted" arg, to save (potentially much) time  when alphabetical order is not needed; e.g. in namespace handling
git-svn-id: https://svn.r-project.org/R/trunk@66349 00db46b3-68df-0310-9c12-caf00c1e9a41

src/include/Rinternals.h
@@ -790,7 +790,7 @@ Rboolean Rf_isUnordered(SEXP);
 Rboolean Rf_isUnsorted(SEXP, Rboolean);
 SEXP Rf_lengthgets(SEXP, R_len_t);
 SEXP Rf_xlengthgets(SEXP, R_xlen_t);
-SEXP R_lsInternal(SEXP, Rboolean);
+SEXP R_lsInternal(SEXP, Rboolean, Rboolean);
 SEXP Rf_match(SEXP, SEXP, int);
 SEXP Rf_matchE(SEXP, SEXP, int, SEXP);
 SEXP Rf_namesgets(SEXP, SEXP);

src/main/envir.c
@@ -2470,7 +2470,7 @@ SEXP attribute_hidden do_search(SEXP call, SEXP op, SEXP args, SEXP env)
   do_ls
 
   This code implements the functionality of the "ls" and "objects"
-  functions.  [ ls(envir, all.names) ]
+  functions.  [ ls(envir, all.names, sorted) ]
 
 */
 
@@ -2633,8 +2633,6 @@ static void BuiltinValues(int all, int intern, SEXP values, int *indx)
 
 SEXP attribute_hidden do_ls(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
-    SEXP env;
-    int all;
     checkArity(op, args);
 
     if (IS_USER_DATABASE(CAR(args)))
@@ -2643,20 +2641,24 @@ SEXP attribute_hidden do_ls(SEXP call, SEXP op, SEXP args, SEXP rho)
         return (tb->objects(tb));
     }
 
-    env = CAR(args);
+    SEXP env = CAR(args);
 
     /* if (env == R_BaseNamespace) env = R_BaseEnv; */
 
-    all = asLogical(CADR(args));
+    int all = asLogical(CADR(args));
     if (all == NA_LOGICAL)
         all = 0;
 
-    return R_lsInternal(env, all);
+    int sort_nms = asLogical(CADDR(args)); /* sorted = TRUE/FALSE */
+    if (sort_nms == NA_LOGICAL)
+        sort_nms = 0;
+
+    return R_lsInternal(env, all, sort_nms);
 }
 
 /* takes a *list* of environments and a boolean indicating whether to get all
    names */
-SEXP R_lsInternal(SEXP env, Rboolean all)
+SEXP R_lsInternal(SEXP env, Rboolean all, Rboolean sorted)
 {
     int k;
     SEXP ans;
@@ -2689,7 +2691,8 @@ SEXP R_lsInternal(SEXP env, Rboolean all)
     }
 
     UNPROTECT(1);
-    sortVector(ans, FALSE);
+    if (sorted)
+        sortVector(ans, FALSE);
     return ans;
 }
 

src/library/base/R/attach.R
@@ -197,7 +197,7 @@ detach <- function(name, pos = 2L, unload = FALSE, character.only = FALSE,
 
 ls <- objects <-
     function (name, pos = -1L, envir = as.environment(pos), all.names = FALSE,
-              pattern)
+              pattern, sorted = TRUE)
 {
     if (!missing(name)) {
         pos <- tryCatch(name, error = function(e)e)
@@ -210,7 +210,7 @@ ls <- objects <-
             pos <- name
         }
     }
-    all.names <- .Internal(ls(envir, all.names))
+    all.names <- .Internal(ls(envir, all.names, sorted))
     if (!missing(pattern)) {
         if ((ll <- length(grep("[", pattern, fixed = TRUE))) &&
             ll != length(grep("]", pattern, fixed = TRUE))) {

src/library/base/R/namespace.R
@@ -969,9 +969,9 @@ namespaceImportMethods <- function(self, ns, vars, from = NULL)
 
 importIntoEnv <- function(impenv, impnames, expenv, expnames) {
     exports <- getNamespaceInfo(expenv, "exports")
-    ex <- .Internal(ls(exports, TRUE))
-    if(!all(expnames %in% ex)) {
-        miss <- expnames[! expnames %in% ex]
+    ex <- .Internal(ls(exports, TRUE, FALSE))
+    if(!all(eie <- expnames %in% ex)) {
+        miss <- expnames[!eie]
         ## if called (indirectly) for namespaceImportClasses
         ## these are all classes
         if(all(grepl("^\\.__C__", miss))) {
@@ -1008,7 +1008,7 @@ namespaceExport <- function(ns, vars) {
             exports <- getNamespaceInfo(ns, "exports")
             expnames <- names(new)
             intnames <- new
-            objs <- .Internal(ls(exports, TRUE))
+            objs <- .Internal(ls(exports, TRUE, FALSE))
             ex <- expnames %in% objs
             if(any(ex))
                 warning(sprintf(ngettext(sum(ex),
@@ -1032,7 +1032,7 @@ namespaceExport <- function(ns, vars) {
         }
         new <- makeImportExportNames(unique(vars))
         ## calling exists each time is too slow, so do two phases
-        undef <- new[! new %in% .Internal(ls(ns, TRUE))]
+        undef <- new[! new %in% .Internal(ls(ns, TRUE, FALSE))]
         undef <- undef[! vapply(undef, exists, NA, envir = ns)]
         if (length(undef)) {
             undef <- do.call("paste", as.list(c(undef, sep = ", ")))
@@ -1405,7 +1405,7 @@ registerS3methods <- function(info, package, env)
     z <- is.na(info[,3])
     info[z,3] <- methname[z]
     Info <- cbind(info, methname)
-    loc <- .Internal(ls(env, TRUE))
+    loc <- .Internal(ls(env, TRUE, FALSE))
     notex <- !(info[,3] %in% loc)
     if(any(notex))
         warning(sprintf(ngettext(sum(notex),

src/library/base/man/ls.Rd
@@ -1,6 +1,6 @@
 % File src/library/base/man/ls.Rd
 % Part of the R package, http://www.R-project.org
-% Copyright 1995-2012 R Core Team
+% Copyright 1995-2014 R Core Team
 % Distributed under GPL 2 or later
 
 \name{ls}
@@ -9,9 +9,9 @@
 \title{List Objects}
 \usage{
 ls(name, pos = -1L, envir = as.environment(pos),
-   all.names = FALSE, pattern)
+   all.names = FALSE, pattern, sorted = TRUE)
 objects(name, pos= -1L, envir = as.environment(pos),
-        all.names = FALSE, pattern)
+        all.names = FALSE, pattern, sorted = TRUE)
 }
 \arguments{
   \item{name}{which environment to use in listing the available objects.
@@ -29,6 +29,9 @@ objects(name, pos= -1L, envir = as.environment(pos),
   \item{pattern}{an optional \link{regular expression}.  Only names
     matching \code{pattern} are returned.  \code{\link{glob2rx}} can be
     used to convert wildcard patterns to regular expressions.}
+  \item{sorted}{logical indicating if the resulting
+    \code{\link{character}} should be sorted alphabetically.  Note that
+    this is the part of \code{ls()} which may take most of the time.}
 }
 \description{
   \code{ls} and \code{objects} return a vector of character strings

src/main/names.c
@@ -673,7 +673,7 @@ FUNTAB R_FunTab[] = {
     {"recordGraphics", do_recordGraphics, 0, 211, 3, {PP_FOREIGN, PREC_FN, 0}},
     {"dyn.load", do_dynload, 0, 111, 4, {PP_FUNCALL, PREC_FN, 0}},
     {"dyn.unload", do_dynunload, 0, 111, 1, {PP_FUNCALL, PREC_FN, 0}},
-    {"ls", do_ls, 1, 11, 2, {PP_FUNCALL, PREC_FN, 0}},
+    {"ls", do_ls, 1, 11, 3, {PP_FUNCALL, PREC_FN, 0}},
     {"typeof", do_typeof, 1, 11, 1, {PP_FUNCALL, PREC_FN, 0}},
     {"eval", do_eval, 0, 211, 3, {PP_FUNCALL, PREC_FN, 0}},
     {"sys.parent", do_sys, 1, 11, -1, {PP_FUNCALL, PREC_FN, 0}},

commit 0f9b63aa9c133c82d5b9ee1d81ade73ee7bb4210

workaround for package breakage caused by r66349
git-svn-id: https://svn.r-project.org/R/trunk@66350 00db46b3-68df-0310-9c12-caf00c1e9a41

src/include/Rinternals.h
@@ -790,7 +790,8 @@ Rboolean Rf_isUnordered(SEXP);
 Rboolean Rf_isUnsorted(SEXP, Rboolean);
 SEXP Rf_lengthgets(SEXP, R_len_t);
 SEXP Rf_xlengthgets(SEXP, R_xlen_t);
-SEXP R_lsInternal(SEXP, Rboolean, Rboolean);
+SEXP R_lsInternal(SEXP, Rboolean);
+SEXP R_lsInternal3(SEXP, Rboolean, Rboolean);
 SEXP Rf_match(SEXP, SEXP, int);
 SEXP Rf_matchE(SEXP, SEXP, int, SEXP);
 SEXP Rf_namesgets(SEXP, SEXP);

src/main/envir.c
@@ -2653,12 +2653,12 @@ SEXP attribute_hidden do_ls(SEXP call, SEXP op, SEXP args, SEXP rho)
     if (sort_nms == NA_LOGICAL)
         sort_nms = 0;
 
-    return R_lsInternal(env, all, sort_nms);
+    return R_lsInternal3(env, all, sort_nms);
 }
 
 /* takes a *list* of environments and a boolean indicating whether to get all
    names */
-SEXP R_lsInternal(SEXP env, Rboolean all, Rboolean sorted)
+SEXP R_lsInternal3(SEXP env, Rboolean all, Rboolean sorted)
 {
     int k;
     SEXP ans;
@@ -2694,6 +2694,12 @@ SEXP R_lsInternal(SEXP env, Rboolean all, Rboolean sorted)
     if (sorted)
         sortVector(ans, FALSE);
     return ans;
+}
+
+/* non-API version used in several packages */
+SEXP R_lsInternal(SEXP env, Rboolean all)
+{
+    return R_lsInternal3(env, all, TRUE);
 }
 
 /* transform an environment into a named list: as.list.environment(.) */

src/main/eval.c
@@ -4421,8 +4421,8 @@ static R_INLINE void DO_MATSUBSET(SEXP rho)
 
     if (dim != R_NilValue)
     {
-        int i = bcStackIndex(R_BCNodeStackTop - 2);
-        int j = bcStackIndex(R_BCNodeStackTop - 1);
+        int i = (int)bcStackIndex(R_BCNodeStackTop - 2);
+        int j = (int)bcStackIndex(R_BCNodeStackTop - 1);
         int nrow = INTEGER(dim)[0];
         int ncol = INTEGER(dim)[1];
         if (i > 0 && j > 0 && i <= nrow && j <= ncol)
@@ -4576,8 +4576,8 @@ static R_INLINE void DO_SETMATSUBSET(SEXP rho)
 
     if (dim != R_NilValue)
     {
-        int i = bcStackIndex(R_BCNodeStackTop - 2);
-        int j = bcStackIndex(R_BCNodeStackTop - 1);
+        int i = (int)bcStackIndex(R_BCNodeStackTop - 2);
+        int j = (int)bcStackIndex(R_BCNodeStackTop - 1);
         int nrow = INTEGER(dim)[0];
         int ncol = INTEGER(dim)[1];
         if (i > 0 && j > 0 && i <= nrow && j <= ncol)