e481d1861bd25083fbcce9eafce46357e35b0219
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-12-24 07:05:53 +0000

    correct PROTECT imbalance in last change
    git-svn-id: https://svn.r-project.org/R/trunk@67213 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/src/methods_list_dispatch.c
+++ src/library/methods/src/methods_list_dispatch.c
@@ -827,7 +827,10 @@ static SEXP R_loadMethod(SEXP def, SEXP fname, SEXP ev)
         /* this shouldn't be needed but check the generic being
            "loadMethod", which would produce a recursive loop */
         if (strcmp(CHAR(asChar(fname)), "loadMethod") == 0)
+        {
+            UNPROTECT(1);
             return def;
+        }
         SEXP e, val;
         PROTECT(e = allocVector(LANGSXP, 4));
         SETCAR(e, R_loadMethod_name);
