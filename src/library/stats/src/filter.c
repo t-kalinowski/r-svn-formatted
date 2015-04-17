8a4bd29ef9d668ed874c9b8d1040f5d5e2e515f0
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-04-17 15:05:13 +0000

    PR#15832: now |acf()| <= 1, same for ccf()
    git-svn-id: https://svn.r-project.org/R/trunk@68194 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/filter.c
+++ src/library/stats/src/filter.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
 
- *  Copyright (C) 1999-12   The R Core Team
+ *  Copyright (C) 1999-2015   The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -138,18 +138,16 @@ SEXP rfilter(SEXP x, SEXP filter, SEXP out)
 }
 
 /* now allows missing values */
-static void acf0(double *x, int n, int ns, int nl, int correlation, double *acf)
+static void acf0(double *x, int n, int ns, int nl, Rboolean correlation, double *acf)
 {
-    int d1 = nl + 1, d2 = ns * d1, nu;
-    double sum, *se;
+    int d1 = nl + 1, d2 = ns * d1;
 
-    se = (double *)R_alloc(ns, sizeof(double));
     for (int u = 0; u < ns; u++)
         for (int v = 0; v < ns; v++)
             for (int lag = 0; lag <= nl; lag++)
             {
-                sum = 0.0;
-                nu = 0;
+                double sum = 0.0;
+                int nu = 0;
                 for (int i = 0; i < n - lag; i++)
                     if (!ISNAN(x[i + lag + n * u]) && !ISNAN(x[i + n * v]))
                     {
@@ -160,8 +158,6 @@ static void acf0(double *x, int n, int ns, int nl, int correlation, double *acf)
             }
     if (correlation)
     {
-        for (int u = 0; u < ns; u++)
-            se[u] = sqrt(acf[0 + d1 * u + d2 * u]);
         if (n == 1)
         {
             for (int u = 0; u < ns; u++)
@@ -169,10 +165,16 @@ static void acf0(double *x, int n, int ns, int nl, int correlation, double *acf)
         }
         else
         {
+            double *se = (double *)R_alloc(ns, sizeof(double));
+            for (int u = 0; u < ns; u++)
+                se[u] = sqrt(acf[0 + d1 * u + d2 * u]);
             for (int u = 0; u < ns; u++)
                 for (int v = 0; v < ns; v++)
                     for (int lag = 0; lag <= nl; lag++)
-                        acf[lag + d1 * u + d2 * v] /= se[u] * se[v];
+                    { // ensure correlations remain in  [-1,1] :
+                        double a = acf[lag + d1 * u + d2 * v] / (se[u] * se[v]);
+                        acf[lag + d1 * u + d2 * v] = (a > 1.) ? 1. : ((a < -1.) ? -1. : a);
+                    }
         }
     }
 }
