67817bc5548195e284dbf102686b5c2217a4cfe7
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-09-01 07:23:16 +0000

    pnchisq (*, df=0, ncp > 0, log.p=TRUE) now correct and accurate
    git-svn-id: https://svn.r-project.org/R/trunk@69248 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pnchisq.c
+++ src/nmath/pnchisq.c
@@ -11,8 +11,8 @@
  *    distribution function. Appl.Statist., 41, 478-482.
 
  *  Other parts
- *  Copyright (C) 2000-2014  The R Core Team
- *  Copyright (C) 2003-2014  The R Foundation
+ *  Copyright (C) 2000-2015  The R Core Team
+ *  Copyright (C) 2003-2015  The R Foundation
  */
 
 #include "nmath.h"
@@ -21,8 +21,8 @@
 /*----------- DEBUGGING -------------
  *
  *	make CFLAGS='-DDEBUG_pnch ....'
-(cd ~/R/D/r-devel/debian-64-inst/src/nmath; gcc -I. -I../../src/include -I../../../R/src/include -I/usr/local/include
--DHAVE_CONFIG_H -DDEBUG_pnch -fopenmp -g -pedantic -Wall --std=gnu99 -DDEBUG_q -Wcast-align -Wclobbered  -c
+(cd `R-devel RHOME`/src/nmath; gcc -I. -I../../src/include -I../../../R/src/include -I/usr/local/include -DHAVE_CONFIG_H
+-fopenmp -g -O0 -pedantic -Wall --std=gnu99 -DDEBUG_pnch -DDEBUG_q -Wcast-align -Wclobbered  -c
 ../../../R/src/nmath/pnchisq.c -o pnchisq.o )
 
  * -- Feb.6, 2000 (R pre0.99); M.Maechler:  still have
@@ -97,12 +97,15 @@ double attribute_hidden pnchisq_raw(double x, double f, double theta /* = ncp */
     if (x <= 0.)
     {
         if (x == 0. && f == 0.)
-            return lower_tail ? exp(-0.5 * theta) : -expm1(-0.5 * theta);
+        {
+#define _L (-0.5 * theta) // = -lambda
+            return lower_tail ? R_D_exp(_L) : (log_p ? R_Log1_Exp(_L) : -expm1(_L));
+        }
         /* x < 0  or {x==0, f > 0} */
-        return lower_tail ? 0. : 1.;
+        return R_DT_0;
     }
     if (!R_FINITE(x))
-        return lower_tail ? 1. : 0.;
+        return R_DT_1;
 
         /* This is principally for use from qnchisq */
 #ifndef MATHLIB_STANDALONE
