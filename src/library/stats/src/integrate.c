54f4e77f1b38aebc244438d2ff3f7d6d6456062d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-03-30 10:38:06 +0000

    integrate() now signals an error when one of its bounds is longer than one
    git-svn-id: https://svn.r-project.org/R/trunk@70401 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/integrate.c
+++ src/library/stats/src/integrate.c
@@ -25,7 +25,14 @@
 #include <Rinternals.h>
 #include <R_ext/Applic.h>
 
-/* alled via .External(.) :*/
+#ifdef ENABLE_NLS
+#include <libintl.h>
+#define _(String) dgettext("stats", String)
+#else
+#define _(String) (String)
+#endif
+
+/* called via .External(.) :*/
 SEXP call_dqags(SEXP args);
 SEXP call_dqagi(SEXP args);
 
@@ -79,8 +86,12 @@ SEXP call_dqags(SEXP args)
     args = CDR(args);
     is.env = CAR(args);
     args = CDR(args);
+    if (length(CAR(args)) > 1)
+        error(_("'%s' must be of length one"), "lower");
     lower = asReal(CAR(args));
     args = CDR(args);
+    if (length(CAR(args)) > 1)
+        error(_("'%s' must be of length one"), "upper");
     upper = asReal(CAR(args));
     args = CDR(args);
     epsabs = asReal(CAR(args));
@@ -127,6 +138,8 @@ SEXP call_dqagi(SEXP args)
     args = CDR(args);
     is.env = CAR(args);
     args = CDR(args);
+    if (length(CAR(args)) > 1)
+        error(_("'%s' must be of length one"), "bound");
     bound = asReal(CAR(args));
     args = CDR(args);
     inf = asInteger(CAR(args));
