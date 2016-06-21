717e360f27e2ecccdaa74395d4b8782ef887f035
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-06-21 20:19:15 +0000

    Mark as immutable arguments extracted from a call for a SPECIAL call in the byte code intepreter; from Tomas Kalibera.
    git-svn-id: https://svn.r-project.org/R/trunk@70810 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -5600,6 +5600,14 @@ static R_INLINE Rboolean GETSTACK_LOGICAL_NO_NA_PTR(R_bcstack_t *s, int callidx,
     }
 }
 
+static SEXP markSpecialArgs(SEXP args)
+{
+    SEXP arg;
+    for (arg = args; arg != R_NilValue; arg = CDR(arg))
+        MARK_NOT_MUTABLE(CAR(arg));
+    return args;
+}
+
 static SEXP bcEval(SEXP body, SEXP rho, Rboolean useCache)
 {
     SEXP value = R_NilValue, constants;
@@ -6109,7 +6117,7 @@ static SEXP bcEval(SEXP body, SEXP rho, Rboolean useCache)
             case SPECIALSXP:
                 flag = PRIMPRINT(fun);
                 R_Visible = flag != 1;
-                value = PRIMFUN(fun)(call, fun, CDR(call), rho);
+                value = PRIMFUN(fun)(call, fun, markSpecialArgs(CDR(call)), rho);
                 if (flag < 2)
                     R_Visible = flag != 1;
                 break;
@@ -6167,7 +6175,7 @@ static SEXP bcEval(SEXP body, SEXP rho, Rboolean useCache)
             }
             flag = PRIMPRINT(fun);
             R_Visible = flag != 1;
-            value = PRIMFUN(fun)(call, fun, CDR(call), rho);
+            value = PRIMFUN(fun)(call, fun, markSpecialArgs(CDR(call)), rho);
             if (flag < 2)
                 R_Visible = flag != 1;
             vmaxset(vmax);
