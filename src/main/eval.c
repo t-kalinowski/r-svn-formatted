43ff771399f4c8ab94e0d4294b47fd111cdc832d
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-06-28 12:01:49 +0000

    Fix for infinite loop reported in PR16971.
    git-svn-id: https://svn.r-project.org/R/trunk@70841 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1181,8 +1181,10 @@ SEXP applyClosure(SEXP call, SEXP op, SEXP arglist, SEXP rho, SEXP suppliedvars)
 
     if ((SETJMP(cntxt.cjmpbuf)))
     {
-        if (R_ReturnedValue == R_RestartToken)
+        if (!cntxt.jumptarget && /* ignores intermediate jumps for on.exits */
+            R_ReturnedValue == R_RestartToken)
         {
+            REprintf("flag = %d\n", cntxt.callflag);
             cntxt.callflag = CTXT_RETURN; /* turn restart off */
             R_ReturnedValue = R_NilValue; /* remove restart token */
             PROTECT(tmp = eval(body, newrho));
