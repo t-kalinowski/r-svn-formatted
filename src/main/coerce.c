8aafc18afa4706fde00b7bd227548fc2e97ffcea
lawrence <lawrence@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-09-19 12:52:22 +0000

    as.character.factor() uses S4 inheritance when checking the type of its argument (fixes PR#17141)
    git-svn-id: https://svn.r-project.org/R/trunk@71307 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/coerce.c
+++ src/main/coerce.c
@@ -1509,7 +1509,7 @@ SEXP asCharacterFactor(SEXP x)
 {
     SEXP ans;
 
-    if (!inherits(x, "factor"))
+    if (!inherits2(x, "factor"))
         error(_("attempting to coerce non-factor"));
 
     R_xlen_t i, n = XLENGTH(x);
