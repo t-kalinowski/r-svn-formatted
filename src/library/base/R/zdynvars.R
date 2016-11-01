t-kalinowski/r-svn-formatted

commit 80f9a502d4a968ae6349753b3d308d76dfc4190b

check existence before normalizePath
git-svn-id: https://svn.r-project.org/R/trunk@71356 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/base/R/zdynvars.R
@@ -43,10 +43,11 @@
         if(!missing(new)) {
             ## paths don't really need to be unique, but searching
             ## large library trees repeatedly would be inefficient.
-            ## Use normalizePath for display: but also does path.expand
+            ## Use normalizePath for display
             new <- Sys.glob(path.expand(new))
-            paths <- unique(normalizePath(c(new, .Library.site, .Library), '/'))
-            .lib.loc <<- paths[dir.exists(paths)]
+            paths <- c(new, .Library.site, .Library)
+            paths <- paths[dir.exists(paths)]
+            .lib.loc <<- unique(normalizePath(paths))
         }
         else
             .lib.loc

commit 100bbf0191ed006ce800313625d2d813d63d478e

Fix Windows bug introduced in r71356 because .libPaths() started returning backslashes.  This removes the need for workaround r71614, but it has been left in place as it might help in some obscure situation.
git-svn-id: https://svn.r-project.org/R/trunk@71616 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/base/R/zdynvars.R
@@ -47,7 +47,7 @@
             new <- Sys.glob(path.expand(new))
             paths <- c(new, .Library.site, .Library)
             paths <- paths[dir.exists(paths)]
-            .lib.loc <<- unique(normalizePath(paths))
+            .lib.loc <<- unique(normalizePath(paths, "/"))
         }
         else
             .lib.loc