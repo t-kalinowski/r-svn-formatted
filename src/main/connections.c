dcf259bd174f4e239f5ecfb26cd6a4ea1cbe2ad2
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-12-04 15:30:02 +0000

    Do not push back incomplete final line for text mode gzfile connections.
    git-svn-id: https://svn.r-project.org/R/trunk@71724 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -4075,7 +4075,7 @@ no_more_lines:
     }
     if (nbuf > 0)
     { /* incomplete last line */
-        if (con->text && !con->blocking)
+        if (con->text && !con->blocking && (strcmp(con->class, "gzfile") != 0))
         {
             /* push back the rest */
             con_pushback(con, 0, buf);
