c02473a46c25008fd73ae6144b1b6109b888eb23
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-05-19 15:47:53 +0000

    Do not eagerly duplicate in body, bodyCode, formals (performance optimization, with Luke Tierney).
    git-svn-id: https://svn.r-project.org/R/trunk@72705 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/builtin.c
+++ src/main/builtin.c
@@ -263,7 +263,12 @@ SEXP attribute_hidden do_formals(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
     checkArity(op, args);
     if (TYPEOF(CAR(args)) == CLOSXP)
-        return duplicate(FORMALS(CAR(args)));
+    {
+        SEXP f = FORMALS(CAR(args));
+        if (NAMED(CAR(args)) > NAMED(f))
+            SET_NAMED(f, NAMED(CAR(args)));
+        return f;
+    }
     else
     {
         if (!(TYPEOF(CAR(args)) == BUILTINSXP || TYPEOF(CAR(args)) == SPECIALSXP))
@@ -276,7 +281,12 @@ SEXP attribute_hidden do_body(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
     checkArity(op, args);
     if (TYPEOF(CAR(args)) == CLOSXP)
-        return duplicate(BODY_EXPR(CAR(args)));
+    {
+        SEXP b = BODY_EXPR(CAR(args));
+        if (NAMED(CAR(args)) > NAMED(b))
+            SET_NAMED(b, NAMED(CAR(args)));
+        return b;
+    }
     else
     {
         if (!(TYPEOF(CAR(args)) == BUILTINSXP || TYPEOF(CAR(args)) == SPECIALSXP))
@@ -289,7 +299,12 @@ SEXP attribute_hidden do_bodyCode(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
     checkArity(op, args);
     if (TYPEOF(CAR(args)) == CLOSXP)
-        return duplicate(BODY(CAR(args)));
+    {
+        SEXP bc = BODY(CAR(args));
+        if (NAMED(CAR(args)) > NAMED(bc))
+            SET_NAMED(bc, NAMED(CAR(args)));
+        return bc;
+    }
     else
         return R_NilValue;
 }
