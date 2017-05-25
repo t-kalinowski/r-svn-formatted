abafa37beaaaac7e62f3a25bf89a7bc3a5736496
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-05-25 07:54:50 +0000

    Fix progress bar when downloading multiple files concurrently with libcurl on Windows (PR16454), from Nathan Sosnovske.
    git-svn-id: https://svn.r-project.org/R/trunk@72735 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/libcurl.c
+++ src/modules/internet/libcurl.c
@@ -725,7 +725,7 @@ SEXP attribute_hidden in_do_curlDownload(SEXP call, SEXP op, SEXP args, SEXP rho
     } while (still_running);
     R_Busy(0);
 #ifdef Win32
-    if (R_Interactive && !quiet)
+    if (R_Interactive && !quiet && nurls <= 1)
     {
         endcontext(&(pbar.cntxt));
         doneprogressbar(&pbar);
