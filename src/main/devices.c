9a8079214f196fb3a037f9bcfda3165cb29d830b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-08-01 12:14:56 +0000

    finally fix PR#15883: options(device = <invalid fn>)
    git-svn-id: https://svn.r-project.org/R/trunk@73005 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/devices.c
+++ src/main/devices.c
@@ -181,6 +181,8 @@ pGEDevDesc GEcurrentDevice(void)
         }
         else
             error(_("no active or default device"));
+        if (NoDevices()) // the startup above may have failed
+            error(_("no active device and default getOption(\"device\") is invalid"));
     }
     return R_Devices[R_CurrentDevice];
 }
