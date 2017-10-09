7ada4c66d1a7939b12f19e156d365f6b14f5824d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-10-09 07:06:41 +0000

    add comment
    git-svn-id: https://svn.r-project.org/R/trunk@73503 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/raw.c
+++ src/main/raw.c
@@ -401,6 +401,7 @@ SEXP attribute_hidden do_intToUtf8(SEXP call, SEXP op, SEXP args, SEXP env)
                     break;
                 }
                 len += 4;
+                ##all points not in the basic plane have length 4
             }
             else
                 len += inttomb(NULL, this);
