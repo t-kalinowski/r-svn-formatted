a0045578e8c6cff2efe4684030fd1ab2623d6ff0
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-11-15 08:49:37 +0000

    Removed stray character.
    git-svn-id: https://svn.r-project.org/R/trunk@73733 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/complex.c
+++ src/main/complex.c
@@ -1,23 +1,23 @@
-x /*
-   *  R : A Computer Language for Statistical Data Analysis
-   *  Copyright (C) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
-   *  Copyright (C) 2000-2016	    The R Core Team
-   *  Copyright (C) 2005		    The R Foundation
-   *
-   *  This program is free software; you can redistribute it and/or modify
-   *  it under the terms of the GNU General Public License as published by
-   *  the Free Software Foundation; either version 2 of the License, or
-   *  (at your option) any later version.
-   *
-   *  This program is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   *  GNU General Public License for more details.
-   *
-   *  You should have received a copy of the GNU General Public License
-   *  along with this program; if not, a copy is available at
-   *  https://www.R-project.org/Licenses/
-   */
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
+ *  Copyright (C) 2000-2016	    The R Core Team
+ *  Copyright (C) 2005		    The R Foundation
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  https://www.R-project.org/Licenses/
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -70,8 +70,7 @@ x /*
 /* interval at which to check interrupts, a guess */
 #define NINTERRUPT 10000000
 
-    SEXP attribute_hidden
-    complex_unary(ARITHOP_TYPE code, SEXP s1, SEXP call)
+SEXP attribute_hidden complex_unary(ARITHOP_TYPE code, SEXP s1, SEXP call)
 {
     R_xlen_t i, n;
     SEXP ans;
