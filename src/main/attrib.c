6cc5ccf9762453f64d81e83cd603f8ce4b74793a
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-01-02 12:50:26 +0000

    Removed commented-out code.
    git-svn-id: https://svn.r-project.org/R/trunk@74020 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/attrib.c
+++ src/main/attrib.c
@@ -273,7 +273,6 @@ SEXP setAttrib(SEXP vec, SEXP name, SEXP val)
 
     if (MAYBE_REFERENCED(val))
         val = R_FixupRHS(vec, val);
-    // SET_NAMED(val, NAMED(val) | NAMED(vec));
     UNPROTECT(2);
 
     if (name == R_NamesSymbol)
@@ -1951,7 +1950,6 @@ SEXP R_do_slot_assign(SEXP obj, SEXP name, SEXP value)
         PROTECT(name);
         if (MAYBE_REFERENCED(value))
             value = R_FixupRHS(obj, value);
-        // SET_NAMED(value, NAMED(value) | NAMED(obj));
         UNPROTECT(1);
         installAttrib(obj, name, value);
 #endif
