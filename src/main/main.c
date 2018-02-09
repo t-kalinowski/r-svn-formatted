9bde362735a54baeeafeb5144411bbf88d81e0f1
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-02-09 11:04:05 +0000

    Show source references for frames in byte-code interpreter in 'where' output in debugger.
    git-svn-id: https://svn.r-project.org/R/trunk@74239 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -1149,7 +1149,12 @@ static void printwhere(void)
         if ((cptr->callflag & (CTXT_FUNCTION | CTXT_BUILTIN)) && (TYPEOF(cptr->call) == LANGSXP))
         {
             Rprintf("where %d", lct++);
-            SrcrefPrompt("", cptr->srcref);
+            SEXP sref;
+            if (cptr->srcref == R_InBCInterpreter)
+                sref = R_findBCInterpreterSrcref(cptr);
+            else
+                sref = cptr->srcref;
+            SrcrefPrompt("", sref);
             PrintValue(cptr->call);
         }
     }
