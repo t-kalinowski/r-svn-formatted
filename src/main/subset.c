d92d94d2b22193ec3ee2d171ffea7c095ba32810
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-03-12 21:57:21 +0000

    Return NULL when indexing a list using dollar and empty string.
    git-svn-id: https://svn.r-project.org/R/trunk@74391 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subset.c
+++ src/main/subset.c
@@ -1190,6 +1190,7 @@ enum pmatch
 static enum pmatch pstrmatch(SEXP target, SEXP input, size_t slen)
 {
     const char *st = "";
+    const char *si = "";
     const void *vmax = vmaxget();
 
     if (target == R_NilValue)
@@ -1204,7 +1205,8 @@ static enum pmatch pstrmatch(SEXP target, SEXP input, size_t slen)
         st = translateChar(target);
         break;
     }
-    if (strncmp(st, translateChar(input), slen) == 0)
+    si = translateChar(input);
+    if (si[0] != '\0' && strncmp(st, si, slen) == 0)
     {
         vmaxset(vmax);
         return (strlen(st) == slen) ? EXACT_MATCH : PARTIAL_MATCH;
