d7e4d278194b0c8b3404a5d009bd6948863f22dd
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-04-05 13:22:13 +0000

    Allow UTF-8 on standard error with RGui on Windows (PR#16543).
    git-svn-id: https://svn.r-project.org/R/trunk@74537 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2000-2017   The R Core Team.
+ *  Copyright (C) 2000-2018   The R Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -5783,7 +5783,7 @@ SEXP attribute_hidden do_sinknumber(SEXP call, SEXP op, SEXP args, SEXP rho)
 void WinCheckUTF8(void)
 {
     if (CharacterMode == RGui)
-        WinUTF8out = (SinkCons[R_SinkNumber] == 1);
+        WinUTF8out = (SinkCons[R_SinkNumber] == 1 || SinkCons[R_SinkNumber] == 2);
     else
         WinUTF8out = FALSE;
 }
