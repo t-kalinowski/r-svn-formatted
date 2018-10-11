b6b84c1243c66979d0fde8352b4d3d546deb59b7
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-10-11 12:08:52 +0000

    Update report for condition of length bigger than one.
    git-svn-id: https://svn.r-project.org/R/trunk@75428 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -2221,6 +2221,7 @@ static void badIfCond(SEXP s, SEXP call, SEXP rho)
         int olderr = R_ErrorCon;
         R_ErrorCon = 2;
         REprintf(" ----------- FAILURE REPORT -------------- \n");
+        REprintf(" --- failure: the condition has length > 1 ---\n");
         REprintf(" --- srcref --- \n");
         SrcrefPrompt("", R_getCurrentSrcref());
         REprintf("\n");
