0d28d2fbfbd8be33c4f6357d32c988b0453af2a8
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-10-24 22:21:56 +0000

    Avoid creating wrappers of wrappers in some cases.
    git-svn-id: https://svn.r-project.org/R/trunk@75487 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/altclasses.c
+++ src/main/altclasses.c
@@ -1758,6 +1758,10 @@ static SEXP wrap_meta(SEXP x, int srt, int no_na)
         return x;
     }
 
+    /* avoid wrappers of wrappers, at least in some cases */
+    if (is_wrapper(x) && srt == 0 && no_na == 0)
+        return shallow_duplicate(x);
+
 #ifndef WRAPATTRIB
     if (ATTRIB(x) != R_NilValue)
         /* For objects without references we could move the attributes
