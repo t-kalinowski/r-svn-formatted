46372e1c5b2c0b9c3e8f3c890d51cc27f9ad2f50
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-10-29 20:22:56 +0000

    Fix typo in comment.
    git-svn-id: https://svn.r-project.org/R/trunk@75514 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/duplicate.c
+++ src/main/duplicate.c
@@ -640,7 +640,7 @@ static SEXP duplicate_attr(SEXP x, Rboolean deep)
         if (deep)
         {
             PROTECT(val);
-            /* the spine has been duplicated; we could just to the values */
+            /* the spine has been duplicated; we could just do the values */
             SET_ATTRIB(val, duplicate(ATTRIB(val)));
             UNPROTECT(1); /* val */
         }
