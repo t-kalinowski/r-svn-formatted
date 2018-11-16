0b8ad5274b4f84ed63afc2ba2f34ddbd4d6fb09d
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-11-16 08:49:02 +0000

    Avoid using UNPROTECT_PTR in the parser. Simpler allocation/protection of source references and parse data as part of parse state.
    git-svn-id: https://svn.r-project.org/R/trunk@75613 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -878,12 +878,12 @@ void setup_Rmainloop(void)
     srand(TimeToSeed());
 
     InitArithmetic();
-    InitParser();
     InitTempDir(); /* must be before InitEd */
     InitMemory();
     InitStringHash(); /* must be before InitNames */
     InitBaseEnv();
-    InitNames(); /* must be after InitBaseEnv to use R_EmptyEnv */
+    InitNames();  /* must be after InitBaseEnv to use R_EmptyEnv */
+    InitParser(); /* must be after InitMemory, InitNames */
     InitGlobalEnv();
     InitDynload();
     InitOptions();
