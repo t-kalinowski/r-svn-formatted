aa721c39e933b718d7b70d1916c594fe0ac4dbde
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-12-31 15:14:02 +0000

    Fix for PR 17514.
    git-svn-id: https://svn.r-project.org/R/trunk@75934 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/duplicate.c
+++ src/main/duplicate.c
@@ -488,6 +488,11 @@ void copyListMatrix(SEXP s, SEXP t, Rboolean byrow)
     }
 }
 
+static R_INLINE SEXP VECTOR_ELT_LD(SEXP x, R_xlen_t i)
+{
+    return lazy_duplicate(VECTOR_ELT(x, i));
+}
+
 void copyMatrix(SEXP s, SEXP t, Rboolean byrow)
 {
     int nr = nrows(s), nc = ncols(s);
@@ -520,7 +525,7 @@ void copyMatrix(SEXP s, SEXP t, Rboolean byrow)
         case EXPRSXP:
         case VECSXP:
             FILL_MATRIX_BYROW_ITERATE(0, nr, nc, nt)
-            SET_VECTOR_ELT(s, didx, VECTOR_ELT(t, sidx));
+            SET_VECTOR_ELT(s, didx, VECTOR_ELT_LD(t, sidx));
             break;
         case RAWSXP:
             FILL_MATRIX_BYROW_ITERATE(0, nr, nc, nt)
@@ -597,8 +602,8 @@ COPY_WITH_RECYCLE(double, Real)      /* xcopyRealWithRecycle */
         }                                                                                                              \
     }
 
-COPY_ELT_WITH_RECYCLE(String, STRING_ELT, SET_STRING_ELT) /* xcopyStringWithRecycle */
-COPY_ELT_WITH_RECYCLE(Vector, VECTOR_ELT, SET_VECTOR_ELT) /* xcopyVectorWithRecycle */
+COPY_ELT_WITH_RECYCLE(String, STRING_ELT, SET_STRING_ELT)    /* xcopyStringWithRecycle */
+COPY_ELT_WITH_RECYCLE(Vector, VECTOR_ELT_LD, SET_VECTOR_ELT) /* xcopyVectorWithRecycle */
 
 #define FILL_WITH_RECYCLE(VALTYPE, TNAME)                                                                              \
     void attribute_hidden xfill##TNAME##MatrixWithRecycle(VALTYPE *dst, VALTYPE *src, R_xlen_t dstart, R_xlen_t drows, \
