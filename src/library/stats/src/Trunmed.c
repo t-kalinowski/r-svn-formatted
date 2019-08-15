02db67bb2d1329b72eeaff5e82b5a83dbf8b0e06
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-08-15 06:19:28 +0000

    fix thinko (every_i)
    git-svn-id: https://svn.r-project.org/R/trunk@76991 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/Trunmed.c
+++ src/library/stats/src/Trunmed.c
@@ -374,7 +374,7 @@ static void runmedint(R_xlen_t n, int k, int k2, const double *data, double *med
         median[k2] = window[k];
     }
     R_xlen_t every_i;
-    if (print_level == 2)
+    if (print_level >= 2)
         every_i = (n > 100) ? n / 10 : 10;
 
     outnext = 0;
