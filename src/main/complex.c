84c895f7ca837908686dc415c8d12ae4c4e9ba31
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-11-11 10:33:06 +0000

    comments (incl typos); white space etc cosmetics
    git-svn-id: https://svn.r-project.org/R/trunk@77395 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/complex.c
+++ src/main/complex.c
@@ -1,8 +1,8 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 2000-2016	    The R Core Team
- *  Copyright (C) 2005		    The R Foundation
+ *  Copyright (C) 2000-2019  The R Core Team
+ *  Copyright (C) 2005       The R Foundation
+ *  Copyright (C) 1995-1997  Robert Gentleman and Ross Ihaka
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 #include <config.h>
 #endif
 
-/* Note: gcc -peantic may warn in several places about C99 features
+/* Note: gcc -pedantic may warn in several places about C99 features
    as extensions.
    This was a very-long-standing GCC bug, http://gcc.gnu.org/PR7263
    The system <complex.h> header can work around it: some do.
@@ -372,17 +372,15 @@ SEXP attribute_hidden do_cmathfuns(SEXP call, SEXP op, SEXP args, SEXP env)
     return y;
 }
 
-/* used in format.c and printutils.c */
+/* Implementing  signif(<complex>)  *and* used in format.c and printutils.c */
 #define MAX_DIGITS 22
 void attribute_hidden z_prec_r(Rcomplex *r, const Rcomplex *x, double digits)
 {
-    double m = 0.0, m1, m2;
-    int dig, mag;
+    // Implement    r <- signif(x, digits)
 
     r->r = x->r;
     r->i = x->i;
-    m1 = fabs(x->r);
-    m2 = fabs(x->i);
+    double m = 0.0, m1 = fabs(x->r), m2 = fabs(x->i);
     if (R_FINITE(m1))
         m = m1;
     if (R_FINITE(m2) && m2 > m)
@@ -399,12 +397,12 @@ void attribute_hidden z_prec_r(Rcomplex *r, const Rcomplex *x, double digits)
             return;
         }
     }
-    dig = (int)floor(digits + 0.5);
+    int dig = (int)floor(digits + 0.5);
     if (dig > MAX_DIGITS)
         return;
     else if (dig < 1)
         dig = 1;
-    mag = (int)floor(log10(m));
+    int mag = (int)floor(log10(m));
     dig = dig - mag - 1;
     if (dig > 306)
     {
@@ -664,12 +662,17 @@ SEXP attribute_hidden complex_math1(SEXP call, SEXP op, SEXP args, SEXP env)
     case 10003:
         naflag = cmath1(clog, px, py, n);
         break;
+    // 1: floor
+    // 2: ceil[ing]
     case 3:
         naflag = cmath1(csqrt, px, py, n);
         break;
+    // 4: sign
     case 10:
         naflag = cmath1(cexp, px, py, n);
         break;
+    // 11: expm1
+    // 12: log1p
     case 20:
         naflag = cmath1(ccos, px, py, n);
         break;
@@ -688,6 +691,7 @@ SEXP attribute_hidden complex_math1(SEXP call, SEXP op, SEXP args, SEXP env)
     case 25:
         naflag = cmath1(z_atan, px, py, n);
         break;
+
     case 30:
         naflag = cmath1(ccosh, px, py, n);
         break;
