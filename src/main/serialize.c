b639cf8bfa896645fea578e365c549ef2562f0b9
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-12-16 13:18:51 +0000

    Fix treatmeant of invalid encoding length in deserialization.
    git-svn-id: https://svn.r-project.org/R/trunk@77578 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/serialize.c
+++ src/main/serialize.c
@@ -2313,12 +2313,9 @@ SEXP R_Unserialize(R_inpstream_t stream)
         break;
     case 3: {
         int nelen = InInteger(stream);
-        if (nelen > R_CODESET_MAX + 1)
+        if (nelen > R_CODESET_MAX)
             error(_("invalid length of encoding name"));
-        char nbuf[nelen + 1];
-        InString(stream, nbuf, nelen);
-        nbuf[nelen] = '\0';
-        strncpy(stream->native_encoding, nbuf, nelen);
+        InString(stream, stream->native_encoding, nelen);
         stream->native_encoding[nelen] = '\0';
         break;
     }
@@ -2414,7 +2411,7 @@ SEXP R_SerializeInfo(R_inpstream_t stream)
     {
         SET_STRING_ELT(names, 4, mkChar("native_encoding"));
         int nelen = InInteger(stream);
-        if (nelen > R_CODESET_MAX + 1)
+        if (nelen > R_CODESET_MAX)
             error(_("invalid length of encoding name"));
         char nbuf[nelen + 1];
         InString(stream, nbuf, nelen);
