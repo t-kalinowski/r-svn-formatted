f1ff27d56313e3cf643e58eeea39ee957961dc2b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-09-25 08:38:07 +0000

    report current timeout if reached
    git-svn-id: https://svn.r-project.org/R/trunk@79254 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/libcurl.c
+++ src/modules/internet/libcurl.c
@@ -55,6 +55,8 @@
 extern void Rsleep(double timeint);
 #endif
 
+static int current_timeout = 0;
+
 #if (LIBCURL_VERSION_MAJOR == 7 && LIBCURL_VERSION_MINOR < 28)
 
 // curl/curl.h includes <sys/select.h> and headers it requires.
@@ -301,7 +303,10 @@ static int curlMultiCheckerrs(CURLM *mhnd)
             else
             {
                 strerr = curl_easy_strerror(msg->data.result);
-                warning(_("URL '%s': status was '%s'"), url, strerr);
+                if (streql(strerr, "Timeout was reached"))
+                    warning(_("URL '%s': Timeout of %d seconds was reached"), url, current_timeout);
+                else
+                    warning(_("URL '%s': status was '%s'"), url, strerr);
             }
             retval++;
         }
@@ -361,6 +366,7 @@ static void curlCommon(CURL *hnd, int redirect, int verify)
 #endif
     int timeout0 = asInteger(GetOption1(install("timeout")));
     long timeout = (timeout0 == NA_INTEGER) ? 0 : (1000L * timeout0);
+    current_timeout = (timeout0 == NA_INTEGER) ? 0 : timeout0;
     curl_easy_setopt(hnd, CURLOPT_CONNECTTIMEOUT_MS, timeout);
     curl_easy_setopt(hnd, CURLOPT_TIMEOUT_MS, timeout);
     if (redirect)
@@ -431,7 +437,10 @@ SEXP attribute_hidden in_do_curlGetHeaders(SEXP call, SEXP op, SEXP args, SEXP r
     curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, &rcvBody);
     curlCommon(hnd, redirect, verify);
     if (timeout > 0)
+    {
         curl_easy_setopt(hnd, CURLOPT_TIMEOUT, timeout);
+        current_timeout = timeout;
+    }
 
     char errbuf[CURL_ERROR_SIZE];
     curl_easy_setopt(hnd, CURLOPT_ERRORBUFFER, errbuf);
