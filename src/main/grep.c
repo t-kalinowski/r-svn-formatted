847cfc82f64cb84e128a03ca3496667cddad41ff
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-09-25 16:54:18 +0000

    more likely intention
    git-svn-id: https://svn.r-project.org/R/trunk@79261 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/grep.c
+++ src/main/grep.c
@@ -3824,8 +3824,10 @@ SEXP attribute_hidden do_regexec(SEXP call, SEXP op, SEXP args, SEXP env)
                 }
                 setAttrib(matchpos, install("match.length"), matchlen);
                 if (useBytes)
+                {
                     setAttrib(matchpos, install("index.type"), itype);
-                setAttrib(matchpos, install("useBytes"), R_TrueValue);
+                    setAttrib(matchpos, install("useBytes"), R_TrueValue);
+                }
                 SET_VECTOR_ELT(ans, i, matchpos);
                 UNPROTECT(2);
             }
@@ -3841,8 +3843,10 @@ SEXP attribute_hidden do_regexec(SEXP call, SEXP op, SEXP args, SEXP env)
                 PROTECT(matchlen = ScalarInteger(-1));
                 setAttrib(matchpos, install("match.length"), matchlen);
                 if (useBytes)
+                {
                     setAttrib(matchpos, install("index.type"), itype);
-                setAttrib(matchpos, install("useBytes"), R_TrueValue);
+                    setAttrib(matchpos, install("useBytes"), R_TrueValue);
+                }
                 SET_VECTOR_ELT(ans, i, matchpos);
                 UNPROTECT(2);
             }
