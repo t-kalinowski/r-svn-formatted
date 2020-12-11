6f12ee1c8d2c237557dadb2b0fe9834ea9ca2975
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-12-11 14:02:21 +0000

    Allow only \Uxxxxxxxx escapes valid for UTF-8 Ouptput with only 6 hex digits Avoid system mbrtowc (Solaris one is wrong for upper Unicode points).
    git-svn-id: https://svn.r-project.org/R/trunk@79612 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gram.c
+++ src/main/gram.c
@@ -5290,6 +5290,8 @@ static int StringValue(int c, Rboolean forSymbol)
                 }
                 if (!val)
                     error(_("nul character not allowed (line %d)"), ParseState.xxlineno);
+                if (val > 0x10FFFF)
+                    error(_("invalid \\U{xxxxxxxx} sequence (line %d)"), ParseState.xxlineno);
 #ifdef Win32
                 if (0x010000 <= val && val <= 0x10FFFF)
                 { /* Need surrogate pair in Windows */
