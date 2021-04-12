75e432e41d81cc6bc3bec62a18822f46d5bdd8fc
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-04-12 17:25:18 +0000

    Allow setting marked bytes encoding to unknown.  With Brodie Gaslam.
    git-svn-id: https://svn.r-project.org/R/trunk@80160 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/util.c
+++ src/main/util.c
@@ -1312,6 +1312,8 @@ SEXP attribute_hidden do_encoding(SEXP call, SEXP op, SEXP args, SEXP rho)
     return ans;
 }
 
+#define IS_NATIVE(tmp) (!IS_LATIN1(tmp) && !IS_UTF8(tmp) && !IS_BYTES(tmp))
+
 SEXP attribute_hidden do_setencoding(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
     SEXP x, enc, tmp;
@@ -1345,7 +1347,7 @@ SEXP attribute_hidden do_setencoding(SEXP call, SEXP op, SEXP args, SEXP rho)
         if (tmp == NA_STRING)
             continue;
         if (!((ienc == CE_LATIN1 && IS_LATIN1(tmp)) || (ienc == CE_UTF8 && IS_UTF8(tmp)) ||
-              (ienc == CE_BYTES && IS_BYTES(tmp)) || (ienc == CE_NATIVE && !IS_LATIN1(tmp) && !IS_UTF8(tmp))))
+              (ienc == CE_BYTES && IS_BYTES(tmp)) || (ienc == CE_NATIVE && IS_NATIVE(tmp))))
             SET_STRING_ELT(x, i, mkCharLenCE(CHAR(tmp), LENGTH(tmp), ienc));
     }
     UNPROTECT(1);
