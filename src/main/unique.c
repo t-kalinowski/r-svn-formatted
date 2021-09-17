0e590dc4b27d2e46c1dce4416a526672e2785159
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-09-17 05:40:00 +0000

    Transform classed objects via mtfrm() for matching.
    git-svn-id: https://svn.r-project.org/R/trunk@80918 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/unique.c
+++ src/main/unique.c
@@ -1336,6 +1336,14 @@ static SEXP match_transform(SEXP s, SEXP env)
             UNPROTECT(1);
             return r;
         }
+        else
+        {
+            SEXP call, r;
+            PROTECT(call = lang2(install("mtfrm"), s));
+            r = eval(call, env);
+            UNPROTECT(1);
+            return r;
+        }
     }
     /* else */
     return duplicate(s);
