20fd6c1eede9f52f08e5700739afe7fc40c070e6
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-03-16 10:12:55 +0000

    Disallow bidi formatting in string values (from Brodie Gaslam).
    git-svn-id: https://svn.r-project.org/R/trunk@81912 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gram.c
+++ src/main/gram.c
@@ -5289,6 +5289,15 @@ static int skipBytesByChar(char *c, int min)
     } while (0)
 #define CTEXT_POP() ct--
 
+/* Bidi formatting could confuse the code */
+#define BIDI_CHECK(wc)                                                                                                 \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if ((wc) >= 0x202A && (wc) <= 0x2069 && !((wc) > 0x202E && (wc) < 0x2066))                                     \
+            error(_("bidi formatting not allowed (line %d), use escapes instead (\\u%04x)"), ParseState.xxlineno,      \
+                  (wc));                                                                                               \
+    } while (0)
+
 /* forSymbol is true when parsing backticked symbols */
 static int StringValue(int c, Rboolean forSymbol)
 {
@@ -5548,6 +5557,7 @@ static int StringValue(int c, Rboolean forSymbol)
             int i, clen;
             ucs_t wc;
             clen = mbcs_get_next2(c, &wc);
+            BIDI_CHECK(wc);
             WTEXT_PUSH(wc);
             ParseState.xxbyteno += clen - 1;
 
@@ -5725,6 +5735,7 @@ static int RawStringValue(int c0, int c)
             int i, clen;
             ucs_t wc;
             clen = mbcs_get_next2(c, &wc);
+            BIDI_CHECK(wc);
             WTEXT_PUSH(wc);
             ParseState.xxbyteno += clen - 1;
 
