e98aac5faa13cd9892c882b800d08d86068ff54a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-03-18 15:18:25 +0000

    pacify incorrect warning from GCC 12
    git-svn-id: https://svn.r-project.org/R/trunk@81932 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/Trunmed.c
+++ src/library/stats/src/Trunmed.c
@@ -373,7 +373,7 @@ static void runmedint(R_xlen_t n, int k, int k2, const double *data, double *med
             ;
         median[k2] = window[k];
     }
-    R_xlen_t every_i;
+    R_xlen_t every_i = 1; // -Wall: only used for print_level >= 2.
     if (print_level >= 2)
         every_i = (n > 100) ? n / 10 : 10;
 
