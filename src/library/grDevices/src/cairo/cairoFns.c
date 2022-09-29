a84481547c910865d89fefcde9754097397e72c1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-09-29 07:35:00 +0000

    more chasing doen warnings
    git-svn-id: https://svn.r-project.org/R/trunk@82947 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/cairo/cairoFns.c
+++ src/library/grDevices/src/cairo/cairoFns.c
@@ -1513,7 +1513,8 @@ static void Cairo_Raster(unsigned int *raster, int w, int h, double x, double y,
      * the image used for the Cairo surface is going to be a
      * different size
      */
-    if (interpolate && (CAIRO_VERSION_MAJOR < 2) & (CAIRO_VERSION_MINOR < 6))
+    // clang 15 prefers & here for constant expressions.
+    if (interpolate & (CAIRO_VERSION_MAJOR < 2) & (CAIRO_VERSION_MINOR < 6))
     {
         imageWidth = (int)(width + .5);
         imageHeight = abs((int)(height + .5));
@@ -1534,6 +1535,7 @@ static void Cairo_Raster(unsigned int *raster, int w, int h, double x, double y,
 
     if (interpolate)
     {
+        // clang 15 prefers & here for constant expressions.
         if ((CAIRO_VERSION_MAJOR < 2) & (CAIRO_VERSION_MINOR < 6))
         {
             /* CAIRO_EXTEND_PAD not supported for image sources
